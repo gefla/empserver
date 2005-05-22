82ac1a9ddcf65a009cb0e1431cb563a423199f05
Ron Koenderink <rkoenderink@yahoo.ca>
2005-05-22 00:52:43 +0000

    Initial Release.

---
+++ scripts/backup.bat
@@ -0,0 +1,16 @@
+REM Example Backup script to be run from pre_update_hook
+REM Written by Ron Koenderink, 2005
+REM This script is in the public domain.
+REM
+REM Use: edit configuration variables below to taste, then set
+REM   pre_update_hook = c:\home\wolfpack\emp4\scripts\backup.bat
+REM in econfig.
+REM
+REM Rules when running as pre_update_hook:
+REM Data directory is working directory.
+REM stdin, stdout, stderr are redirected to /dev/null
+REM Exit code other than zero cancels update!
+REM
+REM This script is as simple as possible, since failure cancels the
+REM update.
+xcopy /E /Y c:\home\wolfpack\emp4\data c:\home\wolfpack\emp4\backup
---
+++ scripts/blitz-w32/blitz_script.bat
@@ -0,0 +1,21 @@
+REM Example Blitz script which can be ran as a scheduled task.
+REM Written by Ron Koenderink, 2005
+REM This script is in the public domain.
+REM
+REM You will need to set the EMPIREHOST and EMPIREPORT if the 
+REM emp_client is not compiled with the correct defaults.
+REM set EMPIREHOST=localhost
+REM set EMPIREPORT=6665
+REM
+REM The script can be simplified by setting the execute
+REM directory to the bin directory.
+REM The -e is not necessary unless you are not using the
+REM the default game directory.
+REM
+echo power new | c:\home\wolfpack\emp4\bin\emp_client -2 power.out POGO peter
+net stop "Empire Server"
+c:\home\wolfpack\emp4\bin\files -f -e c:\home\wolfpack\emp4\data\econfig
+c:\home\wolfpack\emp4\bin\fairland -e c:\home\wolfpack\emp4\data\econfig 10 30
+net start "Empire Server"
+c:\home\wolfpack\emp4\bin\emp_client POGO peter <newcap_script
+echo enable | c:\home\wolfpack\emp4\bin\emp_client POGO peter
