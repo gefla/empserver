95a2523b9f25806e010e5927524d1cd84b017c18
Markus Armbruster <armbru@pond.sub.org>
2005-12-01 19:28:31 +0000

    (xuflds): Fix confusing diagnostics on unexpected EOF.

--- src/lib/common/xundump.c
+++ src/lib/common/xundump.c
@@ -117,6 +117,8 @@ xuflds(FILE *fp, struct value values[])
 	ungetc(ch, fp);
 
 	switch (ch) {
+	case EOF:
+	    return gripe("Unexpected EOF");
 	case '+': case '-': case '.':
 	case '0': case '1': case '2': case '3': case '4':
 	case '5': case '6': case '7': case '8': case '9':
