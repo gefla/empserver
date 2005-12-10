ef79cd67ef16f61658733787dca9fa636122ad7b
Markus Armbruster <armbru@pond.sub.org>
2005-12-10 15:03:11 +0000

    (lwpInitContext) [UCONTEXT]: Failed to initialized uc_stack.ss_flags.

--- src/lib/lwp/arch.c
+++ src/lib/lwp/arch.c
@@ -60,8 +60,7 @@ void
 lwpInitContext(struct lwpProc *newp, stack_t *spp)
 {
     getcontext(&newp->context);
-    newp->context.uc_stack.ss_sp = spp->ss_sp;
-    newp->context.uc_stack.ss_size = spp->ss_size;
+    newp->context.uc_stack = *spp;
     newp->context.uc_link = NULL;
     makecontext(&newp->context, lwpEntryPoint, 0);
 }
