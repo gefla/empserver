0c6d9e108c073f157c33b236e9cd166105de758c
Markus Armbruster <armbru@pond.sub.org>
2006-06-08 20:55:17 +0000

    (cant_listen): Supply missing newline.

--- src/lib/gen/tcp_listen.c
+++ src/lib/gen/tcp_listen.c
@@ -160,6 +160,6 @@ tcp_listen(char *host, char *serv, size_t *addrlenp)
 static int
 cant_listen(char *host, char *serv, const char *err)
 {
-    fprintf(stderr, "Can't listen on %s:%s: %s", host, serv, err);
+    fprintf(stderr, "Can't listen on %s:%s: %s\n", host, serv, err);
     exit(1);
 }
