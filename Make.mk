b3c5ba2f758440c9033110c68f5f1c53f0974e84
Markus Armbruster <armbru@pond.sub.org>
2008-03-03 20:12:46 +0100

    Tighten gcc warning options
    Suppress only unused parameters, not all unused stuff.

--- Make.mk
+++ Make.mk
@@ -154,8 +154,9 @@ endif
 ifeq ($(have_gcc),yes)
 CFLAGS += -fno-builtin-carg	# conflicts with our carg()
 CFLAGS += -fno-common
-CFLAGS += -Wall -W -Wno-unused -Wpointer-arith -Wstrict-prototypes	\
--Wmissing-prototypes -Wnested-externs -Wredundant-decls
+CFLAGS += -Wall -W -Wno-unused-parameter -Wpointer-arith	\
+-Wstrict-prototypes -Wmissing-prototypes -Wnested-externs	\
+-Wredundant-decls
 endif
 LDLIBS += -lm
 $(client): LDLIBS += $(termlibs)
