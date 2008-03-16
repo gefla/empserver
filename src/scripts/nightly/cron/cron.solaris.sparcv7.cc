975419e7d3e07a2779ad9c53d37e2919cca781bc
Ron Koenderink <rkoenderink@yahoo.ca>
2008-03-16 17:17:04 -0600

    Remove nightly build configurations files that are not being used
    This files are not current with nightlybuild.sh, common.sh
    and report.sh.

--- src/scripts/nightly/cron/cron.solaris.sparcv7.cc
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/marcolz/src/empire/nightlybuild/
-ksh ./nightlybuild.sh conf/solaris.sparcv7.cc.config
-ksh ./report.sh conf/solaris.sparcv7.cc.config | mutt -s 'empserver solaris.sparcv7.cc' empserver-devel@lists.sourceforge.net
