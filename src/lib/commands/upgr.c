65e934a083080b237cd88034a77540d736c9391c
Markus Armbruster <armbru@pond.sub.org>
2008-03-22 18:25:39 +0100

    Make pupgr() reject planes in orbit early
    It's best to check and report conditions the player can't change
    before those he could.

--- src/lib/commands/upgr.c
+++ src/lib/commands/upgr.c
@@ -272,6 +272,10 @@ pupgr(void)
 	       plane.pln_uid);
 	    continue;
 	}
+	if (plane.pln_flags & PLN_LAUNCHED) {
+	    pr("Plane %s is in orbit!\n", prplane(&plane));
+	    continue;
+	}
 	n++;
 	pp = &plchr[(int)plane.pln_type];
 	avail = (PLN_BLD_WORK(pp->pl_lcm, pp->pl_hcm) * UPGR_COST + 99) / 100;
@@ -294,10 +298,6 @@ pupgr(void)
 	if (cost + player->dolcost > cash) {
 	    pr("You don't have enough money to upgrade %s!\n",
 	       prplane(&plane));
-	    continue;
-	}
-	if (plane.pln_flags & PLN_LAUNCHED) {
-	    pr("Plane %s is in orbit!\n", prplane(&plane));
 	    continue;
 	}
 
