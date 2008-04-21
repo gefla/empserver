b313b61cd431b4260204ccf2b912b768efef0212
Markus Armbruster <armbru@pond.sub.org>
2008-04-21 22:03:36 +0200

    Oops on sequence number mismatch
    Old code only logged this, for fear of false positives.

--- src/lib/common/file.c
+++ src/lib/common/file.c
@@ -507,16 +507,8 @@ new_seqno(struct empfile *ep, void *buf)
     if (!(ep->flags & EFF_TYPED))
 	return;
     old_seqno = get_seqno(ep, elt->uid);
-#if 0
     if (CANT_HAPPEN(old_seqno != elt->seqno))
 	old_seqno = MAX(old_seqno, elt->seqno);
-#else
-    if (old_seqno != elt->seqno) {
-	logerror("seqno mismatch ef_type=%d uid=%d: %d!=%d",
-		 ep->uid, elt->uid, old_seqno, elt->seqno);
-	old_seqno = MAX(old_seqno, elt->seqno);
-    }
-#endif
     elt->seqno = old_seqno + 1;
 }
 
