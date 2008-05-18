gefla/empserver

commit dc9d847b8ba8bcf7da2ca7b47695abf51745f3eb

Add sequence numbers to game state (experimental)
This catches output dependency violations, e.g. two threads doing a
read-modify-write without synchronization.

New struct emptypedstr member seqno.  Make sure all members of unit
empobj_storage share it.  Set it in ef_blank() and ef_set_uid(), step
it in ef_write().  fairland and files don't use ef_set_uid(); need to
set it manually in files.c's main() and file_sct_init().

Factor do_read() out of fillcache() to make it available for
new get_seqno().

src/lib/commands/edit.c
@@ -584,7 +584,7 @@ doland(char op, int arg, char *p, struct sctstr *sect)
 	    return RET_SYN;
 	sect->sct_x = newx;
 	sect->sct_y = newy;
-	sect->sct_uid = XYOFFSET(newx, newy);
+	ef_set_uid(EF_SECTOR, &sect, XYOFFSET(newx, newy));
 	break;
     case 'D':
 	if (!sarg_xy(p, &newx, &newy))
@@ -767,7 +767,7 @@ doship(char op, int arg, char *p, struct shpstr *ship)
 	ship->shp_rflags = arg;
 	break;
     case 'U':
-	ship->shp_uid = arg;
+	ef_set_uid(EF_SHIP, ship, arg);
 	break;
     case 'O':
 	if (ship->shp_own)
@@ -871,7 +871,7 @@ dounit(char op, int arg, char *p, struct lndstr *land)
 	land->lnd_land = arg;
 	break;
     case 'U':
-	land->lnd_uid = arg;
+	ef_set_uid(EF_SHIP, land, arg);
 	break;
     case 'O':
 	if (land->lnd_own)

include/commodity.h
@@ -44,6 +44,7 @@ struct comstr {
     /* initial part must match struct empobj */
     short ef_type;
     short com_uid;
+    unsigned com_seqno;
     time_t com_timestamp;
     natid com_owner;
     /* end of part matching struct empobj */

include/empobj.h
@@ -57,6 +57,7 @@ struct empobj {
      */
     short ef_type;
     short uid;
+    unsigned seqno;
     time_t timestamp;
     /* end of part matching struct emptypedstr */
     natid own;		/* valid if EFF_OWNER is in table's flags */

include/file.h
@@ -67,6 +67,7 @@ struct empfile {
 struct emptypedstr {
     short ef_type;
     short uid;
+    unsigned seqno;
     time_t timestamp;
 };
 
@@ -187,6 +188,7 @@ extern int ef_close(int);
 extern int ef_flush(int);
 extern void ef_blank(int, int, void *);
 extern int ef_write(int, int, void *);
+extern void ef_set_uid(int, void *, int);
 extern int ef_extend(int, int);
 extern int ef_ensure_space(int, int, int);
 extern int ef_truncate(int, int);

include/game.h
@@ -40,6 +40,7 @@ struct gamestr {
     /* initial part must match struct empobj */
     short ef_type;
     short game_uid;
+    unsigned game_seqno;
     time_t game_timestamp;
     /* end of part matching struct empobj */
     char game_upd_disable;	/* updates disabled? */

include/land.h
@@ -51,6 +51,7 @@ struct lndstr {
     /* initial part must match struct empobj */
     short ef_type;
     short lnd_uid;		/* unit id (land unit) */
+    unsigned lnd_seqno;
     time_t lnd_timestamp;	/* Last time this unit was touched */
     natid lnd_own;		/* owner's country num */
     coord lnd_x;		/* x location in abs coords */

include/loan.h
@@ -44,6 +44,7 @@ struct lonstr {
     /* initial part must match struct empobj */
     short ef_type;
     short l_uid;
+    unsigned l_seqno;
     time_t l_timestamp;
     /* end of part matching struct empobj */
     natid l_loner;		/* loan shark */

include/lost.h
@@ -41,6 +41,7 @@ struct loststr {
     /* initial part must match struct empobj */
     short ef_type;
     int lost_uid;
+    unsigned lost_seqno;
     time_t lost_timestamp;	/* When it was lost */
     natid lost_owner;		/* Who lost it */
     /* end of part matching struct empobj */

commit e7aa3627c44926d9b1f23c1460ffb57027fdf85f

Fix edit command seqno oopses
Broken by commit dc9d847b.

src/lib/commands/edit.c
@@ -871,7 +871,7 @@ dounit(char op, int arg, char *p, struct lndstr *land)
 	land->lnd_land = arg;
 	break;
     case 'U':
-	ef_set_uid(EF_SHIP, land, arg);
+	ef_set_uid(EF_LAND, land, arg);
 	break;
     case 'O':
 	if (land->lnd_own)
@@ -994,7 +994,7 @@ doplane(char op, int arg, char *p, struct plnstr *plane)
 	plane->pln_nuketype = arg;
 	break;
     case 'U':
-	plane->pln_uid = arg;
+	ef_set_uid(EF_PLANE, plane, arg);
 	break;
     case 'l':
 	if (!sarg_xy(p, &newx, &newy))