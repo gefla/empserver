gefla/empserver

commit b8bdc32b3cf608b63e025836d33964ebfa81fba1

Fix unlimbering, it had no effect
Broken by mismerging the rebase of Hvy Metal I in commit 5d0f5e69,
except for fire command when the target was out of range.

src/lib/commands/mfir.c
@@ -349,6 +349,7 @@ multifire(void)
 		range2 = -1;
 	    }
 
+	    lnd_unlimber(&fland);
 	    dam = lnd_fire(&fland);
 	    putland(fland.lnd_uid, &fland);
 	    if (dam < 0) {
@@ -356,7 +357,6 @@ multifire(void)
 		continue;
 	    }
 
-	    lnd_unlimber(&fland);
 	    if (target == targ_ship) {
 		if (chance(lnd_acc(&fland) / 100.0))
 		    dam = ldround(dam / 2.0, 1);
@@ -713,13 +713,13 @@ quiet_bigdef(int type, struct emp_qelem *list, natid own, natid aown,
 	if (fp)
 	    dam2 = fp->defdam;
 	else {
+	    lnd_unlimber(&land);
 	    dam2 = lnd_fire(&land);
 	    putland(land.lnd_uid, &land);
 	}
 	if (dam2 < 0)
 	    continue;
 
-	lnd_unlimber(&land);
 	(*nfiring)++;
 	if (!fp)
 	    add_to_flist(list, (struct empobj *)&land, dam2, 0);

src/lib/subs/lndsub.c
@@ -1142,12 +1142,12 @@ lnd_support(natid victim, natid attacker, coord x, coord y, int defending)
 	if (dist > range)
 	    continue;
 
+	lnd_unlimber(&land);
 	dam2 = lnd_fire(&land);
 	putland(land.lnd_uid, &land);
 	if (dam2 < 0)
 	    continue;
 
-	lnd_unlimber(&land);
 	if (defending)
 	    nreport(land.lnd_own, N_FIRE_BACK, victim, 1);
 	else

src/lib/subs/mission.c
@@ -444,12 +444,12 @@ perform_mission(coord x, coord y, natid victim, struct emp_qelem *list,
 	    if (md > range)
 		continue;
 
+	    lnd_unlimber(lp);
 	    dam2 = lnd_fire(lp);
 	    putland(lp->lnd_uid, lp);
 	    if (dam2 < 0)
 		continue;
 
-	    lnd_unlimber(lp);
 	    if (sect.sct_type == SCT_WATER) {
 		if (chance(lnd_acc(lp) / 100.0))
 		    dam2 = ldround(dam2 / 2.0, 1);

commit c40dd61bf0639414fa94312b950112469bebad67

Don't unlimber when guns unsuccessfully try to fire
This happened when fire command failed becayse the gun lacked shells,
and when other ways to fire failed because the gun was inefficient,
embarked, lacked mil, guns or shells.

Broken in commit b8bdc32b, hvy-metal-2.4.

src/lib/commands/mfir.c
@@ -349,7 +349,6 @@ multifire(void)
 		range2 = -1;
 	    }
 
-	    lnd_unlimber(&fland);
 	    dam = lnd_fire(&fland);
 	    putland(fland.lnd_uid, &fland);
 	    if (dam < 0) {
@@ -713,7 +712,6 @@ quiet_bigdef(int type, struct emp_qelem *list, natid own, natid aown,
 	if (fp)
 	    dam2 = fp->defdam;
 	else {
-	    lnd_unlimber(&land);
 	    dam2 = lnd_fire(&land);
 	    putland(land.lnd_uid, &land);
 	}

src/lib/subs/landgun.c
@@ -207,6 +207,7 @@ lnd_fire(struct lndstr *lp)
 			    I_SHELL, ammo - shells);
     if (shells == 0)
 	return -1;
+    lnd_unlimber(lp);
     d = landunitgun(lp->lnd_effic, guns);
     if (shells < ammo) {
 	d *= (double)shells / (double)ammo;

src/lib/subs/lndsub.c
@@ -1142,7 +1142,6 @@ lnd_support(natid victim, natid attacker, coord x, coord y, int defending)
 	if (dist > range)
 	    continue;
 
-	lnd_unlimber(&land);
 	dam2 = lnd_fire(&land);
 	putland(land.lnd_uid, &land);
 	if (dam2 < 0)

src/lib/subs/mission.c
@@ -445,7 +445,6 @@ perform_mission(coord x, coord y, natid victim, struct emp_qelem *list,
 	    if (md > range)
 		continue;
 
-	    lnd_unlimber(lp);
 	    dam2 = lnd_fire(lp);
 	    putland(lp->lnd_uid, lp);
 	    if (dam2 < 0)