gefla/empserver

commit 84cfd670ce1cd08ef185e3bea3e2a3e68020df4b

Abstract from shutdown initiation mechanism: (shutdown_initiate): New. (shut): Use it.  Shutdown in zero minutes no longer cancels the shutdown, it just works.  Use negative argument to cancel.  Logging is less detailed. (shutdown_sequence): Internal linkage.
(pr_wall): All callers prefix text it with the same header.  Move it
into the function.

src/lib/commands/shut.c
@@ -49,43 +49,25 @@ shut(void)
 
     shutdown_minutes =
 	onearg(player->argp[1],
-	       "Time until shutdown in minutes (0 to abort shutdown sequence)? ");
-    if (shutdown_minutes < 0)
-	return RET_SYN;
+	       "Time until shutdown in minutes (-1 to abort shutdown sequence)? ");
     if (!updates_disabled())
 	if (!(p = getstarg(player->argp[2], "Disable update [y]? ", buf))
 	    || *p != 'n')
 	    disa();
 
-    shutdown_was_pending = shutdown_pending;
-    shutdown_pending = shutdown_minutes + !!shutdown_minutes;
-    msgbuf[0] = '\0';
+    shutdown_was_pending = shutdown_initiate(shutdown_minutes);
+    if (shutdown_was_pending < 0)
+	return RET_FAIL;
+
     if (shutdown_was_pending) {
-	if (shutdown_minutes) {
-	    sprintf(msgbuf,
-		    ": The shutdown time has been changed to %d minutes!",
-		    shutdown_minutes);
+	if (shutdown_minutes >= 0) {
+	    pr("The shutdown time has been changed to %d minutes!",
+	       shutdown_minutes);
 	} else {
-	    sprintf(msgbuf, ": The server shutdown has been cancelled!");
+	    pr("The server shutdown has been cancelled!");
 	}
     } else if (shutdown_minutes) {
 	pr("Shutdown sequence begun.\n");
-	logerror("Shutdown sequence begun");
-	empth_create(PP_SHUTDOWN, shutdown_sequence, (50 * 1024),
-		     0, "shutdownSeq", "Counts down server shutdown", 0);
-    }
-    us = getnatp(player->cnum);
-    if (msgbuf[0]) {
-	sendmessage(us, 0, msgbuf, 1);
-	pr("%s\n", msgbuf + 2);
-	logerror("%s", msgbuf + 2);
-    }
-    if (shutdown_minutes) {
-	sprintf(msgbuf, ": The server will shut down in %d minutes!",
-		shutdown_minutes);
-	sendmessage(us, 0, msgbuf, 1);
-	pr("%s\n", msgbuf + 2);
-	logerror("%s", msgbuf + 2);
     }
     return RET_OK;
 }

include/server.h
@@ -43,15 +43,15 @@ extern time_t update_time;
 extern int updating_mob;
 
 void mobility_init(void);
-
-/* thread entry points */
-void delete_lostitems(void *);
 void market_init(void);
-void mobility_check(void *);
-void player_kill_idle(void *);
 void update_main(void);
 void update_init(void);
 int update_trigger(time_t);
-void shutdown_sequence(void *);
+int shutdown_initiate(int);
+
+/* thread entry points */
+void delete_lostitems(void *);
+void mobility_check(void *);
+void player_kill_idle(void *);
 
 #endif

info/shutdown.t
@@ -2,7 +2,7 @@
 .NA shutdown "Shut down the server"
 .LV Expert
 .SY "shutdown <minutes> <disable update?>"
-Shutdown the server in <minutes> minutes.  If <minutes> is 0,
+Shutdown the server in <minutes> minutes.  If <minutes> is negative,
 then any previous shutdown request will be canceled.  If
 the second argument is a "n" then updates will not be disabled
 (otherwise they will be).

src/lib/subs/pr.c
@@ -182,17 +182,26 @@ pr_inform(struct player *pl, char *format, ...)
  * Send C_FLASH text to everyone.
  * Format text to send using printf-style FORMAT and optional
  * arguments.  It is assumed to be plain ASCII.
+ * Prefix text it with a header suitable for broadcast from deity.
  * Initiate an output queue flush, but do not wait for it to complete.
  */
 void
 pr_wall(char *format, ...)
 {
+    time_t now;
+    struct tm *tm;
     char buf[4096];		/* UTF-8 */
+    int n;
     struct player *p;
     va_list ap;
 
+    time(&now);
+    tm = localtime(&now);
+    n = sprintf(buf, "BROADCAST from %s @ %02d:%02d: ",
+		getnatp(0)->nat_cnam, tm->tm_hour, tm->tm_min);
+
     va_start(ap, format);
-    (void)vsprintf(buf, format, ap);
+    (void)vsprintf(buf + n, format, ap);
     va_end(ap);
     for (p = player_next(0); p; p = player_next(p)) {
 	if (p->state != PS_PLAYING)

src/server/shutdown.c
@@ -42,46 +42,66 @@
 
 int shutdown_pending;
 
-void
+static void shutdown_sequence(void *unused);
+
+int
+shutdown_initiate(int mins_from_now)
+{
+    int old_pending = shutdown_pending;
+
+    if (mins_from_now < 0) {
+	if (shutdown_pending) {
+	    shutdown_pending = 0;
+	    pr_wall("The server shutdown has been cancelled!\n");
+	}
+	return old_pending;
+    }
+
+    shutdown_pending = mins_from_now + 1;
+
+    if (old_pending) {
+	pr_wall("The shutdown time has been changed to %d minutes!\n",
+		mins_from_now);
+	/* FIXME wake up shutdown_sequence() */
+    } else {
+	if (!empth_create(PP_SHUTDOWN, shutdown_sequence, 50 * 1024,
+			  0, "shutdownSeq", "Counts down server shutdown",
+			  NULL))
+	    return -1;
+    }
+
+    return old_pending;
+}
+
+static void
 shutdown_sequence(void *unused)
 {
-    struct natstr *god;
-    struct tm *tm;
     time_t now;
-    char header[100];
 
-    if (shutdown_pending <= 0) {
-	shutdown_pending = 0;
-	logerror("shutdown called with 0 shutdown_pending");
-	empth_exit();
-	return;
-    }
-    god = getnatp(0);
+    pr_wall("The server will shut down in %d minutes!\n",
+	    shutdown_pending - 1);
+
     while (shutdown_pending > 0) {
 	--shutdown_pending;
 	time(&now);
 	if (shutdown_pending <= 1440) {	/* one day */
-	    tm = localtime(&now);
-	    sprintf(header, "BROADCAST from %s @ %02d:%02d: ",
-		    god->nat_cnam, tm->tm_hour, tm->tm_min);
-	    if (!shutdown_pending) {
-		pr_wall("%sServer shutting down NOW!\n", header);
+	    if (shutdown_pending == 0) {
 		shutdwn(0);
 	    } else if (shutdown_pending == 1) {
-		pr_wall("%sServer shutting down in 1 minute!\n", header);
+		pr_wall("Server shutting down in 1 minute!\n");
 	    } else if (shutdown_pending <= 5) {
-		pr_wall("%sServer shutting down in %d minutes!\n",
-			header, shutdown_pending);
+		pr_wall("Server shutting down in %d minutes!\n",
+			shutdown_pending);
 	    } else if (shutdown_pending <= 60
 		       && shutdown_pending % 10 == 0) {
-		pr_wall("%sThe server will be shutting down in %d minutes!\n",
-			header, shutdown_pending);
+		pr_wall("The server will be shutting down in %d minutes!\n",
+			shutdown_pending);
 	    } else if (shutdown_pending % 60 == 0) {
-		pr_wall("%sThe server will be shutting down %d hours from now.\n",
-			header, shutdown_pending / 60);
+		pr_wall("The server will be shutting down %d hours from now.\n",
+			shutdown_pending / 60);
 	    }
 	}
+	/* FIXME error due to late wakeup accumulates */
 	empth_sleep(now + 60);
     }
-    empth_exit();
 }

commit 9eda5f87b8a3ddb67c972b0545d97992328c379f

Fix command abortion after getting player input
The old code didn't honor command abortion at the following prompts:

* arm third argument

* deliver fourth argument (also simplify)

* fire third argument

* fly and recon prompt for carrier to land on: pln_onewaymission()
  treated abort like empty input, which made planes attempt landing in
  the sector.

* lmine second argument

* order d fourth argument

* power c nat(s) argument

* range second argument

* sail second argument

* shutdown both arguments (first one was broken in commit 84cfd670,
  v4.3.10, second one never worked).

* tend third argument

src/lib/commands/arm.c
@@ -96,6 +96,8 @@ arm(void)
 	    return RET_FAIL;
 	}
 	p = getstarg(player->argp[3], "Airburst [n]? ", buf);
+	if (!p)
+	    return RET_SYN;
 
 	if (!check_plane_ok(&pl) || !check_nuke_ok(&nuke))
 	    return RET_FAIL;
@@ -106,7 +108,7 @@ arm(void)
 	    return RET_FAIL;
 	}
 
-	if (p && (*p == 'y' || *p == 'Y'))
+	if (*p == 'y' || *p == 'Y')
 	    pl.pln_flags |= PLN_AIRBURST;
 	else
 	    pl.pln_flags &= ~PLN_AIRBURST;

src/lib/commands/deli.c
@@ -55,7 +55,7 @@ deli(void)
     if (!snxtsct(&nstr, player->argp[2]))
 	return RET_SYN;
 
-    while (!player->aborted && nxtsct(&nstr, &sect) > 0) {
+    while (nxtsct(&nstr, &sect) > 0) {
 	if (!player->owner)
 	    continue;
 
@@ -69,15 +69,17 @@ deli(void)
 	if (!(p = getstarg(player->argp[3], prompt, buf)) || !*p)
 	    return RET_SYN;
 	if (*p != 'q') {
-	    sprintf(prompt, "%s %s %s direction? ",
-		    xyas(nstr.x, nstr.y, player->cnum),
-		    dchr[sect.sct_type].d_name, ich->i_name);
 	    if (((*p >= '0') && (*p <= '9')) || *p == '+') {
 		thresh = atoi(p) & ~0x7;
 		if (*p == '+')
 		    p = NULL;
 		else {
+		    sprintf(prompt, "%s %s %s direction? ",
+			    xyas(nstr.x, nstr.y, player->cnum),
+			    dchr[sect.sct_type].d_name, ich->i_name);
 		    p = getstarg(player->argp[4], prompt, buf);
+		    if (!p)
+			return RET_SYN;
 		}
 	    }
 	    if (p && *p) {

src/lib/commands/mfir.c
@@ -118,10 +118,6 @@ multifire(void)
     if (!snxtitem(&nbst, type, ptr))
 	return RET_SYN;
 
-    if (player->aborted) {
-	pr("Fire aborted.\n");
-	return RET_OK;
-    }
     while (nxtitem(&nbst, &item)) {
 	if (type == EF_LAND) {
 	    if (!getland(item.land.lnd_uid, &fland))
@@ -220,13 +216,11 @@ multifire(void)
 	    fy = fsect.sct_y;
 	}
 
-	if ((ptr = getstarg(player->argp[3], "Firing at? ", buf)) == 0
-	    || *ptr == '\0')
+	ptr = getstarg(player->argp[3], "Firing at? ", buf);
+	if (!ptr)
+	    return RET_SYN;
+	if (!*ptr)
 	    continue;
-	if (player->aborted) {
-	    pr("Fire aborted.\n");
-	    continue;
-	}
 	if (!issector(ptr)) {
 	    vshipno = atoi(ptr);
 	    if (vshipno < 0 || !getship(vshipno, &vship) ||

src/lib/commands/mine.c
@@ -128,9 +128,11 @@ landmine(void)
 	       sect.sct_mines, xyas(sect.sct_x, sect.sct_y, player->cnum));
 	sprintf(prompt, "Drop how many mines from %s? ", prland(&land));
 	mines_wanted = onearg(player->argp[2], prompt);
-	if (!check_land_ok(&land))
+	if (mines_wanted < 0)
+	    return RET_SYN;
+	if (mines_wanted == 0)
 	    continue;
-	if (mines_wanted <= 0)
+	if (!check_land_ok(&land))
 	    continue;
 	land.lnd_mission = 0;
 	total_mines_laid = 0;

src/lib/commands/orde.c
@@ -136,7 +136,9 @@ orde(void)
 
 	    if (!orders) {
 		p = getstarg(player->argp[4], "Second dest? ", buf);
-		if (!p || !*p || !strcmp(p, "-")) {
+		if (!p)
+		    return RET_SYN;
+		if (!*p || !strcmp(p, "-")) {
 		    orders = 1;
 		    pr("A one-way order has been accepted.\n");
 		} else if (!strncmp(p, "s", 1)) {

src/lib/commands/powe.c
@@ -94,7 +94,8 @@ powe(void)
 
     if (player->argp[i]) {
 	if (player->argp[i][0] == 'c') {
-	    snxtitem(&ni, EF_NATION, player->argp[i + 1]);
+	    if (!snxtitem(&ni, EF_NATION, player->argp[i + 1]))
+		return RET_SYN;
 	    while (nxtitem(&ni, &nat)) {
 		if (nat.nat_stat == STAT_UNUSED)
 		    continue;

src/lib/commands/rang.c
@@ -52,9 +52,11 @@ range(void)
 	if (!player->owner || plane.pln_own == 0)
 	    continue;
 	p = getstarg(player->argp[2], "New range? ", buf);
+	if (!p)
+	    return RET_SYN;
 	if (!check_plane_ok(&plane))
 	    return RET_SYN;
-	if (!p || (i = atoi(p)) < 0)
+	if ((i = atoi(p)) < 0)
 	    continue;
 	rmax = pln_range_max(&plane);
 	plane.pln_range = MIN(rmax, i);

src/lib/commands/sail.c
@@ -110,7 +110,7 @@ cmd_sail_ship(struct nstr_item *nstr)
     struct shpstr ship;
     char navpath[MAX_PATH_LEN];
 
-    while (!player->aborted && nxtitem(nstr, &ship)) {
+    while (nxtitem(nstr, &ship)) {
 	if (!player->owner || ship.shp_own == 0)
 	    continue;
 	if ((ship.shp_autonav & AN_AUTONAV) &&
@@ -123,13 +123,13 @@ cmd_sail_ship(struct nstr_item *nstr)
 	   xyas(ship.shp_x, ship.shp_y, ship.shp_own));
 	cp = getpath(navpath, player->argp[2],
 		     ship.shp_x, ship.shp_y, 0, 0, P_SAILING);
+	if (!cp)
+	    return RET_SYN;
 	if (!check_ship_ok(&ship))
 	    continue;
-	if (!player->aborted) {
-	    strncpy(ship.shp_path, cp, sizeof(ship.shp_path) - 2);
-	    ship.shp_mission = 0;
-	    putship(ship.shp_uid, &ship);
-	}
+	strncpy(ship.shp_path, cp, sizeof(ship.shp_path) - 2);
+	ship.shp_mission = 0;
+	putship(ship.shp_uid, &ship);
     }
     return RET_OK;
 }

src/lib/commands/shut.c
@@ -50,10 +50,15 @@ shut(void)
     shutdown_minutes =
 	onearg(player->argp[1],
 	       "Time until shutdown in minutes (-1 to abort shutdown sequence)? ");
-    if (!updates_disabled())
-	if (!(p = getstarg(player->argp[2], "Disable update [y]? ", buf))
-	    || *p != 'n')
+    if (player->aborted)
+	return RET_SYN;
+    if (!updates_disabled()) {
+	p = getstarg(player->argp[2], "Disable update [y]? ", buf);
+	if (!p)
+	    return RET_SYN;
+	if (*p != 'n')
 	    disa();
+    }
 
     shutdown_was_pending = shutdown_initiate(shutdown_minutes);
     if (shutdown_was_pending < 0)

src/lib/commands/tend.c
@@ -90,7 +90,10 @@ tend(void)
 	if (type == EF_LAND) {
 	    sprintf(prompt, "Land unit(s) to tend from %s? ",
 		    prship(&tender));
-	    if (!(p = getstarg(player->argp[3], prompt, buf)) || !*p)
+	    p = getstarg(player->argp[3], prompt, buf);
+	    if (!p)
+		return RET_SYN;
+	    if (!*p)
 		continue;
 	    if (!check_ship_ok(&tender))
 		return RET_SYN;
@@ -100,7 +103,10 @@ tend(void)
 	}
 	sprintf(prompt, "Number of %s to tend from %s? ",
 		ip->i_name, prship(&tender));
-	if (!(p = getstarg(player->argp[3], prompt, buf)) || !*p)
+	p = getstarg(player->argp[3], prompt, buf);
+	if (!p)
+	    return RET_SYN;
+	if (!*p)
 	    continue;
 	if (!check_ship_ok(&tender))
 	    return RET_SYN;

src/lib/subs/plnsub.c
@@ -114,7 +114,10 @@ pln_onewaymission(struct sctstr *target, int *shipno, int *flagp)
     nships = carriersatxy(target->sct_x, target->sct_y, player->cnum);
     if (nships) {
 	for (;;) {
-	    if (!(p = getstarg(0, "Carrier #? ", buf)) || !*p)
+	    p = getstring("Carrier #? ", buf);
+	    if (!p)
+		return -1;
+	    if (!*p)
 		break;
 	    cno = atoi(p);
 	    if (cno < 0