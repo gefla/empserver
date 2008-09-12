gefla/empserver

commit effc1b187ace9f9c52800b482657415320f701e1

Use the thread name to identify threads in the journal
This makes the journal easier to read, and makes journals
from the nightly build easier to diff.

Since we use only the 10 first characters of the name,
abridge existing thread names to make them unique within
that many characters.

src/lib/subs/journal.c
@@ -83,7 +83,7 @@ journal_entry(char *fmt, ...)
 
     if (journal) {
 	time(&now);
-	fprintf(journal, "%.24s %p ", ctime(&now), empth_self());
+	fprintf(journal, "%.24s %10.10s:", ctime(&now), empth_name());
 	
 	va_start(ap, fmt);
 	vsnprintf(buf, sizeof(buf) - 1, fmt, ap);

src/lib/player/accept.c
@@ -213,7 +213,7 @@ player_accept(void *unused)
 	stacksize = 100000
 /* budget */  + MAX(WORLD_SZ() * sizeof(int) * 7,
 /* power */ MAXNOC * sizeof(struct powstr));
-	sprintf(buf, "Player (fd #%d)", ns);
+	sprintf(buf, "Conn%d", ns);
 	empth_create(player_login, stacksize, 0, buf, np);
     }
 }

commit 19614ab6dcebcc3a8eb789e83c005d32ae2a68dd

Fix journal_entry() to separate thread and event by space
Broken in commit effc1b18.

src/lib/subs/journal.c
@@ -83,7 +83,7 @@ journal_entry(char *fmt, ...)
 
     if (journal) {
 	time(&now);
-	fprintf(journal, "%.24s %10.10s:",
+	fprintf(journal, "%.24s %10.10s ",
 		ctime(&now), empth_name(empth_self()));
 	
 	va_start(ap, fmt);