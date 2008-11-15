gefla/empserver

commit 0d139ee1d1605069dc9759d578ac4356e8f3335f

Update lost file from prewrite callbacks
Losses of sectors, ships, planes, land units and nukes are tracked in
the lost file.  To keep it current, makelost() and makenotlost() were
called whenever one of these changed owners.  Cumbersome and
error-prone.  In fact, the lost file was never perfectly accurate.

Detect the ownership change in the prewrite callback and call
makelost() / makenotlost() from there.  Remove lost file updates from
where they're no longer needed: right before a put.  takeover() is a
bit more involved: it doesn't put the sectors, but all callers do,
except for guerrilla().  So remove the lost file update from
takeover(), but add it to guerrilla().

This takes care of lost file update for all ownership changes that go
through ef_write().  It can't take care of any missing updates for
changes that don't go through it.

src/lib/subs/sect.c
@@ -61,6 +61,7 @@ sct_postread(int id, void *ptr)
 void
 sct_prewrite(int id, void *old, void *new)
 {
+    struct sctstr *oldsp = old;
     struct sctstr *sp = new;
     int mil, civs;
     natid own;
@@ -70,23 +71,33 @@ sct_prewrite(int id, void *old, void *new)
 
     mil = sp->sct_item[I_MILIT];
     civs = sp->sct_item[I_CIVIL];
+    own = sp->sct_own;
 
-    if (sp->sct_own != 0 && !civs) {
+    if (own && !civs) {
 	sp->sct_work = 100;
-	sp->sct_oldown = sp->sct_own;
+	sp->sct_oldown = own;
     }
 
-    if (sp->sct_own && !civs && !mil
-	&& !has_units(sp->sct_x, sp->sct_y, sp->sct_own, NULL)
+    if (own && !civs && !mil && !has_units(sp->sct_x, sp->sct_y, own, NULL)
 	&& !(sp->sct_flags & MOVE_IN_PROGRESS)) {
 	/* more cruft! */
-	own = sp->sct_own;
-	makelost(EF_SECTOR, sp->sct_own, 0, sp->sct_x, sp->sct_y);
-	sp->sct_own = 0;
+	own = 0;
 	sp->sct_mobil = 0;
 	if (sp->sct_type == SCT_CAPIT || sp->sct_type == SCT_MOUNT)
 	    caploss(sp, own, "");
     }
+
+    /* We've avoided assigning to sp->sct_own, in case oldsp == sp */
+    if (oldsp->sct_own != own) {
+	if (oldsp->sct_own)
+	    makelost(EF_SECTOR, oldsp->sct_own,
+		     0, sp->sct_x, sp->sct_y);
+	if (own)
+	    makenotlost(EF_SECTOR, own,
+			0, sp->sct_x, sp->sct_y);
+    }
+
+    sp->sct_own = own;
 }
 
 void

src/lib/commands/anti.c
@@ -138,10 +138,6 @@ anti(void)
 		sect.sct_item[I_MILIT] = ache;
 		if (sect.sct_own == sect.sct_oldown)
 		    sect.sct_oldown = 0;
-		makelost(EF_SECTOR, sect.sct_own, 0,
-			 sect.sct_x, sect.sct_y);
-		makenotlost(EF_SECTOR, sect.sct_oldown, 0,
-			    sect.sct_x, sect.sct_y);
 		sect.sct_own = sect.sct_oldown;
 		sect.sct_off = 1;	/* Turn the sector off */
 		putsect(&sect);

src/lib/commands/buil.c
@@ -383,8 +383,6 @@ build_ship(struct sctstr *sp, struct mchrstr *mp, short *vec, int tlev)
 
     if (sp->sct_pstage == PLG_INFECT)
 	ship.shp_pstage = PLG_EXPOSED;
-    makenotlost(EF_SHIP, ship.shp_own, ship.shp_uid,
-		ship.shp_x, ship.shp_y);
     putship(ship.shp_uid, &ship);
     pr("%s", prship(&ship));
     pr(" built in sector %s\n", xyas(sp->sct_x, sp->sct_y, player->cnum));
@@ -507,8 +505,6 @@ build_land(struct sctstr *sp, struct lchrstr *lp, short *vec, int tlev)
     if (sp->sct_pstage == PLG_INFECT)
 	land.lnd_pstage = PLG_EXPOSED;
     putland(nstr.cur, &land);
-    makenotlost(EF_LAND, land.lnd_own, land.lnd_uid,
-		land.lnd_x, land.lnd_y);
     pr("%s", prland(&land));
     pr(" built in sector %s\n", xyas(sp->sct_x, sp->sct_y, player->cnum));
     return 1;
@@ -693,8 +689,6 @@ build_nuke(struct sctstr *sp, struct nchrstr *np, short *vec, int tlev)
     vec[I_OIL] -= np->n_oil;
     vec[I_RAD] -= np->n_rad;
 
-    makenotlost(EF_NUKE, nuke.nuk_own, nuke.nuk_uid,
-		nuke.nuk_x, nuke.nuk_y);
     putnuke(nuke.nuk_uid, &nuke);
     pr("%s created in %s\n", prnuke(&nuke),
        xyas(sp->sct_x, sp->sct_y, player->cnum));
@@ -793,8 +787,6 @@ build_plane(struct sctstr *sp, struct plchrstr *pp, short *vec, int tlev)
     vec[I_HCM] -= hcm;
     vec[I_MILIT] -= mil;
 
-    makenotlost(EF_PLANE, plane.pln_own, plane.pln_uid,
-		plane.pln_x, plane.pln_y);
     putplane(plane.pln_uid, &plane);
     pr("%s built in sector %s\n", prplane(&plane),
        xyas(sp->sct_x, sp->sct_y, player->cnum));

src/lib/commands/cede.c
@@ -229,10 +229,7 @@ grab_sect(struct sctstr *sp, natid to)
 
 	wu(0, to, "\t%s ceded to you by %s\n",
 	   prplane(pp), cname(player->cnum));
-	makelost(EF_PLANE, pp->pln_own, pp->pln_uid, pp->pln_x, pp->pln_y);
 	pp->pln_own = to;
-	makenotlost(EF_PLANE, pp->pln_own, pp->pln_uid,
-		    pp->pln_x, pp->pln_y);
 	pp->pln_mobil = 0;
 	pp->pln_mission = 0;
 	putplane(pp->pln_uid, pp);
@@ -246,10 +243,7 @@ grab_sect(struct sctstr *sp, natid to)
 
 	wu(0, to, "\t%s ceded to you by %s\n",
 	   prnuke(np), cname(player->cnum));
-	makelost(EF_NUKE, np->nuk_own, np->nuk_uid, np->nuk_x, np->nuk_y);
 	np->nuk_own = to;
-	makenotlost(EF_NUKE, np->nuk_own, np->nuk_uid,
-		    np->nuk_x, np->nuk_y);
 	putnuke(ni.cur, np);
     }
 
@@ -265,8 +259,6 @@ grab_sect(struct sctstr *sp, natid to)
 
 	wu(0, to, "\t%s ceded to you by %s\n",
 	   prland(lp), cname(player->cnum));
-	makelost(EF_LAND, lp->lnd_own, lp->lnd_uid, lp->lnd_x, lp->lnd_y);
-	makenotlost(EF_LAND, to, lp->lnd_uid, lp->lnd_x, lp->lnd_y);
 	lp->lnd_own = to;
 	lp->lnd_mobil = 0;
 	lp->lnd_mission = 0;
@@ -288,8 +280,6 @@ grab_sect(struct sctstr *sp, natid to)
 
     sp->sct_dist_x = sp->sct_x;
     sp->sct_dist_y = sp->sct_y;
-    makelost(EF_SECTOR, sp->sct_own, 0, sp->sct_x, sp->sct_y);
-    makenotlost(EF_SECTOR, to, 0, sp->sct_x, sp->sct_y);
     if (sp->sct_oldown == sp->sct_own)
 	sp->sct_oldown = to;
     sp->sct_own = to;
@@ -358,10 +348,7 @@ grab_ship(struct shpstr *sp, natid to)
 
 	wu(0, to, "\t%s ceded to you by %s\n",
 	   prplane(pp), cname(player->cnum));
-	makelost(EF_PLANE, pp->pln_own, pp->pln_uid, pp->pln_x, pp->pln_y);
 	pp->pln_own = to;
-	makenotlost(EF_PLANE, pp->pln_own, pp->pln_uid,
-		    pp->pln_x, pp->pln_y);
 	pp->pln_mobil = 0;
 	pp->pln_mission = 0;
 	putplane(pp->pln_uid, pp);
@@ -379,15 +366,11 @@ grab_ship(struct shpstr *sp, natid to)
 
 	wu(0, to, "\t%s ceded to you by %s\n",
 	   prland(lp), cname(player->cnum));
-	makelost(EF_LAND, lp->lnd_own, lp->lnd_uid, lp->lnd_x, lp->lnd_y);
-	makenotlost(EF_LAND, to, lp->lnd_uid, lp->lnd_x, lp->lnd_y);
 	lp->lnd_own = to;
 	lp->lnd_mobil = 0;
 	lp->lnd_mission = 0;
 	putland(ni.cur, lp);
     }
 
-    makelost(EF_SHIP, sp->shp_own, sp->shp_uid, sp->shp_x, sp->shp_y);
     sp->shp_own = to;
-    makenotlost(EF_SHIP, sp->shp_own, sp->shp_uid, sp->shp_x, sp->shp_y);
 }

src/lib/commands/coll.c
@@ -112,8 +112,6 @@ coll(void)
      * Used to call takeover() here a long time ago, but that does
      * unwanted things, like generate che.
      */
-    makelost(EF_SECTOR, sect.sct_own, 0, sect.sct_x, sect.sct_y);
-    makenotlost(EF_SECTOR, player->cnum, 0, sect.sct_x, sect.sct_y);
     sect.sct_own = player->cnum;
 
     memset(sect.sct_dist, 0, sizeof(sect.sct_dist));

src/lib/commands/edit.c
@@ -462,8 +462,6 @@ doland(char op, int arg, char *p, struct sctstr *sect)
 	   xyas(sect->sct_x, sect->sct_y, player->cnum),
 	   cname(sect->sct_own), sect->sct_own, cname(newown), newown);
 	if (sect->sct_own) {
-	    makelost(EF_SECTOR, sect->sct_own, 0,
-		     sect->sct_x, sect->sct_y);
 	    wu(player->cnum, sect->sct_own,
 	       "Sector %s lost to deity intervention\n",
 	       xyas(sect->sct_x, sect->sct_y, sect->sct_own));
@@ -471,7 +469,6 @@ doland(char op, int arg, char *p, struct sctstr *sect)
 	benefit(sect->sct_own, 0);
 	sect->sct_own = newown;
 	if (newown) {
-	    makenotlost(EF_SECTOR, newown, 0, sect->sct_x, sect->sct_y);
 	    wu(player->cnum, newown,
 	       "Sector %s gained from deity intervention\n",
 	       xyas(sect->sct_x, sect->sct_y, sect->sct_own));
@@ -777,11 +774,7 @@ doship(char op, int arg, char *p, struct shpstr *ship)
 	if (arg && arg < MAXNOC) {
 	    wu(player->cnum, (natid)arg,
 	       "%s given to you by deity intervention!\n", prship(ship));
-	    makelost(EF_SHIP, ship->shp_own, ship->shp_uid,
-		     ship->shp_x, ship->shp_y);
 	    ship->shp_own = (natid)arg;
-	    makenotlost(EF_SHIP, ship->shp_own, ship->shp_uid,
-			ship->shp_x, ship->shp_y);
 	} else if (!arg)
 	    ship->shp_effic = 0;
 	break;
@@ -882,11 +875,7 @@ dounit(char op, int arg, char *p, struct lndstr *land)
 	if (arg && arg < MAXNOC) {
 	    wu(player->cnum, (natid)arg,
 	       "%s given to you by deity intervention!\n", prland(land));
-	    makelost(EF_LAND, land->lnd_own, land->lnd_uid,
-		     land->lnd_x, land->lnd_y);
 	    land->lnd_own = (natid)arg;
-	    makenotlost(EF_LAND, land->lnd_own, land->lnd_uid,
-			land->lnd_x, land->lnd_y);
 	} else if (!arg)
 	    land->lnd_effic = 0;
 	break;
@@ -1009,11 +998,7 @@ doplane(char op, int arg, char *p, struct plnstr *plane)
 	       "%s taken from you by deity intervention!\n",
 	       prplane(plane));
 	if (arg && arg < MAXNOC) {
-	    makelost(EF_PLANE, plane->pln_own, plane->pln_uid,
-		     plane->pln_x, plane->pln_y);
 	    plane->pln_own = (natid)arg;
-	    makenotlost(EF_PLANE, plane->pln_own, plane->pln_uid,
-			plane->pln_x, plane->pln_y);
 	    wu(player->cnum, plane->pln_own,
 	       "%s given to you by deity intervention!\n", prplane(plane));
 	} else if (!arg)

src/lib/commands/load.c
@@ -316,10 +316,7 @@ gift(natid givee, natid giver, void *ptr, char *mesg)
 
     if (giver != givee)
 	wu(0, givee, "%s %s %s\n", cname(giver), obj_nameof(gen), mesg);
-
-    makelost(gen->ef_type, gen->own, gen->uid, gen->x, gen->y);
     gen->own = givee;
-    makenotlost(gen->ef_type, gen->own, gen->uid, gen->x, gen->y);
 }
 
 static int

src/lib/commands/scra.c
@@ -196,11 +196,7 @@ scra(void)
 			   "%s given to you by %s\n", prplane(&plane),
 			   cname(player->cnum));
 		    }
-		    makelost(EF_PLANE, plane.pln_own, plane.pln_uid,
-			     plane.pln_x, plane.pln_y);
 		    plane.pln_own = sect2.sct_own;
-		    makenotlost(EF_PLANE, plane.pln_own, plane.pln_uid,
-				plane.pln_x, plane.pln_y);
 		    putplane(plane.pln_uid, &plane);
 		}
 	    }
@@ -222,11 +218,7 @@ scra(void)
 			   "%s given to you by %s\n", prland(&land),
 			   cname(player->cnum));
 		    }
-		    makelost(EF_LAND, land.lnd_own, land.lnd_uid,
-			     land.lnd_x, land.lnd_y);
 		    land.lnd_own = sect2.sct_own;
-		    makenotlost(EF_LAND, land.lnd_own, land.lnd_uid,
-				land.lnd_x, land.lnd_y);
 		    putland(land.lnd_uid, &land);
 		}
 	    }
@@ -261,11 +253,7 @@ scra(void)
 			   "%s given to you by %s\n", prland(&land),
 			   cname(player->cnum));
 		    }
-		    makelost(EF_LAND, land.lnd_own, land.lnd_uid,
-			     land.lnd_x, land.lnd_y);
 		    land.lnd_own = sect2.sct_own;
-		    makenotlost(EF_LAND, land.lnd_own, land.lnd_uid,
-				land.lnd_x, land.lnd_y);
 		    putland(land.lnd_uid, &land);
 		}
 	    }
@@ -288,11 +276,7 @@ scra(void)
 			   "%s given to you by %s\n", prplane(&plane),
 			   cname(player->cnum));
 		    }
-		    makelost(EF_PLANE, plane.pln_own, plane.pln_uid,
-			     plane.pln_x, plane.pln_y);
 		    plane.pln_own = sect2.sct_own;
-		    makenotlost(EF_PLANE, plane.pln_own, plane.pln_uid,
-				plane.pln_x, plane.pln_y);
 		    putplane(plane.pln_uid, &plane);
 		}
 	    }

commit fe695140678bf252b37ca5b3458e32cab601f3d5

Capital obliteration failed to charge money and report news
Commit 221e88f1 (v4.3.15) made caploss() expect the new owner in
sct_own, so it can distinguish between sack and obliteration.  Commit
0d139ee1 (v4.3.17) broke that in sct_prewrite(): it moved zapping the
sector owner after caploss(), and passed 0 instead of the old owner to
caploss().  Because of that, obliterating a capital didn't make the
news and didn't cost the victim money.  Fix that.

src/lib/subs/sect.c
@@ -65,7 +65,7 @@ sct_prewrite(int id, void *old, void *new)
     struct sctstr *oldsp = old;
     struct sctstr *sp = new;
     int mil, civs;
-    natid own;
+    natid own, prev_own;
 
     bridge_damaged(sp);
     item_prewrite(sp->sct_item);
@@ -73,6 +73,7 @@ sct_prewrite(int id, void *old, void *new)
     mil = sp->sct_item[I_MILIT];
     civs = sp->sct_item[I_CIVIL];
     own = sp->sct_own;
+    prev_own = oldsp->sct_own;
 
     if (own && !civs) {
 	sp->sct_work = 100;
@@ -82,18 +83,15 @@ sct_prewrite(int id, void *old, void *new)
     if (own && !civs && !mil && !has_units(sp->sct_x, sp->sct_y, own, NULL)
 	&& !(sp->sct_flags & MOVE_IN_PROGRESS)) {
 	/* more cruft! */
-	own = 0;
+	own = sp->sct_own = 0;
 	sp->sct_mobil = 0;
 	if (sp->sct_type == SCT_CAPIT || sp->sct_type == SCT_MOUNT)
-	    caploss(sp, own, "");
+	    caploss(sp, prev_own, "");
     }
 
-    /* We've avoided assigning to sp->sct_own, in case oldsp == sp */
-    if (oldsp->sct_own != own)
-	lost_and_found(EF_SECTOR, oldsp->sct_own, own,
+    if (prev_own != own)
+	lost_and_found(EF_SECTOR, prev_own, own,
 		       0, sp->sct_x, sp->sct_y);
-
-    sp->sct_own = own;
 }
 
 void