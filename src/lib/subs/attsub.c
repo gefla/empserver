gefla/empserver

commit 2673a258fe6afdb71b028927c8bcd1bfe358d862

(ask_olist): Let land units attack as long as they have positive mobility, except for high-mobility terrain (mountains), where the rules remain as they were: land units need to have all the mobility charged for the attack, not counting combat and moving in to occupy. Rationale: Making sure your land units reach attack positions with enough mobility left is a pain in the neck.  Requiring only positive mobility is friendlier, but allows rushing of mountains.

src/lib/subs/attsub.c
@@ -1037,13 +1037,29 @@ ask_olist(int combat_mode, struct combat *off, struct combat *def,
 	}
 	switch (combat_mode) {
 	case A_ATTACK:
-	    mobcost = lnd_pathcost(&land,
-				   att_mobcost(off->own, def,
-					       lnd_mobtype(&land)));
-	    if (land.lnd_mobil < mobcost) {
-		pr("%s does not have enough mobility (%d needed)\n",
-		   prland(&land), (int)ceil(mobcost));
-		continue;
+	    /*
+	     * We used to let land units attack only if they have the
+	     * mobility consumed by the attack, not counting combat
+	     * and moving in to occupy.  Making sure your land units
+	     * reach attack positions with enough mobility left is a
+	     * pain in the neck.  We now require positive mobility,
+	     * just like for marching.  Except we don't allow rushing
+	     * of high-mobility sectors (mountains): for those we
+	     * still require attack mobility.
+	     */
+	    mobcost = att_mobcost(off->own, def, lnd_mobtype(&land));
+	    if (mobcost < 1.0) {
+		if (land.lnd_mobil <= 0) {
+		    pr("%s is out of mobility\n", prland(&land));
+		    continue;
+		}
+	    } else {
+		mobcost = lnd_pathcost(&land, mobcost);
+		if (land.lnd_mobil < mobcost) {
+		    pr("%s does not have enough mobility (%d needed)\n",
+		       prland(&land), (int)ceil(mobcost));
+		    continue;
+		}
 	    }
 	    break;
 	case A_ASSAULT:

commit 350a392dd5ce8420a6c0f88a3e4b460a4ebeb9f7

Fix land unit attack mobility cost
Land units were erroneously charged the much lower raw path cost,
except when attacking high-mobility terrain (mountains).  Broken in
commit 2673a258, v4.3.6.

src/lib/subs/attsub.c
@@ -966,7 +966,7 @@ ask_olist(int combat_mode, struct combat *off, struct combat *def,
 {
     struct nstr_item ni;
     struct lndstr land;
-    double mobcost;
+    double pathcost, mobcost;
     struct ulist *llp;
     struct lchrstr *lcp;
     double att_val;
@@ -1040,14 +1040,14 @@ ask_olist(int combat_mode, struct combat *off, struct combat *def,
 	     * of high-mobility sectors (mountains): for those we
 	     * still require attack mobility.
 	     */
-	    mobcost = att_mobcost(off->own, def, lnd_mobtype(&land));
-	    if (mobcost < 1.0) {
+	    pathcost = att_mobcost(off->own, def, lnd_mobtype(&land));
+	    mobcost = lnd_pathcost(&land, pathcost);
+	    if (pathcost < 1.0) {
 		if (land.lnd_mobil <= 0) {
 		    pr("%s is out of mobility\n", prland(&land));
 		    continue;
 		}
 	    } else {
-		mobcost = lnd_pathcost(&land, mobcost);
 		if (land.lnd_mobil < mobcost) {
 		    pr("%s does not have enough mobility (%d needed)\n",
 		       prland(&land), (int)ceil(mobcost));