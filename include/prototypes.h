gefla/empserver

commit c7e2442d3a8f63bdbcd9b3e1a72aa8db5ea470f9

Prevent command from executing after game is down
Move the gamedown() check from status to may_play_now() so it is
checked upon login, before a command is executed and after command
completion.  This fixes the situation where a player to could execute
one more command after the game was down.

Report to the deities that the game is down.

Remove to duplicate gamedown message.

include/prototypes.h
@@ -365,7 +365,6 @@ extern int dispatch(char *, char *);
 extern int getcommand(char *);
 extern void init_player_commands(void);
 extern void log_last_commands(void);
-extern int gamedown(void);
 extern void update_timeused_login(time_t now);
 extern void update_timeused(time_t now);
 extern void enforce_minimum_session_time(void);

src/lib/player/empdis.c
@@ -180,15 +180,13 @@ disable_coms(void)
 /*
  * returns true if down
  */
-int
-gamedown(void)
+static int
+gamedown(int suppress_deity_message)
 {
     FILE *down_fp;
     struct telstr tgm;
     char buf[MAXTELSIZE + 1];	/* UTF-8 */
 
-    if (player->god)
-	return 0;
     if ((down_fp = fopen(downfil, "rb")) == NULL)
 	return 0;
     if (fread(&tgm, sizeof(tgm), 1, down_fp) != 1) {
@@ -207,9 +205,14 @@ gamedown(void)
 	return 1;
     }
     buf[tgm.tel_length] = 0;
+    fclose(down_fp);
+    if (player->god) {
+	if (!suppress_deity_message)
+	    pr("The game is down\n");
+	return 0;
+    }
     uprnf(buf);
     pr("\nThe game is down\n");
-    fclose(down_fp);
     return 1;
 }
 
@@ -278,6 +281,9 @@ may_play_now(struct natstr *natp, time_t now,
 	if (natp->nat_stat != STAT_GOD)
 	    return 0;
     }
+
+    if (gamedown(suppress_deity_message))
+	return 0;
 
     if ((natp->nat_stat != STAT_GOD && natp->nat_stat != STAT_VIS)
 	&& natp->nat_timeused > m_m_p_d * 60) {

src/lib/player/player.c
@@ -198,10 +198,6 @@ status(void)
     if (natp->nat_stat == STAT_ACTIVE && (player->nstat & CAP) == 0)
 	pr("You lost your capital... better designate one (see info capital)\n");
     putnat(natp);
-    if (gamedown() && !player->god) {
-	pr("gamedown\n");
-	return 0;
-    }
     return 1;
 }
 

commit d2cd46ce204349e00097322cc9952ffaf1a85908

Don't log out deity when gamedown() can't read downfil
Broken in commit c7e2442d.  Fix by factoring show_first_tel() out of
gamedown() and show_motd().

include/prototypes.h
@@ -382,6 +382,7 @@ extern int natbyname(char *, natid *);
 extern int natpass(int, char *);
 /* player.c */
 extern struct player *player;	/* current player's context */
+extern int show_first_tel(char *);
 extern char *praddr(struct player *);
 extern void player_main(struct player *);
 /* more under Commands */

src/lib/player/empdis.c
@@ -48,7 +48,6 @@
 #include "player.h"
 #include "proto.h"
 #include "prototypes.h"
-#include "tel.h"
 
 
 #define KEEP_COMMANDS 50
@@ -184,40 +183,14 @@ disable_coms(void)
 static int
 gamedown(int suppress_deity_message)
 {
-    FILE *down_fp;
-    struct telstr tgm;
-    char buf[MAXTELSIZE + 1];	/* UTF-8 */
-
     if (!game_play_disabled())
 	return 0;
-
-    if ((down_fp = fopen(downfil, "rb")) == NULL) {
-	logerror("Could not open downfil.\n");
-	return 1;
-    }
-    if (fread(&tgm, sizeof(tgm), 1, down_fp) != 1) {
-	logerror("bad header on login message (downfil)");
-	fclose(down_fp);
-	return 1;
-    }
-    if (tgm.tel_length >= (long)sizeof(buf)) {
-	logerror("text length (%ld) is too long for login message (downfil)", tgm.tel_length);
-	fclose(down_fp);
-	return 1;
-    }
-    if (fread(buf, tgm.tel_length, 1, down_fp) != 1) {
-	logerror("bad length %ld on login message", tgm.tel_length);
-	fclose(down_fp);
-	return 1;
-    }
-    buf[tgm.tel_length] = 0;
-    fclose(down_fp);
     if (player->god) {
 	if (!suppress_deity_message)
 	    pr("The game is down\n");
 	return 0;
     }
-    uprnf(buf);
+    show_first_tel(downfil);
     pr("\nThe game is down\n");
     return 1;
 }

src/lib/player/player.c
@@ -257,37 +257,48 @@ execute(void)
 int
 show_motd(void)
 {
-    FILE *motd_fp;
+    show_first_tel(motdfil);
+    return RET_OK;
+}
+
+/*
+ * Print first telegram in file FNAME.
+ */
+int
+show_first_tel(char *fname)
+{
+    FILE *fp;
     struct telstr tgm;
     char buf[MAXTELSIZE + 1];	/* UTF-8 */
 
-    if ((motd_fp = fopen(motdfil, "rb")) == NULL) {
+    if ((fp = fopen(fname, "rb")) == NULL) {
 	if (errno == ENOENT)
-	    return RET_OK;
+	    return 0;
 	else {
-	    logerror("Could not open motd (%s).\n", motdfil);
-	    return RET_OK;
+	    logerror("Could not open %s.\n", fname);
+	    return -1;
 	}
     }
-    if (fread(&tgm, sizeof(tgm), 1, motd_fp) != 1) {
-	logerror("bad header on login message (motdfil)");
-	fclose(motd_fp);
-	return RET_OK;
+    if (fread(&tgm, sizeof(tgm), 1, fp) != 1) {
+	logerror("bad header on login message (%s)", fname);
+	fclose(fp);
+	return -1;
     }
     if (tgm.tel_length >= (long)sizeof(buf)) {
-	logerror("text length (%ld) is too long for login message (motdfil)", tgm.tel_length);
-	fclose(motd_fp);
-	return RET_OK;
+	logerror("text length (%ld) is too long for login message (%s)",
+		 tgm.tel_length, fname);
+	fclose(fp);
+	return -1;
     }
-    if (fread(buf, tgm.tel_length, 1, motd_fp) != 1) {
+    if (fread(buf, tgm.tel_length, 1, fp) != 1) {
 	logerror("bad length %ld on login message", tgm.tel_length);
-	fclose(motd_fp);
-	return RET_OK;
+	fclose(fp);
+	return -1;
     }
     buf[tgm.tel_length] = 0;
     uprnf(buf);
-    fclose(motd_fp);
-    return RET_OK;
+    fclose(fp);
+    return 0;
 }
 
 int