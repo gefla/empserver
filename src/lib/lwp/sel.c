gefla/empserver

commit 08b94556826ec883ab68c736ad8494118511446c

Reimplement max_idle without a separate thread
Remove the KillIdle thread.  Add timeout to struct iop, initialized in
io_open().  Obey it in io_input() by passing it to empth_select().  If
empth_select() times out, report that back through io_input() to
recvclient() and player_login().  If player_login() receives a timeout
indication, print a message and terminate the session.  If
recvclient() receives a timeout indication, flash a message to the
player and initiate a shut down the player's session.

Create WIN32 sys/time.h to define struct timeval.  This creates some
conflicts with WIN32 windows.h definitions.  Including windows.h in
show.c and info.c creates conflicts, so remove that.  Modify service.c
to include sys/socket.h instead of windows.h to remove the conflict
with sys/time.h.

src/lib/lwp/sel.c
@@ -55,7 +55,10 @@ static fd_set LwpReadfds, LwpWritefds;
 /* Map file descriptor to thread sleeping in lwpSleepFd() */
 static struct lwpProc **LwpFdwait;
 
-/* Threads sleeping in lwpSleepUntil(), in no particular order */
+/*
+ * Threads sleeping until a wakeup time, in lwpSleepUntil() or
+ * lwpSleepFd(), in no particular order
+ */
 static struct lwpQueue LwpDelayq;
 
 /* The thread executing lwpSelect() */
@@ -74,23 +77,25 @@ lwpInitSelect(struct lwpProc *proc)
     LwpSelProc = proc;
 }
 
-void
-lwpSleepFd(int fd, int mask)
+int
+lwpSleepFd(int fd, int mask, struct timeval *timeout)
 {
     lwpStatus(LwpCurrent, "sleeping on fd %d for %d", fd, mask);
 
-    if (CANT_HAPPEN(fd > FD_SETSIZE))
-	return;
+    if (CANT_HAPPEN(fd > FD_SETSIZE)) {
+	errno = EBADF;
+	return -1;
+    }
     if (LwpFdwait[fd] != 0) {
 	lwpStatus(LwpCurrent,
 		  "multiple sleeps attempted on file descriptor %d", fd);
-	return;
+	errno = EBADF;
+	return -1;
     }
     if (mask & LWP_FD_READ)
 	FD_SET(fd, &LwpReadfds);
     if (mask & LWP_FD_WRITE)
 	FD_SET(fd, &LwpWritefds);
-
     LwpNfds++;
 
     if (LwpMaxfd == 0 && LwpDelayq.head == 0) {
@@ -99,20 +104,39 @@ lwpSleepFd(int fd, int mask)
 	lwpReady(LwpSelProc);
     }
     lwpStatus(LwpCurrent, "going to wait on fd %d", fd);
+
+    if (timeout) {
+	LwpCurrent->runtime = time(NULL) + timeout->tv_sec +
+	    (timeout->tv_usec > 0);
+	lwpAddTail(&LwpDelayq, LwpCurrent);
+    } else
+	LwpCurrent->runtime = (time_t)-1;
+
     if (fd > LwpMaxfd)
 	LwpMaxfd = fd;
     LwpFdwait[fd] = LwpCurrent;
     LwpCurrent->fd = fd;
+    LwpCurrent->fd_ready = 0;
     lwpReschedule();
+    return LwpCurrent->fd_ready != 0;
 }
 
+/*
+ * Wake up PROC if it is sleeping in lwpSleepFd().
+ * Must be followed by lwpWakeupSleep() before the next lwpReschedule().
+ */
 static void
 lwpWakeupFd(struct lwpProc *proc)
 {
-    if (proc->fd < 0)
+    if (CANT_HAPPEN(proc->fd < 0 || proc->fd > LwpMaxfd))
 	return;
 
     lwpStatus(proc, "awakening; was sleeping on fd %d", proc->fd);
+    if (proc->runtime != (time_t)-1) {
+	/* is in LwpDelayq; leave the job to lwpWakeupSleep() */
+	proc->runtime = 0;
+	return;
+    }
     FD_CLR(proc->fd, &LwpReadfds);
     FD_CLR(proc->fd, &LwpWritefds);
     LwpNfds--;
@@ -121,6 +145,9 @@ lwpWakeupFd(struct lwpProc *proc)
     lwpReady(proc);
 }
 
+/*
+ * Wake up threads in LwpDelayq whose time has come.
+ */
 void
 lwpWakeupSleep(void)
 {
@@ -134,7 +161,11 @@ lwpWakeupSleep(void)
 	while (NULL != (proc = lwpGetFirst(&LwpDelayq))) {
 	    if (now >= proc->runtime) {
 		lwpStatus(proc, "sleep done");
-		lwpReady(proc);
+		proc->runtime = (time_t)-1;
+		if (proc->fd >= 0)
+		    lwpWakeupFd(proc);
+		else
+		    lwpReady(proc);
 	    } else {
 		lwpAddTail(&save, proc);
 	    }
@@ -148,10 +179,9 @@ lwpWakeup(struct lwpProc *proc)
 {
     if (proc->fd >= 0)
 	lwpWakeupFd(proc);
-    else if (proc->runtime != (time_t)-1) {
+    else if (proc->runtime != (time_t)-1)
 	proc->runtime = 0;
-	lwpWakeupSleep();
-    }
+    lwpWakeupSleep();
 }
 
 int
@@ -230,7 +260,6 @@ lwpSelect(void *arg)
 	    continue;
 	}
 
-	lwpWakeupSleep();
 	if (n > 0) {
 	    /* file descriptor activity */
 	    for (fd = 0; fd <= LwpMaxfd; fd++) {
@@ -238,16 +267,19 @@ lwpSelect(void *arg)
 		    continue;
 		if (FD_ISSET(fd, &readmask)) {
 		    lwpStatus(LwpFdwait[fd], "input ready");
+		    LwpFdwait[fd]->fd_ready = 1;
 		    lwpWakeupFd(LwpFdwait[fd]);
 		    continue;
 		}
 		if (FD_ISSET(fd, &writemask)) {
 		    lwpStatus(LwpFdwait[fd], "output ready");
+		    LwpFdwait[fd]->fd_ready = 1;
 		    lwpWakeupFd(LwpFdwait[fd]);
 		    continue;
 		}
 	    }
 	}
+	lwpWakeupSleep();
 	lwpStatus(us, "fd dispatch completed");
 	lwpReady(LwpCurrent);
 	lwpReschedule();

include/empio.h
@@ -33,6 +33,7 @@
 
 #ifndef EMPIO_H
 #define EMPIO_H
+#include <sys/time.h>
 
 #define	IO_READ		0x1
 #define IO_WRITE	0x2
@@ -46,7 +47,7 @@
 #define IO_NOWAIT	0
 #define IO_WAIT		1
 
-extern struct iop *io_open(int, int, int);
+extern struct iop *io_open(int, int, int, struct timeval);
 extern void io_init(void);
 extern int io_noblocking(struct iop *, int);
 extern void io_close(struct iop *);

include/empthread.h
@@ -48,6 +48,7 @@
 #ifndef EMPTHREAD_H
 #define EMPTHREAD_H
 
+#include <sys/time.h>
 #include <time.h>
 
 #ifdef EMPTH_LWP
@@ -168,10 +169,13 @@ void empth_terminate(empth_t *thread);
  * If FLAGS & EMPTH_FD_READ, wake up if FD is ready for input.
  * If FLAGS & EMPTH_FD_WRITE, wake up if FD is ready for output.
  * At most one thread may sleep on the same file descriptor.
+ * TIMEOUT, if non-null, limits the sleep time.
+ * Return one when the FD is ready, zero on timeout, -1 on error with
+ * errno set.
  * Note: Currently, Empire sleeps only on network I/O, i.e. FD is a
  * socket.  Implementations should not rely on that.
  */
-void empth_select(int fd, int flags);
+int empth_select(int fd, int flags, struct timeval *timeout);
 
 /*
  * Awaken THREAD if it is sleeping in empth_select() or empth_sleep().

include/lwp.h
@@ -45,7 +45,7 @@ struct lwpProc *lwpCreate(int prio, void (*)(void *), int size,
 void lwpExit(void);
 void lwpTerminate(struct lwpProc * p);
 void lwpYield(void);
-void lwpSleepFd(int fd, int flags);
+int lwpSleepFd(int fd, int flags, struct timeval *timeout);
 int lwpSleepUntil(time_t until);
 void lwpWakeup(struct lwpProc *);
 int lwpSigWait(sigset_t *set, int *sig);

src/lib/commands/info.c
@@ -43,8 +43,6 @@
 #include <stdio.h>
 #if !defined(_WIN32)
 #include <dirent.h>
-#else
-#include <windows.h>
 #endif
 #include "commands.h"
 #include "optlist.h"

src/lib/empthread/io.c
@@ -61,6 +61,7 @@ struct iop {
     struct ioqueue *output;
     int flags;
     int bufsize;
+    struct timeval input_timeout;
 };
 
 void
@@ -69,7 +70,7 @@ io_init(void)
 }
 
 struct iop *
-io_open(int fd, int flags, int bufsize)
+io_open(int fd, int flags, int bufsize, struct timeval timeout)
 {
     struct iop *iop;
 
@@ -83,6 +84,7 @@ io_open(int fd, int flags, int bufsize)
     iop->input = 0;
     iop->output = 0;
     iop->flags = 0;
+    iop->input_timeout = timeout;
     iop->bufsize = bufsize;
     if ((flags & IO_READ) && (flags & IO_NEWSOCK) == 0)
 	iop->input = ioq_create(bufsize);
@@ -106,31 +108,47 @@ io_close(struct iop *iop)
     free(iop);
 }
 
+/*
+ * Return number of bytes read on success, zero on timeout or EOF, -1
+ * on error, with errno set appropriately.  In particular, return -1
+ * with errno set to EAGAIN or EWOULDBLOCK when no data is available
+ * for non-blocking input (WAITFORINPUT false).  Use io_eof() to
+ * distinguish timeout from EOF.
+ */
 int
 io_input(struct iop *iop, int waitforinput)
 {
     char buf[IO_BUFSIZE];
     int cc;
+    int res;
+    struct timeval timeout = iop->input_timeout;
 
     /* Not a read IOP */
-    if ((iop->flags & IO_READ) == 0)
+    if ((iop->flags & IO_READ) == 0) {
+	errno = EBADF;
 	return -1;
+    }
     /* IOP is markes as in error. */
-    if (iop->flags & IO_ERROR)
+    if (iop->flags & IO_ERROR) {
+	errno = EBADF;
 	return -1;
+    }
     /* Wait for the file to have input. */
     if (waitforinput) {
-	empth_select(iop->fd, EMPTH_FD_READ);
+	res = empth_select(iop->fd, EMPTH_FD_READ, &timeout);
+	if (res < 0) {
+	    iop->flags |= IO_ERROR;
+	    return -1;
+	} else if (res == 0)
+	    return 0;
     }
+
     /* Do the actual read. */
     cc = read(iop->fd, buf, sizeof(buf));
     if (cc < 0) {
-	/* would block, so nothing to read. */
-	if (errno == EAGAIN || errno == EWOULDBLOCK)
-	    return 0;
-
-	/* Some form of file error occurred... */
-	iop->flags |= IO_ERROR;
+	if (errno != EAGAIN && errno != EWOULDBLOCK)
+	    /* Some form of file error occurred... */
+	    iop->flags |= IO_ERROR;
 	return -1;
     }
 
@@ -190,7 +208,7 @@ io_output(struct iop *iop, int waitforoutput)
     if (waitforoutput != IO_NOWAIT) {
 	/* This waits for the file to be ready for writing, */
 	/* and lets other threads run. */
-	empth_select(iop->fd, EMPTH_FD_WRITE);
+	empth_select(iop->fd, EMPTH_FD_WRITE, NULL);
     }
 
     /* Do the actual write. */
@@ -272,7 +290,7 @@ io_output_all(struct iop *iop)
      * a malicous player could delay the update indefinitely
      */
     while ((n = io_output(iop, IO_NOWAIT)) > 0 && !play_wrlock_wanted)
-	empth_select(iop->fd, EMPTH_FD_WRITE);
+	empth_select(iop->fd, EMPTH_FD_WRITE, NULL);
 
     return n;
 }

src/lib/empthread/lwp.c
@@ -104,10 +104,10 @@ empth_terminate(empth_t *a)
     lwpTerminate(a);
 }
 
-void
-empth_select(int fd, int flags)
+int
+empth_select(int fd, int flags, struct timeval *timeout)
 {
-    lwpSleepFd(fd, flags);
+    return lwpSleepFd(fd, flags, timeout);
 }
 
 void

src/lib/empthread/ntthread.c
@@ -560,12 +560,14 @@ empth_terminate(empth_t *pThread)
  *
  * This would be one of the main functions used within gen\io.c
  */
-void
-empth_select(int fd, int flags)
+int
+empth_select(int fd, int flags, struct timeval *timeout)
 {
     int handle;
     WSAEVENT hEventObject[2];
+    DWORD result, msec;
     empth_t *pThread = TlsGetValue(dwTLSIndex);
+    int res;
 
     loc_debug("%s select on %d",
 	      flags == EMPTH_FD_READ ? "read" : "write", fd);
@@ -586,13 +588,31 @@ empth_select(int fd, int flags)
 	empth_exit();
     }
 
-    WSAWaitForMultipleEvents(2, hEventObject, FALSE, WSA_INFINITE, FALSE);
+    if (timeout)
+	msec = timeout->tv_sec * 1000L + timeout->tv_usec / 1000L;
+    else
+	msec = WSA_INFINITE;
+    result = WSAWaitForMultipleEvents(2, hEventObject, FALSE, msec,
+	FALSE);
+
+    switch (result) {
+    case WSA_WAIT_TIMEOUT:
+	res = 0;
+	break;
+    case WSA_WAIT_FAILED:
+	errno = WSAGetLastError();
+	res = -1;
+	break;
+    default:
+	res = 1;
+    }
 
     WSAEventSelect(handle, hEventObject[0], 0);
 
     WSACloseEvent(hEventObject[0]);
 
     loc_RunThisThread(NULL);
+    return res;
 }
 
 /************************

commit 4c32aac8326b818be020de49aa9498ede23ebc68

Fix update thread not successfully waking up with LWP
Commit 08b945568 broke empth_sleep() for LWP: it returned zero even
when woken up early.

src/lib/lwp/sel.c
@@ -132,9 +132,9 @@ lwpWakeupFd(struct lwpProc *proc)
 	return;
 
     lwpStatus(proc, "awakening; was sleeping on fd %d", proc->fd);
-    if (proc->runtime != (time_t)-1) {
+    if (proc->runtime != (time_t)-1 && proc->runtime != 0) {
 	/* is in LwpDelayq; leave the job to lwpWakeupSleep() */
-	proc->runtime = 0;
+	proc->runtime = (time_t)-1;
 	return;
     }
     FD_CLR(proc->fd, &LwpReadfds);
@@ -161,7 +161,8 @@ lwpWakeupSleep(void)
 	while (NULL != (proc = lwpGetFirst(&LwpDelayq))) {
 	    if (now >= proc->runtime) {
 		lwpStatus(proc, "sleep done");
-		proc->runtime = (time_t)-1;
+		if (proc->runtime != 0)
+		    proc->runtime = (time_t)-1;
 		if (proc->fd >= 0)
 		    lwpWakeupFd(proc);
 		else