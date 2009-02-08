gefla/empserver

commit df7dc203c47ecc39e3492cc2c2ba9a0128171646

(turn): Change to stdio.
(turn): Always close the file after attempting to write.
Fixes bug where the file is not close when there is writing
error.

src/lib/commands/turn.c
@@ -46,7 +46,7 @@
 int
 turn(void)
 {
-    int fd;
+    FILE *fptr;
     struct telstr tgm;
     char *p;
     char buf[MAXTELSIZE];
@@ -95,8 +95,8 @@ turn(void)
 	    pr("Writing empty no-login message.\n");
     }
 
-    fd = open(msgfilepath, O_RDWR | O_CREAT | O_TRUNC, 0660);
-    if (fd == -1) {
+    fptr = fopen(msgfilepath, "w");
+    if (fptr == NULL) {
 	pr("Something went wrong opening the message file.\n");
 	logerror("Could not open message file (%s).\n", msgfilepath);
 	return RET_SYS;
@@ -105,14 +105,15 @@ turn(void)
     if (msgfilepath == downfil)
 	pr("Logins disabled.\n");
 
-    if ((write(fd, &tgm, sizeof(tgm)) < (ssize_t)sizeof(tgm)) ||
-	(write(fd, buf, tgm.tel_length) < tgm.tel_length) ||
-	(close(fd) == -1)) {
+    if ((fwrite((void *)&tgm, sizeof(tgm), 1, fptr) != 1) ||
+	(fwrite((void *)buf, tgm.tel_length, 1, fptr) != 1)) {
+	fclose(fptr);
 	pr("Something went wrong writing the message file.\n");
 	logerror("Could not properly write message file (%s).\n",
 	    msgfilepath);
 	return RET_SYS;
     }
+    fclose(fptr);
     pr("\n");
 
     return RET_OK;

commit 421119e1f1904798def7f635b76a4235b63aee28

Fix turn off for empty message
Always failed with an empty message, due to misuse of fwrite().
Broken in commit df7dc203, v4.2.20.

src/lib/commands/turn.c
@@ -111,8 +111,8 @@ turn(void)
     memset(&tgm, 0, sizeof(tgm));
     time(&tgm.tel_date);
     tgm.tel_length = len;
-    if ((fwrite(&tgm, sizeof(tgm), 1, fptr) != 1) ||
-	(fwrite(msgbuf, tgm.tel_length, 1, fptr) != 1)) {
+    if (fwrite(&tgm, sizeof(tgm), 1, fptr) != 1 ||
+	fwrite(msgbuf, 1, tgm.tel_length, fptr) != tgm.tel_length) {
 	fclose(fptr);
 	pr("Something went wrong writing the message file.\n");
 	logerror("Could not properly write message file (%s).\n",