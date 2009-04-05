gefla/empserver

commit 8b7d0b915d73a80f63743726a1dc7236f5bffa57

Rewrite much of client's playing phase code: (EOF_COOKIE, INTR_COOKIE, input_fd, send_intr, recv_output) (recv_input, intr, play): New playing phase code.  No native Windows support yet.  Sends just one EOF cookie on EOF on standard input instead of up to three.  Old servers (before recvclient.c rev. 1.16) fail to terminate the session when they receive an EOF cookie at an argument prompt.  The session then hangs; use SIGINT to get out.  No longer blocks on sending input, which could deadlock the session. Closes #827090.  Fixes error handling for select().  Fixes race condition that could cause server output to be discarded on EOF on standard input. (main): Replace old playing phase code by a call to play(). (intr, sock, interrupt, handleintr): Replaced by play(), remove. [_WIN32] (hStdIn): Ditto. (auxfp): New. (servercmd, prompt, doexecute): Remove parameter auxfi, use auxfp. (eight_bit_clean): Move to servcmd.c. (servercmd): Work on a single non-C_DATA line instead of getting lines from an ioqueue. (servercmd, output, screen, outch): Deal with all ids in servercmd() rather than some there and some in output().  Don't treat C_NOECHO, C_ABORT, C_CMDERR, C_BADCMD specially.  Fix C_FLASH and C_EXIT to ignore redirections; they used to ignore them only for some parts. Replace output() by outch(), fold screen into outch(). (servercmd): Truncate long prompts and telegram infos to prevent buffer overflow. (prompt): Use new parameters code, prompt, teles instead of global variables mode, the_prompt, num_teles. (num_teles, the_prompt, mode, nbtu, nmin): Remove. (prompt): Don't write an empty line before argument prompts to auxfp. (servercmd): Don't strip newline from redirections and execute, doredir(), dopipe() and doexecute() need it now. (doredir, dopipe, doexecute): Use new seen_input() instead of gettag(). (doexecute): Set input_fd and leave reading the script file to play(). (serverio, termio, sendeof): Replaced by play(), remove. (LBUF_LEN_MAX, lbuf, lbuf_init, lbuf_

src/client/play.c
@@ -1,0 +1,316 @@
+/*
+ *  Empire - A multi-player, client/server Internet based war game.
+ *  Copyright (C) 1986-2007, Dave Pare, Jeff Bailey, Thomas Ruschak,
+ *                           Ken Stevens, Steve McClure
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  ---
+ *
+ *  See files README, COPYING and CREDITS in the root of the source
+ *  tree for related information and legal notices.  It is expected
+ *  that future projects/authors will amend these files as needed.
+ *
+ *  ---
+ *
+ *  play.c: Playing the game
+ * 
+ *  Known contributors to this file:
+ *     Markus Armbruster, 2007
+ */
+
+#include <config.h>
+
+#include <assert.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "linebuf.h"
+#include "misc.h"
+#include "proto.h"
+#include "ringbuf.h"
+#include "secure.h"
+
+#define EOF_COOKIE "ctld\n"
+#define INTR_COOKIE "\naborted\n"
+
+int input_fd;
+static volatile sig_atomic_t send_intr; /* need to send INTR_COOKIE */
+
+/*
+ * Receive and process server output from SOCK.
+ * Return number of characters received on success, -1 on error.
+ */
+static int
+recv_output(int sock)
+{
+    /*
+     * Read a chunk of server output and feed its characters into a
+     * simple state machine.
+     * Initial state is SCANNING_ID.
+     * In state SCANNING_ID, buffer the character.  If it's a space,
+     * decode the id that has been buffered, and enter state BUFFERING
+     * or COPYING depending on its value.
+     * In state BUFFERING, buffer the character.  If it's newline,
+     * pass id and buffered text to servercmd(), then enter state
+     * SCANNING_ID.
+     * In state COPYING, pass the character to outch().  If it's
+     * newline, enter state SCANNING_ID.
+     */
+    static enum {
+	SCANNING_ID, BUFFERING, COPYING
+    } state = SCANNING_ID;
+    static int id;
+    static struct lbuf lbuf;
+    char buf[4096];
+    ssize_t n;
+    int i, ch, len;
+    char *line, *end;
+
+    n = read(sock, buf, sizeof(buf));
+    if (n < 0)
+	return -1;
+
+    for (i = 0; i < n; i++) {
+	ch = buf[i];
+	switch (state) {
+	case SCANNING_ID:
+	    if (ch == '\n') {
+		/* FIXME gripe unexpected! */
+		lbuf_init(&lbuf);
+		break;
+	    }
+	    if (ch != ' ') {
+		lbuf_putc(&lbuf, ch);
+		break;
+	    }
+	    line = lbuf_line(&lbuf);
+	    id = strtol(line, &end, 16);
+	    if (end == line || *end) {
+		/* FIXME gripe bad id */
+		id = -1;
+	    }
+	    lbuf_init(&lbuf);
+
+	    switch (id) {
+	    case C_PROMPT:
+	    case C_FLUSH:
+	    case C_EXECUTE:
+	    case C_EXIT:
+	    case C_FLASH:
+	    case C_INFORM:
+	    case C_PIPE:
+	    case C_REDIR:
+		state = BUFFERING;
+		break;
+	    default:
+		/* unknown or unexpected id, treat like C_DATA */
+	    case C_DATA:
+		state = COPYING;
+		break;
+	    }
+	    break;
+
+	case BUFFERING:
+	    len = lbuf_putc(&lbuf, ch);
+	    if (len) {
+		line = lbuf_line(&lbuf);
+		servercmd(id, line, len);
+		lbuf_init(&lbuf);
+		state = SCANNING_ID;
+	    }
+	    break;
+
+	case COPYING:
+	    outch(ch);
+	    if (ch == '\n')
+		state = SCANNING_ID;
+	}
+    }
+
+    return n;
+}
+
+/*
+ * Receive command input from FD into INBUF.
+ * Return 1 on receipt of input, zero on EOF, -1 on error.
+ */
+static int
+recv_input(int fd, struct ring *inbuf)
+{
+    static struct lbuf cmdbuf;
+    int n, i, ch;
+    char *line;
+    int res = 1;
+
+    n = ring_from_file(inbuf, fd);
+    if (n < 0)
+	return -1;
+    if (n == 0) {
+	/* EOF on input */
+	if (lbuf_len(&cmdbuf)) {
+	    /* incomplete line */
+	    ring_putc(inbuf, '\n');
+	    n++;
+	}
+	/*
+	 * Can't put EOF cookie into INBUF here, it may not fit.
+	 * Leave it to caller.
+	 */
+	res = 0;
+    }
+
+    /* copy input to AUXFP etc. */
+    for (i = -n; i < 0; i++) {
+	ch = ring_peek(inbuf, i);
+	assert(ch != EOF);
+	if (lbuf_putc(&cmdbuf, ch)) {
+	    line = lbuf_line(&cmdbuf);
+	    if (auxfp)
+		fputs(line, auxfp);
+	    save_input(line);
+	    lbuf_init(&cmdbuf);
+	}
+    }
+
+    return res;
+}
+
+static void
+intr(int sig)
+{
+    send_intr = 1;
+#ifdef _WIN32
+    signal(SIGINT, intr);
+#endif
+}
+
+/*
+ * Play on SOCK.
+ * The session must be in the playing phase.
+ * Return 0 when the session ended, -1 on error.
+ */
+int
+play(int sock)
+{
+    /*
+     * Player input flows from INPUT_FD through recv_input() into ring
+     * buffer INBUF, which drains into SOCK.  This must not block.
+     * Server output flows from SOCK into recv_output().  Reading SOCK
+     * must not block.
+     */
+    struct sigaction sa;
+    struct ring inbuf;		/* input buffer, draining to SOCK */
+    int eof_fd0;		/* read fd 0 hit EOF? */
+    int send_eof;		/* need to send EOF_COOKIE */
+    fd_set rdfd, wrfd;
+    int n;
+
+    sa.sa_flags = 0;
+    sa.sa_handler = intr;
+    sigaction(SIGINT, &sa, NULL);
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &sa, NULL);
+
+    ring_init(&inbuf);
+    eof_fd0 = send_eof = 0;
+
+    for (;;) {
+	FD_ZERO(&rdfd);
+	FD_ZERO(&wrfd);
+
+	/*
+	 * Want to read player input only when we don't need to send
+	 * cookies, and we haven't hit EOF on fd 0, and INBUF can
+	 * accept some.
+	 */
+	if (!send_intr && !send_eof && !eof_fd0 && ring_space(&inbuf))
+	    FD_SET(input_fd, &rdfd);
+	/* Want to send player input only when we have something */
+	if (send_intr || send_eof || ring_len(&inbuf))
+	    FD_SET(sock, &wrfd);
+	/* Always want to read server output */
+	FD_SET(sock, &rdfd);
+
+	n = select(MAX(input_fd, sock) + 1, &rdfd, &wrfd, NULL, NULL);
+	if (n < 0) {
+	    if (errno != EINTR) {
+		perror("select");
+		return -1;
+	    }
+	}
+
+	if (send_eof
+	    && ring_putm(&inbuf, EOF_COOKIE, sizeof(EOF_COOKIE) - 1) >= 0)
+	    send_eof = 0;
+	if (send_intr
+	    && ring_putm(&inbuf, INTR_COOKIE, sizeof(INTR_COOKIE) - 1) >= 0)
+	    send_intr = 0;
+
+	if (n < 0)
+	    continue;
+
+	/* read player input */
+	if (FD_ISSET(input_fd, &rdfd)) {
+	    n = recv_input(input_fd, &inbuf);
+	    if (n < 0) {
+		perror("read stdin"); /* FIXME stdin misleading, could be execing */
+		return -1;
+	    }
+	    if (n == 0) {
+		/* EOF on input */
+		send_eof = 1;
+		if (input_fd) {
+		    /* execute done, switch back to fd 0 */
+		    input_fd = 0;
+		} else {
+		    /* stop reading input, drain socket ring buffers */
+		    eof_fd0 = 1;
+		    sa.sa_handler = SIG_DFL;
+		    sigaction(SIGINT, &sa, NULL);
+		}
+	    }
+	}
+
+	/* send it to the server */
+	if (FD_ISSET(sock, &wrfd)) {
+	    n = ring_to_file(&inbuf, sock);
+	    if (n < 0) {
+		perror("write socket");
+		return -1;
+	    }
+	}
+
+	/* read server output and print it */
+	if (FD_ISSET(sock, &rdfd)) {
+	    n = recv_output(sock);
+	    if (n < 0) {
+		perror("read socket");
+		return -1;
+	    }
+	    if (n == 0)
+		return 0;
+	    if (input_fd < 0) {
+		/* execute failed */
+		input_fd = 0;
+		send_eof = 1;
+	    }
+	}
+    }
+}

src/client/Makefile.in
@@ -53,8 +53,8 @@ srcdir = @srcdir@
 VPATH = @srcdir@
 
 prog = empire$E
-obj = expect.$O host.$O ioqueue.$O ipglob.$O login.$O main.$O queue.$O	\
-servcmd.$O serverio.$O tags.$O termio.$O termlib.$O version.$O
+obj = expect.$O host.$O ipglob.$O linebuf.$O login.$O main.$O play.$O	\
+ringbuf.$O secure.$O servcmd.$O termlib.$O version.$O
 
 all: $(prog)
 
@@ -81,14 +81,13 @@ uninstall:
 # FIXME generate from .d
 expect.$O: misc.h
 host.$O: misc.h
-ioqueue.$O: misc.h queue.h ioqueue.h
+linebuf.$O: linebuf.h
 login.$O: misc.h proto.h
-main.$O: misc.h proto.h queue.h ioqueue.h tags.h version.h
-queue.$O: misc.h queue.h
-servcmd.$O: misc.h proto.h queue.h ioqueue.h tags.h
-serverio.$O: misc.h queue.h ioqueue.h
-tags.$O: misc.h tags.h
-termio.$O: misc.h tags.h
+main.$O: misc.h version.h
+play.$O: linebuf.h misc.h proto.h ringbuf.h secure.h
+ringbuf.$O: ringbuf.h
+secure.$O: ringbuf.h secure.h
+servcmd.$O: misc.h proto.h secure.h
 termlib.$O: misc.h
 version.$O: version.h
 $(obj): config.h

src/client/ioqueue.c
@@ -1,232 +1,0 @@
-/*
- *  Empire - A multi-player, client/server Internet based war game.
- *  Copyright (C) 1986-2007, Dave Pare, Jeff Bailey, Thomas Ruschak,
- *                           Ken Stevens, Steve McClure
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  ---
- *
- *  See files README, COPYING and CREDITS in the root of the source
- *  tree for related information and legal notices.  It is expected
- *  that future projects/authors will amend these files as needed.
- *
- *  ---
- *
- *  ioqueue.c: Manage an i/o queue
- * 
- *  Known contributors to this file:
- *     Steve McClure, 1998
- */
-
-#include <config.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "ioqueue.h"
-#include "misc.h"
-#include "queue.h"
-
-static int ioqtobuf(struct ioqueue *ioq, char *buf, int cc);
-static void enqueuecc(struct ioqueue *ioq, char *buf, int cc);
-static int dequeuecc(struct ioqueue *ioq, int cc);
-
-
-void
-ioq_init(struct ioqueue *ioq, int bsize)
-{
-    initque(&ioq->queue);
-    ioq->cc = 0;
-    ioq->bsize = bsize;
-}
-
-/*
- * Copy the specified number of characters into the buffer
- * provided, without actually dequeueing the data.  Return
- * number of bytes actually found.
- */
-int
-ioq_peek(struct ioqueue *ioq, char *buf, int cc)
-{
-    return ioqtobuf(ioq, buf, cc);
-}
-
-int
-ioq_dequeue(struct ioqueue *ioq, int cc)
-{
-    if (dequeuecc(ioq, cc) != cc)
-	return 0;
-    return cc;
-}
-
-int
-ioq_read(struct ioqueue *ioq, char *buf, int cc)
-{
-    int n;
-
-    n = ioqtobuf(ioq, buf, cc);
-    if (n > 0)
-	dequeuecc(ioq, n);
-    return n;
-}
-
-void
-ioq_write(struct ioqueue *ioq, char *buf, int cc)
-{
-    enqueuecc(ioq, buf, cc);
-}
-
-int
-ioq_qsize(struct ioqueue *ioq)
-{
-    return ioq->cc;
-}
-
-void
-ioq_drain(struct ioqueue *ioq)
-{
-    struct io *io;
-    struct qelem *qp;
-
-    while ((qp = ioq->queue.q_forw) != &ioq->queue) {
-	io = (struct io *)qp;
-	free(io->data);
-	(void)remque(&io->queue);
-	(void)free(io);
-    }
-    ioq->cc = 0;
-}
-
-char *
-ioq_gets(struct ioqueue *ioq, char *buf, int cc, int *eol)
-{
-    char *p;
-    char *end;
-    int nbytes;
-
-    *eol = 0;
-
-    cc--;
-
-    nbytes = ioqtobuf(ioq, buf, cc);
-    end = &buf[nbytes];
-    for (p = buf; p < end && *p; p++) {
-	if (*p == '\n') {
-	    *++p = '\0';
-	    *eol = 1;
-	    dequeuecc(ioq, p - buf);
-	    return buf;
-	}
-    }
-    if (cc && (p - buf) == cc) {
-    	dequeuecc(ioq, cc);
-	buf[cc] = '\0';
-	return buf;
-    }
-    return NULL;
-}
-
-/*
- * all the rest are local to this module
- */
-
-
-/*
- * copy cc bytes from ioq to buf.
- * this routine doesn't free memory; this is
- * left for a higher level.
- */
-static int
-ioqtobuf(struct ioqueue *ioq, char *buf, int cc)
-{
-    struct io *io;
-    struct qelem *qp;
-    char *offset;
-    int nbytes;
-    int nleft;
-
-    nleft = cc;
-    offset = buf;
-    for (qp = ioq->queue.q_forw; qp != &ioq->queue; qp = qp->q_forw) {
-	io = (struct io *)qp;
-	if ((nbytes = io->nbytes - io->offset) < 0) {
-	    fprintf(stderr, "ioqtobuf: offset %d nbytes %d\n",
-		    io->offset, io->nbytes);
-	    continue;
-	}
-	if (nbytes > 0) {
-	    if (nleft < nbytes)
-		nbytes = nleft;
-	    memcpy(offset, io->data + io->offset, nbytes);
-	    offset += nbytes;
-	    nleft -= nbytes;
-	}
-    }
-    return offset - buf;
-}
-
-/*
- * append a buffer to the end of the ioq.
- */
-static void
-enqueuecc(struct ioqueue *ioq, char *buf, int cc)
-{
-    struct io *io;
-
-    io = malloc(sizeof(*io));
-    io->nbytes = cc;
-    io->offset = 0;
-    io->data = buf;
-    insque(&io->queue, ioq->queue.q_back);
-    ioq->cc += cc;
-}
-
-/*
- * remove cc bytes from ioqueue ioq
- * free memory, dequeue io elements
- * which are no longer used.
- */
-static int
-dequeuecc(struct ioqueue *ioq, int cc)
-{
-    struct io *io;
-    struct qelem *qp;
-    int nbytes;
-    int there;
-
-    nbytes = 0;
-    while ((qp = ioq->queue.q_forw) != &ioq->queue) {
-	io = (struct io *)qp;
-	there = io->nbytes - io->offset;
-	if (there < 0) {
-	    fprintf(stderr, "dequeuecc: nbytes %d, offset %d\n",
-		    io->nbytes, io->offset);
-	    continue;
-	}
-	if (cc > there) {
-	    cc -= there;
-	    nbytes += there;
-	    (void)remque(&io->queue);
-	    free(io->data);
-	} else {
-	    io->offset += cc;
-	    nbytes += cc;
-	    break;
-	}
-    }
-    ioq->cc -= nbytes;
-    return nbytes;
-}

src/client/linebuf.c
@@ -1,0 +1,107 @@
+/*
+ *  Empire - A multi-player, client/server Internet based war game.
+ *  Copyright (C) 1986-2007, Dave Pare, Jeff Bailey, Thomas Ruschak,
+ *                           Ken Stevens, Steve McClure
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  ---
+ *
+ *  See files README, COPYING and CREDITS in the root of the source
+ *  tree for related information and legal notices.  It is expected
+ *  that future projects/authors will amend these files as needed.
+ *
+ *  ---
+ *
+ *  linebuf.c: Simple line buffer
+ * 
+ *  Known contributors to this file:
+ *     Markus Armbruster, 2007
+ */
+
+#include <config.h>
+
+#include <assert.h>
+#include <stdlib.h>
+#include "linebuf.h"
+
+/*
+ * Initialize empty line buffer.
+ * Not necessary if *LBUF is already zeroed.
+ */
+void
+lbuf_init(struct lbuf *lbuf)
+{
+    lbuf->len = lbuf->full = 0;
+}
+
+/*
+ * Return length of currently buffered line.
+ * This includes the newline if present.
+ */
+int
+lbuf_len(struct lbuf *lbuf)
+{
+    return lbuf->len;
+}
+
+/*
+ * Is LBUF full (i.e. we got the newline)?
+ */
+int
+lbuf_full(struct lbuf *lbuf)
+{
+    return lbuf->full;
+}
+
+/*
+ * Return a pointer to the currently buffered line.
+ * If you mess with the line in a way that changes the line length,
+ * better call lbuf_init() next.
+ */
+char *
+lbuf_line(struct lbuf *lbuf)
+{
+    assert(lbuf->len < sizeof(lbuf->line));
+    lbuf->line[lbuf->len] = 0;
+    return lbuf->line;
+}
+
+/*
+ * Append CH to the line buffered in LBUF.
+ * LBUF must not be full.
+ * If CH is a newline, the buffer is now full.  Return the line
+ * length, including the newline.
+ * Else return 0 if there was space, and -1 if not.
+ */
+int
+lbuf_putc(struct lbuf *lbuf, char ch)
+{
+    assert(!lbuf->full);
+
+    if (ch == '\n') {
+	assert(lbuf->len + 1 < sizeof(lbuf->line));
+	lbuf->line[lbuf->len++] = ch;
+	lbuf->line[lbuf->len] = 0;
+	lbuf->full = 1;
+	return lbuf->len;
+    }
+
+    if (lbuf->len + 2 >= sizeof(lbuf->line))
+	return -1;		/* truncating long line */
+
+    lbuf->line[lbuf->len++] = ch;
+    return 0;
+}

src/client/linebuf.h
@@ -25,51 +25,28 @@
  *
  *  ---
  *
- *  tags.c: save redir, execute and pipe info to protect against tampering
- *          by the deity.
+ *  linebuf.h: Simple line buffer
  * 
  *  Known contributors to this file:
- *     John Yockey, 1998
+ *     Markus Armbruster, 2007
  */
 
-#include <config.h>
+#ifndef LINEBUF_H
+#define LINEBUF_H
 
-#include <stdlib.h>
-#include <string.h>
-#include "misc.h"
-#include "tags.h"
+#define LBUF_LEN_MAX 4096
 
-struct tagstruct *taglist;
+struct lbuf {
+    /* All members are private! */
+    unsigned len;		/* strlen(line) */
+    int full;			/* got a complete line, with newline? */
+    char line[LBUF_LEN_MAX];	/* buffered line, zero-terminated */
+};
 
-void
-io_init(void)
-{
-    taglist = NULL;
-}
+extern void lbuf_init(struct lbuf *);
+extern int lbuf_len(struct lbuf *);
+extern int lbuf_full(struct lbuf *);
+extern char *lbuf_line(struct lbuf *);
+extern int lbuf_putc(struct lbuf *, char);
 
-char *
-gettag(char *p)
-{
-    struct tagstruct *tag1, *tag2;
-
-    if (taglist == NULL)
-	return NULL;
-    tag1 = taglist;
-    if (!strncmp(tag1->item, p, strlen(tag1->item))) {
-	p = tag1->item;
-	taglist = taglist->next;
-	free(tag1);
-	return p;
-    }
-    while (tag1->next != NULL) {
-	tag2 = tag1->next;
-	if (!strncmp(tag2->item, p, strlen(tag2->item))) {
-	    p = tag2->item;
-	    tag1->next = tag2->next;
-	    free(tag2);
-	    return p;
-	}
-	tag1 = tag1->next;
-    }
-    return NULL;
-}
+#endif

src/client/main.c
@@ -36,38 +36,11 @@
 
 #include <config.h>
 
-#include <errno.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#if defined(_WIN32)
-#include <io.h>
-#else
 #include <pwd.h>
-#include <sys/time.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
-#endif
-
-#include "ioqueue.h"
 #include "misc.h"
-#include "proto.h"
-#include "tags.h"
 #include "version.h"
-
-#ifdef _WIN32
-HANDLE hStdIn;
-#endif
-
-#define	RETRY	3
-
-int eight_bit_clean;
-int sock;
-
-static volatile sig_atomic_t interrupt;
-static void intr(int sig);
-static int handleintr(int);
 
 static void
 print_usage(char *program_name)
@@ -94,25 +67,10 @@ main(int argc, char **argv)
     char *uname;
     char *host;
     char *port;
+    int sock;
 #ifdef _WIN32
-    WORD wVersionRequested;
-    WSADATA WsaData;
-    int err;
-    fd_set readfds;
-    struct timeval tm;
-    DWORD stdinmode;
-    SECURITY_ATTRIBUTES security;
-    int bRedirected = 0;
     char unamebuf[128];
-#else
-    struct sigaction sa;
-    fd_set mask;
-    fd_set savemask;
-    int retry = 0;
 #endif
-    struct ioqueue server;
-    FILE *auxout_fp = NULL;
-    int n;
 
 #ifdef _WIN32
     /*
@@ -124,9 +82,6 @@ main(int argc, char **argv)
      * after each prompt is required.
      */
     setvbuf(stdout, NULL, _IOLBF, 4096);
-#else
-    FD_ZERO(&mask);
-    FD_ZERO(&savemask);
 #endif
 
     while ((opt = getopt(argc, argv, "2:kuhv")) != EOF) {
@@ -192,14 +147,13 @@ main(int argc, char **argv)
     }
 
     getsose();
-    if (auxfname && (auxout_fp = fopen(auxfname, "a")) == NULL) {
+    if (auxfname && (auxfp = fopen(auxfname, "a")) == NULL) {
 	fprintf(stderr, "Unable to open %s for append\n", auxfname);
 	exit(1);
     }
 
 #ifdef _WIN32
-    wVersionRequested = MAKEWORD(2, 0);
-    err = WSAStartup(wVersionRequested, &WsaData);
+    err = WSAStartup(MAKEWORD(2, 0), &WsaData);
     if (err != 0) {
 	printf("WSAStartup Failed, error code %d\n", err);
 	exit(1);
@@ -211,156 +165,8 @@ main(int argc, char **argv)
     if (!login(sock, uname, country, passwd, send_kill, utf8))
 	exit(1);
 
-    ioq_init(&server, 2048);
-    io_init();
-#ifndef _WIN32
-    FD_ZERO(&mask);
-    FD_SET(0, &savemask);
-    FD_SET(sock, &savemask);
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sa.sa_handler = intr;
-    sigaction(SIGINT, &sa, NULL);
-    sa.sa_handler = SIG_IGN;
-    sigaction(SIGPIPE, &sa, NULL);
-    while (FD_ISSET(sock, &savemask)) {
-	mask = savemask;
-	n = select(sock + 1, &mask, NULL, NULL, NULL);
-	if (interrupt) {
-	    if (!handleintr(sock))
-		break;
-	    errno = 0;
-	}
-	if (n <= 0) {
-	    if (errno == EINTR) {
-		perror("select");
-		(void)close(sock);
-		FD_CLR(sock, &savemask);
-	    }
-	} else {
-	    if (FD_ISSET(0, &mask)) {
-		if (!termio(0, sock, auxout_fp)) {
-		    if (retry++ >= RETRY) {
-			FD_CLR(0, &savemask);
-		    }
-		} else {
-		    retry = 0;
-		}
-	    }
-	    if (FD_ISSET(sock, &mask)) {
-		if (!serverio(sock, &server))
-		    FD_CLR(sock, &savemask);
-		else
-		    servercmd(&server, auxout_fp);
-	    }
-	}
-    }
-#else  /* _WIN32 */
-    signal(SIGINT, intr);
+    if (play(sock) < 0)
+	exit(1);
 
-    bRedirected = 0;
-    tm.tv_sec = 0;
-    tm.tv_usec = 1000;
-
-    if (!isatty(fileno(stdin)))
-	bRedirected = 1;
-    else {
-	security.nLength = sizeof(SECURITY_ATTRIBUTES);
-	security.lpSecurityDescriptor = NULL;
-	security.bInheritHandle = TRUE;
-	hStdIn = CreateFile("CONIN$",
-			    GENERIC_READ | GENERIC_WRITE,
-			    FILE_SHARE_READ | FILE_SHARE_WRITE,
-			    &security, OPEN_EXISTING, (DWORD) NULL, NULL);
-	
-	if (hStdIn == INVALID_HANDLE_VALUE) {
-	    printf("Error getting hStdIn.\n");
-	    fflush(stdout);
-	    exit(-3);
-	}
-	
-	err = GetConsoleMode(hStdIn, &stdinmode);
-	if (!err) {
-	    printf("Error getting console mode.\n");
-	    fflush(stdout);
-	    exit(-4);
-	} else {
-	    stdinmode |= ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT;
-	    err = SetConsoleMode(hStdIn, stdinmode);
-	    if (!err) {
-		printf("Error setting console mode.\n");
-		fflush(stdout);
-		exit(-5);
-	    }
-	}
-    }
-    while (1) {
-	FD_ZERO(&readfds);
-	FD_SET(sock, &readfds);
-	n = select(sock + 1, &readfds, NULL, NULL, &tm);
-	if (interrupt) {
-	    if (!handleintr(sock))
-		break;
-	    errno = 0;
-	}
-	if (n < 0) {
-	    if (errno == EINTR) {
-		errno = WSAGetLastError();
-		perror("select");
-		(void)closesocket(sock);
-		break;
-	    }
-	} else {
-	    if (bRedirected == 1) {
-		if (!termio(0, sock, auxout_fp))
-		    bRedirected = -1;
-	    } else if (bRedirected == 0) {
-		if (WaitForSingleObject(hStdIn, 10) != WAIT_TIMEOUT) {
-		    termio(-1, sock, auxout_fp);
-		    FlushConsoleInputBuffer(hStdIn);
-		}
-	    }
-	    if (FD_ISSET(sock, &readfds)) {
-		if (!serverio(sock, &server))
-		    break;
-		else
-		    servercmd(&server, auxout_fp);
-	    }
-	}
-    }
-    if (bRedirected == 0)
-	CloseHandle(hStdIn);
-#endif /* _WIN32 */
-    ioq_drain(&server);
-#ifdef _WIN32
-    (void)closesocket(sock);
-#else
-    (void)close(sock);
-#endif
-    return 0;			/* Shut the compiler up */
-}
-
-static void
-intr(int sig)
-{
-    interrupt = 1;
-#ifdef _WIN32
-    signal(SIGINT, intr);
-#endif
-}
-
-static int
-handleintr(int s)
-{
-    if (interrupt) {
-	/* tacky, but it works */
-#if !defined(_WIN32)
-	if (write(s, "\naborted\n", 1 + 7 + 1) <= 0)
-#else
-	if (send(s, "\naborted\n", 1 + 7 + 1, 0) <= 0)
-#endif
-	    return 0;
-	interrupt = 0;
-    }
-    return 1;
+    return 0;
 }

src/client/misc.h
@@ -39,18 +39,15 @@
 #include <windows.h>
 #endif
 
-struct ioqueue;
+#define MAX(a, b) ((a) >= (b) ? (a) : (b))
 
 extern char empirehost[];
 extern char empireport[];
 extern int eight_bit_clean;
-extern int sock;
+extern int input_fd;
+extern FILE *auxfp;
 extern char *SO;
 extern char *SE;
-
-#ifdef _WIN32
-extern HANDLE hStdIn;
-#endif
 
 #ifdef _WIN32
 #define getsose() ((void)0)
@@ -67,9 +64,9 @@ int recvline(int s, char *buf);
 int expect(int s, int match, char *buf);
 int tcp_connect(char *, char *);
 int login(int s, char *uname, char *cname, char *cpass, int kill_proc, int);
+int play(int);
 void sendcmd(int s, char *cmd, char *arg);
-void servercmd(struct ioqueue *ioq, FILE *auxfi);
-int serverio(int s, struct ioqueue *ioq);
-int termio(int fd, int sock, FILE *auxfi);
+void servercmd(int, char *, int);
+void outch(char);
 
 #endif

commit 2196ffd55c869505f63f3d806c3f7475b52ae12d

Fix client not to hang when EOF on stdin overtakes C_EXECUTE
Player input may overtake batch file contents (well-known protocol
flaw, see doc/clients-howto for details).  This includes EOF.  When
that happens, the client closes standard input, sends an EOF cookie,
and continues reading output until the server closes the connection.
When it gets C_EXECUTE, it redirects input to the batch file.  But it
then failed to read the batch file.  The server waited forever for the
execute's EOF cookie, the client waited forever for the server closing
the connection.

Fix by stopping only reading from standard input.  Broken in 8b7d0b91,
v4.3.11.

Note that the EOF cookie still overtakes the batch file contents,
which makes the server interpret the input between the execute command
and the EOF as batch file, and the batch file contents as ordinary
input.

src/client/play.c
@@ -473,10 +473,10 @@ play(int sock)
 
 	/*
 	 * Want to read player input only when we don't need to send
-	 * cookies, and we haven't hit EOF on fd 0, and INBUF can
-	 * accept some.
+	 * cookies, and INPUT_FD is still open, and INBUF can accept
+	 * some.
 	 */
-	if (!send_intr && !send_eof && !eof_fd0 && ring_space(&inbuf))
+	if (!send_intr && !send_eof && input_fd >= 0 && ring_space(&inbuf))
 	    FD_SET(input_fd, &rdfd);
 	/* Want to send player input only when we have something */
 	if (send_intr || send_eof || ring_len(&inbuf))
@@ -503,7 +503,7 @@ play(int sock)
 	    continue;
 
 	/* read player input */
-	if (FD_ISSET(input_fd, &rdfd)) {
+	if (input_fd >= 0 && FD_ISSET(input_fd, &rdfd)) {
 	    n = recv_input(input_fd, &inbuf);
 	    if (n < 0) {
 		perror("read stdin"); /* FIXME stdin misleading, could be execing */
@@ -515,10 +515,11 @@ play(int sock)
 		if (input_fd) {
 		    /* execute done, switch back to fd 0 */
 		    close(input_fd);
-		    input_fd = 0;
+		    input_fd = eof_fd0 ? -1 : 0;
 		} else {
 		    /* stop reading input, drain socket ring buffers */
 		    eof_fd0 = 1;
+		    input_fd = -1;
 		    sa.sa_handler = SIG_DFL;
 		    sigaction(SIGINT, &sa, NULL);
 		}