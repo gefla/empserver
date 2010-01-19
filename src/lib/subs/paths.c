gefla/empserver

commit 0b0612ea04ed9edc4074ef0e72de086d3c599895

Simplify getpath()

src/lib/subs/paths.c
@@ -169,13 +169,13 @@ getpath(char *buf, char *arg, coord x, coord y, int onlyown,
 		xyas(x, y, player->cnum));
     }
     bp = getstring(prompt, buf2);
-    if (bp && p + strlen(bp) + 1 >= buf + MAX_PATH_LEN) {
+    if (!bp)
+	return NULL;
+    if (p + strlen(bp) + 1 >= buf + MAX_PATH_LEN) {
 	pr("Path length may not exceed %d.\n", MAX_PATH_LEN);
 	pr("Aborting...\n");
 	bp = NULL;
     }
-    if (!bp)
-	return NULL;
     strcpy(p, bp);
     if (*bp)
 	goto more;

commit ad80846283023d319d3a1c2c4d193a42c7690d1f

Fix getpath() not to crash when path exceeds MAX_PATH_LEN
Trivial for players to trigger.  Broken in commit 0b0612ea, v4.3.16.
Spotted by the Clang Static Analyzer.

src/lib/subs/paths.c
@@ -173,7 +173,7 @@ getpath(char *buf, char *arg, coord x, coord y, int onlyown,
     if (p + strlen(bp) + 1 >= buf + MAX_PATH_LEN) {
 	pr("Path length may not exceed %d.\n", MAX_PATH_LEN);
 	pr("Aborting...\n");
-	bp = NULL;
+	return NULL;
     }
     strcpy(p, bp);
     if (*bp)