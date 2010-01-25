gefla/empserver

commit 4d40a27542230e9131eb0d30904bfdeb2d6a9f59

Use src/lib/w32/w32sockets.c for client
Move client's w32_connect() to w32sockets.c.

Replace w32_recv() and w32_send() by read() and write().

Replace w32_close() by w32_close_function.

Replace call of WSAStartup() in w32_sysdep_init() by
w32_socket_init().

Remove the identical copies of fd_is_socket(),
w32_set_winsock_errno(), w32_socket().

src/lib/w32/w32sockets.c
@@ -148,6 +148,25 @@ w32_bind(int fd, const struct sockaddr *name, socklen_t namelen)
 }
 
 /*
+ * POSIX compatible connect() replacement
+ */
+#undef connect
+int
+w32_connect(int sockfd, const struct sockaddr *addr, int addrlen)
+{
+    SOCKET sock = W32_FD_TO_SOCKET(sockfd);
+    int result;
+
+    result = connect(sock, addr, addrlen);
+    if (result == SOCKET_ERROR) {
+	/* FIXME map WSAEWOULDBLOCK to EINPROGRESS */
+	w32_set_winsock_errno();
+	return -1;
+    }
+    return result;
+}
+
+/*
  * POSIX equivalent for listen().
  */
 #undef listen

Make.mk
@@ -74,7 +74,8 @@ gamedir := $(localstatedir)/empire
 builtindir := $(datadir)/empire/builtin
 einfodir := $(datadir)/empire/info.nr
 ehtmldir := $(datadir)/empire/info.html
-client/w32 := sys/uio.h w32io.c w32types.h
+client/w32 := arpa/inet.h netdb.h netinet/in.h sys/time.h sys/socket.h	\
+sys/uio.h unistd.h w32io.c w32sockets.c w32types.h
 
 # How to substitute Autoconf output variables
 # Recursively expanded so that $@ and $< work.

src/client/Makefile.in
@@ -95,7 +95,12 @@ termlib.$O: misc.h
 version.$O: version.h
 $(obj): config.h
 
-expect.$O: w32/w32types.h
-play.$O: w32/w32types.h
+expect.$O: w32/sys/socket.h w32/unistd.h w32/w32types.h
+host.$O: w32/sys/socket.h w32/netinet/in.h w32/arpa/inet.h w32/netdb.h w32/unistd.h w32/w32types.h
+login.$O: w32/unistd.h w32/w32types.h
+main.$O: w32/unistd.h w32/w32types.h
+play.$O: w32/sys/socket.h w32/unistd.h w32/w32types.h
 ringbuf.$O: w32/sys/uio.h w32/w32types.h
+sysdep_w32.$O: w32/sys/socket.h
 w32/w32io.$O: misc.h w32/sys/uio.h w32/w32types.h
+w32/w32sockets.$O: w32/sys/socket.h w32/unistd.h w32/w32types.h

src/client/configure.ac
@@ -56,6 +56,7 @@ LIBS="$LIBS_SOCKETS $LIBS"
 LIB_SOCKET_NSL
 if test "$Windows_API" = yes; then
 	AC_LIBOBJ([w32/w32io])
+	AC_LIBOBJ([w32/w32sockets])
 fi
 
 

src/client/expect.c
@@ -39,22 +39,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef _WIN32
-#include "w32types.h"
-#else
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#endif
 #include "misc.h"
 #include "proto.h"
-
-#ifdef _WIN32
-#define read(sock, buffer, buf_size) \
-	w32_recv((sock), (buffer), (buf_size), 0)
-#define write(sock, buffer, buf_size) \
-	w32_send((sock), (buffer), (buf_size), 0)
-#endif
 
 int
 recvline(int s, char *buf)

src/client/host.c
@@ -40,14 +40,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifndef _WIN32
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
-#endif
 #include "misc.h"
 
 /* Portability cruft, should become unnecessary eventually */

src/client/login.c
@@ -38,8 +38,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#if !defined(_WIN32)
 #include <unistd.h>
+#ifdef _WIN32
+#include <windows.h>
 #endif
 #include "misc.h"
 #include "proto.h"

src/client/main.c
@@ -39,8 +39,8 @@
 #include <stdlib.h>
 #ifndef _WIN32
 #include <pwd.h>
-#include <unistd.h>
 #endif
+#include <unistd.h>
 #include "misc.h"
 #include "version.h"
 

commit eea24fb017e3a1012b8e2a76e6274e2bd1f83b5c

Fix windows client not always exiting
The call to WSAEnumNetworkEvents() was reseting network
events in particular WSAECONNRESET was sometimes being lost
causing the client to not exit.  The WSAEnumNetworkEvents()
was called from fd_is_socket() and has been replace with
getsockopt() to determine if the fd is socket or not.

Broken in commit 4d40a275, v4.3.23

src/lib/w32/w32sockets.c
@@ -68,12 +68,14 @@ static int
 fd_is_socket(int fd, SOCKET *sockp)
 {
     SOCKET sock;
-    WSANETWORKEVENTS ev;
+    BOOL val;
+    int size = sizeof(val);
 
     sock = W32_FD_TO_SOCKET(fd);
     if (sockp)
 	*sockp = sock;
-    return WSAEnumNetworkEvents(sock, NULL, &ev) == 0;
+    return   getsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&val, &size) == 0;
+	//WSAEnumNetworkEvents(sock, NULL, &ev) == 0;
 }
 
 void