gefla/empserver

commit ef7ea8934ffc7a7d480ee985ffa0b8908e05bb07

Drop last parameter of shp_hit_mine() and lnd_hit_mine()

src/lib/subs/lndsub.c
@@ -52,7 +52,7 @@
 #include "unit.h"
 
 static void lnd_mess(char *, struct ulist *);
-static int lnd_hit_mine(struct lndstr *, struct lchrstr *);
+static int lnd_hit_mine(struct lndstr *);
 static int has_helpful_engineer(coord, coord, natid);
 
 double
@@ -623,7 +623,7 @@ lnd_check_mines(struct emp_qelem *land_list)
 	if (SCT_LANDMINES(&sect) == 0)
 	    continue;
 	if (chance(DMINE_LHITCHANCE(sect.sct_mines) / (1 + 2 * with_eng))) {
-	    lnd_hit_mine(&llp->unit.land, ((struct lchrstr *)llp->chrp));
+	    lnd_hit_mine(&llp->unit.land);
 	    sect.sct_mines--;
 	    putsect(&sect);
 	    putland(llp->unit.land.lnd_uid, &llp->unit.land);
@@ -840,7 +840,7 @@ lnd_hardtarget(struct lndstr *lp)
 }
 
 static int
-lnd_hit_mine(struct lndstr *lp, struct lchrstr *lcp)
+lnd_hit_mine(struct lndstr *lp)
 {
     int m;
 
@@ -850,7 +850,7 @@ lnd_hit_mine(struct lndstr *lp, struct lchrstr *lcp)
     nreport(lp->lnd_own, N_LHIT_MINE, 0, 1);
 
     m = MINE_LDAMAGE();
-    if (lcp->l_flags & L_ENGINEER)
+    if (lchr[lp->lnd_uid].l_flags & L_ENGINEER)
 	m /= 2;
 
     landdamage(lp, m);

src/lib/subs/shpsub.c
@@ -53,7 +53,7 @@
 #include "unit.h"
 
 static int shp_check_one_mines(struct ulist *);
-static int shp_hit_mine(struct shpstr *, struct mchrstr *);
+static int shp_hit_mine(struct shpstr *);
 static void shp_mess(char *, struct ulist *);
 
 void
@@ -241,7 +241,7 @@ shp_check_one_mines(struct ulist *mlp)
 	return 0;
     if (chance(DMINE_HITCHANCE(sect.sct_mines))) {
 	actor = mlp->unit.ship.shp_own;
-	shp_hit_mine(&mlp->unit.ship, ((struct mchrstr *)mlp->chrp));
+	shp_hit_mine(&mlp->unit.ship);
 	sect.sct_mines--;
 	if (map_set(actor, sect.sct_x, sect.sct_y, 'X', 0))
 	    writemap(actor);
@@ -687,7 +687,7 @@ shp_hardtarget(struct shpstr *sp)
 }
 
 static int
-shp_hit_mine(struct shpstr *sp, struct mchrstr *mcp)
+shp_hit_mine(struct shpstr *sp)
 {
     double m;
 
@@ -697,7 +697,7 @@ shp_hit_mine(struct shpstr *sp, struct mchrstr *mcp)
     nreport(sp->shp_own, N_HIT_MINE, 0, 1);
 
     m = MINE_DAMAGE();
-    if (mcp->m_flags & M_SWEEP)
+    if (mchr[sp->shp_uid].m_flags & M_SWEEP)
 	m /= 2.0;
 
     shipdamage(sp, ldround(m, 1));

commit 7506039f1b31eeaddab436831d23fb50c2f9409b

Fix test for capability engineer in lnd_hit_mine()
Bogus array index, unpredictable result, can crash the server.  Broken
in commit ef7ea893, v4.3.24.

src/lib/subs/lndsub.c
@@ -862,7 +862,7 @@ lnd_hit_mine(struct lndstr *lp)
     nreport(lp->lnd_own, N_LHIT_MINE, 0, 1);
 
     m = MINE_LDAMAGE();
-    if (lchr[lp->lnd_uid].l_flags & L_ENGINEER)
+    if (lchr[lp->lnd_type].l_flags & L_ENGINEER)
 	m /= 2;
 
     landdamage(lp, m);