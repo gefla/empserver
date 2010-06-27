gefla/empserver

commit 162435e690ad8c4fbc0fb2b4f5b2095d7db7e533

Clean up checks for zero value of pln_damage()
Before commit a269cdd7, pln_damage() returned zero when the damage was
nuclear, and callers used that to bypass conventional damage code.
Zero value can't happen anymore.

src/lib/commands/bomb.c
@@ -345,8 +345,6 @@ eff_bomb(struct emp_qelem *list, struct sctstr *target)
 	    continue;
 	dam += pln_damage(&plp->plane, 'p', 1);
     }
-    if (dam <= 0)
-	return;
     getsect(target->sct_x, target->sct_y, &sect);
     target = &sect;
     oldeff = target->sct_effic;
@@ -420,8 +418,6 @@ comm_bomb(struct emp_qelem *list, struct sctstr *target)
 	    continue;
 	dam += pln_damage(&plp->plane, 'p', 1);
     }
-    if (dam <= 0)
-	return;
     getsect(target->sct_x, target->sct_y, &sect);
     target = &sect;
     before = target->sct_item[ip->i_uid];
@@ -535,10 +531,8 @@ ship_bomb(struct emp_qelem *list, struct sctstr *target)
 	    /* Bombs that miss have to land somewhere! */
 	    dam = pln_damage(&plp->plane, 'p', 0);
 	    collateral_damage(target->sct_x, target->sct_y, dam);
-	    dam = 0;
-	}
-	if (dam <= 0)
 	    continue;
+	}
 	if (mcp->m_flags & M_SUB)
 	    nreport(player->cnum, N_SUB_BOMB, ship.shp_own, 1);
 	else
@@ -637,10 +631,8 @@ plane_bomb(struct emp_qelem *list, struct sctstr *target)
 	    /* Bombs that miss have to land somewhere! */
 	    dam = pln_damage(&plp->plane, 'p', 0);
 	    collateral_damage(target->sct_x, target->sct_y, dam);
-	    dam = 0;
-	}
-	if (dam <= 0)
 	    continue;
+	}
 	if (dam > 100)
 	    dam = 100;
 	own = plane.pln_own;
@@ -747,10 +739,8 @@ land_bomb(struct emp_qelem *list, struct sctstr *target)
 	    /* Bombs that miss have to land somewhere! */
 	    dam = pln_damage(&plp->plane, 'p', 0);
 	    collateral_damage(target->sct_x, target->sct_y, dam);
-	    dam = 0;
-	}
-	if (dam <= 0)
 	    continue;
+	}
 	if (dam > 100)
 	    dam = 100;
 	own = land.lnd_own;
@@ -784,8 +774,6 @@ strat_bomb(struct emp_qelem *list, struct sctstr *target)
 	else
 	    dam += pln_damage(&plp->plane, 's', 1);
     }
-    if (dam <= 0)		/* dam == 0 if only nukes were delivered */
-	return;
     getsect(target->sct_x, target->sct_y, &sect);
     target = &sect;
     if (target->sct_own)

src/lib/subs/shpsub.c
@@ -507,12 +507,10 @@ shp_missile_interdiction(struct emp_qelem *list, coord newx, coord newy,
 			shp_hardtarget(&mvs->unit.ship), EF_SHIP,
 			N_SHP_MISS, N_SHP_SMISS, sublaunch, victim)) {
 		dam = pln_damage(&plp->plane, 'p', 1);
-		if (dam) {
-		    mpr(victim,
-			"missile interdiction mission does %d damage to %s!\n",
-			dam, prship(&mvs->unit.ship));
-		    shp_damage_one(mvs, dam);
-		}
+		mpr(victim,
+		    "missile interdiction mission does %d damage to %s!\n",
+		    dam, prship(&mvs->unit.ship));
+		shp_damage_one(mvs, dam);
 	    } else {
 		dam = pln_damage(&plp->plane, 'p', 0);
 		collateral_damage(newx, newy, dam);

commit f3fc477c82218d800d911ffbe1fdbb7f69700c9b

All-nuclear bombing run reported "bombing raid did 0 damage"
Broken in commit 162435e6, v4.3.26.

src/lib/commands/bomb.c
@@ -774,6 +774,8 @@ strat_bomb(struct emp_qelem *list, struct sctstr *target)
 	else
 	    dam += pln_damage(&plp->plane, 's', 1);
     }
+    if (dam <= 0)
+	return;
     getsect(target->sct_x, target->sct_y, &sect);
     target = &sect;
     if (target->sct_own)