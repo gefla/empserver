7e2008e7f4a7eac9b44ed6e3fa2a7d7dcb0c49bf
Markus Armbruster <armbru@pond.sub.org>
2011-02-21 19:37:40 +0100

    License upgrade to GPL version 3 or later
    Why upgrade?  I'm not a lawyer, but here's my take on the differences
    to version 2:
    
    * Software patents: better protection against abuse of patents to
      prevent users from exercising the rights under the GPL.  I doubt
      we'll get hit with a patent suit, but it's a good move just on
      general principles.
    
    * License compatibility: compatible with more free licenses, i.e. can
      "steal" more free software for use in Empire.  I don't expect to steal
      much, but it's nice to have the option.
    
    * Definition of "source code": modernization of some details for today's
      networked world, to make it easier to distribute the software.  Not
      really relevant to us now, as we normally distribute full source code.
    
    * Tivoization: this is about putting GPL-licensed software in hardware,
      then make the hardware refuse to run modified software.  "Neat" trick
      to effectively deny its users their rights under the GPL.  Abuse was
      "pioneered" by TiVo (popular digital video recorders).  GPLv3 forbids
      it.  Unlikely to become a problem for us.
    
    * Internationalization: more careful wording, to harden the license
      outside the US.  The lawyers tell us it better be done that way.
    
    * License violations: friendlier way to deal with license violations.
      This has come out of past experience enforcing the GPL.
    
    * Additional permissions: Probably not relevant to us.
    
    Also include myself in the list of principal authors.

--- info/mksubj.pl
+++ info/mksubj.pl
@@ -1,36 +1,35 @@
 #!/usr/bin/perl
 #
-#  Empire - A multi-player, client/server Internet based war game.
-#  Copyright (C) 1986-2010, Dave Pare, Jeff Bailey, Thomas Ruschak,
-#                           Ken Stevens, Steve McClure
+#   Empire - A multi-player, client/server Internet based war game.
+#   Copyright (C) 1986-2011, Dave Pare, Jeff Bailey, Thomas Ruschak,
+#                 Ken Stevens, Steve McClure, Markus Armbruster
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
+#   Empire is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#  ---
+#   ---
 #
-#  See files README, COPYING and CREDITS in the root of the source
-#  tree for related information and legal notices.  It is expected
-#  that future projects/authors will amend these files as needed.
+#   See files README, COPYING and CREDITS in the root of the source
+#   tree for related information and legal notices.  It is expected
+#   that future projects/authors will amend these files as needed.
 #
-#  ---
+#   ---
 #
-#  mksubj.pl: Create the index for a subject
+#   mksubj.pl: Create the index for a subject
 #
-#  Known contributors to this file:
-#     Ken Stevens (when it was still info.pl)
-#     Markus Armbruster, 2006
+#   Known contributors to this file:
+#      Ken Stevens (when it was still info.pl)
+#      Markus Armbruster, 2006
 #
 
 # Usage: mksubj.pl OUTFILE INFILE...
