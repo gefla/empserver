gefla/empserver

commit 8da88626d58de0299d8f33ad9069bb273120eae4

(produce_sect, finish_sects): Move stop order expiry to end of update, so that stopped sectors don't repair units.  The update sequence masked this bug until its change in src/lib/update/main.c rev. 1.25, but use of budget priorities (gone since rev. 1.24) could unmask it.

src/lib/update/finish.c
@@ -162,6 +162,8 @@ finish_sects(int etu)
 	if (infptr->path)
 	    free(infptr->path);
 #endif /* SAVE_FINISH_PATHS */
+
+	sp->sct_off = 0;
     }
     logerror("done importing\n");
 

src/lib/update/sect.c
@@ -314,11 +314,8 @@ produce_sect(int natnum, int etu, int *bp, long p_sect[][2])
 	bp_disable_cachepath();
 	bp_clear_cachepath();
 
-	if (sp->sct_off || np->nat_money < 0) {
-	    if (!player->simulation)
-		sp->sct_off = 0;
+	if (sp->sct_off || np->nat_money < 0)
 	    continue;
-	}
 
 	neweff = sp->sct_effic;
 	amount = 0;

commit a12a315cd92569d8fe81dd3fb68d8fbdd23f49b4

Fix stop orders to expire even when the country is broke
Broken in commit 8da88626, v4.3.8.

src/lib/update/finish.c
@@ -98,13 +98,13 @@ finish_sects(int etu)
 
     logerror("importing...");
     for (n = 0; NULL != (sp = getsectid(n)); n++) {
+	sp->sct_off = 0;
 	if (!sp->sct_own)
 	    continue;
 	np = getnatp(sp->sct_own);
 	if (np->nat_money < 0)
 	    continue;
 	dodistribute(sp, IMPORT, import_cost[n]);
-	sp->sct_off = 0;
     }
     logerror("done importing\n");
 