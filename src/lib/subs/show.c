gefla/empserver

commit 3b4de2feb16974b0a65ee17c7a7b761615a5e85b

Remove option FUEL
The abstract idea of tying ships and land units to a logistical tether
is sound, the concrete implementation as option FUEL is flawed.  It
adds too much busy-work to the game to be enjoyable.  It hasn't been
enabled in a public game for years.  The code implementing it is ugly,
repetitive, and a burden to maintain.

Remove selector fuel from ship_ca[] and land_ca[], and selectors
fuelc, fuelu from mchr_ca[] and lchr_ca[].  Remove fields fuelc, fuelu
from ship.config and land.config.

Remove command fuel from player_coms[].

Deprecate edit key 'B' in doship(), dounit(), and don't show it in
pr_ship(), pr_land().

Drop opt_FUEL code from build_ship(), shi(), sdump(), ship_damage(),
show_ship_stats(), do_mob_ship(), nav_ship(), build_land(), land(),
ldump(), land_damage(), show_land_stats(), do_mob_land(),
resupply_all(), resupply_commod(), get_minimum(), has_supply(),
unit_list(), vers().

Remove opt_FUEL, fuel_mult, struct shpstr member shp_fuel, struct
mchrstr members m_fuelc and m_fuelu, M_OILER, struct lndstr member
lnd_fuel, struct lchrstr members l_fuelc and l_fuelu, fuel(), and
auto_fuel_ship().

src/lib/subs/show.c
@@ -244,18 +244,12 @@ show_ship_stats(int tlev)
     int scount;
 
     pr("%25s      s  v  s  r  f  l  p  h  x", "");
-    if (opt_FUEL)
-	pr("  fuel");
     pr("\n");
 
     pr("%25s      p  i  p  n  i  n  l  e  p", "");
-    if (opt_FUEL)
-	pr("   c/u");
     pr("\n");
 
     pr("%25s def  d  s  y  g  r  d  n  l  l", "");
-    if (opt_FUEL)
-	pr("      ");
     pr("\n");
 
 
@@ -275,8 +269,6 @@ show_ship_stats(int tlev)
 	pr("%2d ", mp->m_nplanes);
 	pr("%2d ", mp->m_nchoppers);
 	pr("%2d ", mp->m_nxlight);
-	if (opt_FUEL)
-	    pr("%3d/%1d ", mp->m_fuelc, mp->m_fuelu);
 	pr("\n");
     }
 }
@@ -445,9 +437,9 @@ show_land_stats(int tlev)
     struct lchrstr *lcp;
     int lcount;
 
-    pr("%25s              s  v  s  r  r  a  f  a  a        x  l\n", "");
-    pr("%25s              p  i  p  a  n  c  i  m  a  f  f  p  n\n", "");
-    pr("%25s att def vul  d  s  y  d  g  c  r  m  f  c  u  l  d\n", "");
+    pr("%25s              s  v  s  r  r  a  f  a  a  x  l\n", "");
+    pr("%25s              p  i  p  a  n  c  i  m  a  p  n\n", "");
+    pr("%25s att def vul  d  s  y  d  g  c  r  m  f  l  d\n", "");
 
     make_new_list(tlev, EF_LAND);
     for (lcount = 0; lcount < lookup_list_cnt; lcount++) {
@@ -463,9 +455,6 @@ show_land_stats(int tlev)
 	pr("%2d %2d %2d %2d %2d ",
 	   l_frg(lcp, tlev), l_acc(lcp, tlev), l_dam(lcp, tlev),
 	   lcp->l_ammo, lcp->l_aaf);
-	pr("%2d %2d %2d %2d ",
-	   lcp->l_fuelc, lcp->l_fuelu, lcp->l_nxlight, lcp->l_nland);
-
 	pr("\n");
     }
 }

include/econfig-spec.h
@@ -147,8 +147,6 @@ EMPCF_OPT("EASY_BRIDGES", opt_EASY_BRIDGES,
     "Allow bridge building without bridge heads")
 EMPCF_OPT("FALLOUT", opt_FALLOUT,
     "Enable secondary effects caused by radiation")
-EMPCF_OPT("FUEL", opt_FUEL,
-    "Make ships use fuel to move")
 EMPCF_OPT("GODNEWS", opt_GODNEWS,
     "Inform the world when deities give/take away")
 EMPCF_OPT("GO_RENEW", opt_GO_RENEW,
@@ -391,8 +389,6 @@ EMPCFBOTH("anno_keep_days", anno_keep_days, int, NSC_INT, KM_INTERNAL,
 	  "How long until announcements expire (<0 means never)")
 EMPCFBOTH("news_keep_days", news_keep_days, int, NSC_INT, KM_INTERNAL,
 	  "How long until news expire")
-EMPCFBOTH("fuel_mult", fuel_mult, int, NSC_INT, 0,
-    "Multiplier for fuel to mobility calculation")
 EMPCFBOTH("lost_items_timeout", lost_items_timeout, int, NSC_INT, KM_INTERNAL,
     "Seconds before a lost item is timed out of the database")
 

include/land.h
@@ -69,7 +69,6 @@ struct lndstr {
     short lnd_ship;		/* pointer to transporting ship */
     signed char lnd_harden;	/* fortification */
     short lnd_retreat;		/* retreat percentage */
-    unsigned char lnd_fuel;	/* How much fuel do we have */
     unsigned char lnd_nxlight;	/* How many xlight planes on board? */
     int lnd_rflags;		/* When do I retreat? */
     char lnd_rpath[RET_LEN];	/* retreat path */
@@ -105,8 +104,6 @@ struct lchrstr {
     int l_dam;			/* # of guns firing */
     int l_ammo;			/* firing ammu used per shot */
     int l_aaf;			/* aa fire */
-    unsigned char l_fuelc;	/* fuel capacity */
-    unsigned char l_fuelu;	/* fuel used per 10 mob */
     unsigned char l_nxlight;	/* maximum number of xlight planes */
     unsigned char l_nland;	/* maximum number of units */
     signed char l_type;		/* index in lchr[] */

include/prototypes.h
@@ -130,7 +130,6 @@ int fly(void);
 int foll(void);
 int force(void);
 int fort(void);
-int fuel(void);
 int give(void);
 int grin(void);
 int hard(void);
@@ -751,7 +750,6 @@ extern int nav_ship(struct shpstr *);
 extern int load_it(struct shpstr *, struct sctstr *,
 		   int);
 extern void unload_it(struct shpstr *);
-extern void auto_fuel_ship(struct shpstr *);
 /* nxtitemp.c */
 extern void *nxtitemp(struct nstr_item *);
 /* plague.c */

include/ship.h
@@ -96,7 +96,6 @@ struct shpstr {
     char shp_path[MAXSHPPATH];
     short shp_follow;
     char shp_name[MAXSHPNAMLEN]; /* name set with the "name" command */
-    unsigned char shp_fuel;	/* How much fuel do we have */
     unsigned char shp_nchoppers; /* How many choppers on board? */
     unsigned char shp_nxlight;	/* How many xlight planes on board? */
     coord shp_orig_x;
@@ -118,8 +117,6 @@ struct mchrstr {
     int m_glim;			/* how many guns it can fire */
     unsigned char m_nxlight;	/* maximum number of xlight planes */
     unsigned char m_nchoppers;	/* maximum number of choppers */
-    unsigned char m_fuelc;	/* fuel capacity */
-    unsigned char m_fuelu;	/* fuel used per 10 mob */
     char *m_name;		/* full name of type of ship */
     int m_tech;			/* tech required to build */
     int m_cost;			/* how much it costs to build */
@@ -148,7 +145,7 @@ struct mchrstr {
 #define M_SEMILAND	bit(14)	/* can land 1/4 */
 /* unused		bit(15)	*/
 /* unused		bit(16)	*/
-#define M_OILER		bit(17)	/* can re-fuel ships */
+/* unused		bit(17)	*/
 #define M_SUPPLY	bit(18)	/* Can supply units/sects/ships */
 #define M_CANAL		bit(19) /* Can navigate a canal (BIG CITY) */
 #define M_ANTIMISSILE   bit(20)	/* Shoot down missile */

info/Autofish.t
@@ -66,9 +66,6 @@ repeat 2a,2b for other ships in the area.
 .s1
 During the update your fishing boats will now move to the harbor at
 15,1 unload food until it reaches 30, then move back to 15,1 and STOP.
-If you're using fishing boats that require fuel, i.e. fb2
-when the ship enters the harbor, and the fuel level is at 0, the
-autonav code will automatically refuel the ship for you.
 So unless the ship gets sunk, or something happens to it, like the
 civs starve on board if you don't leave enough food on it,
 the ships will continue to feed your country and you won't even need

info/Land-units.t
@@ -17,7 +17,7 @@ will usually start with 'l'.
 .s1
 Land units have some characteristics of both ships and planes, plus some
 unique ones. Like both ships & planes, they have efficiency & mobility.
-Like ships, they can carry cargo, may have/use fuel, and may be able to fire
+Like ships, they can carry cargo, and may be able to fire
 guns at distant targets. Like planes, they have attack and defense values,
 and may be able to act automatically to defend your country.
 Let's look at a typical land unit:
@@ -31,7 +31,7 @@ Let's look at a typical land unit:
 .s1
 This land unit is an infantry unit, located at 8,0. In many games, you will
 start with 1-2 basic land units of this type. Some of the things about it are
-familiar. It is 96% efficient, has 93 mobility units, carries 1 food, no fuel,
+familiar. It is 96% efficient, has 93 mobility units, carries 1 food,
 and is tech 0 and currently contains 100 mi. It carries 0 extra-light
 planes, and is not on a ship. The 'a'
 stands for army, and is just like fleets or wings, i.e. a way of grouping your
@@ -132,16 +132,16 @@ operate. These are obtainable from the show command:
 .EX show land statistics
 .s1
 .NF
-                                       s  v  s  r  f  a  d  a  a        x
-                                       p  i  p  a  r  c  a  m  a  f  f  p
-                          att def vul  d  s  y  d  g  c  m  m  f  c  u  l
-infantry                  1.0 1.5  60 23 15  2  1  0  0  0  0  0  0  0  0
+                                       s  v  s  r  f  a  d  a  a  x
+                                       p  i  p  a  r  c  a  m  a  p
+                          att def vul  d  s  y  d  g  c  m  m  f  l
+infantry                  1.0 1.5  60 23 15  2  1  0  0  0  0  0  0
 .FI
 .s1
 The infantry unit has an attack multiplier of 1.0, and defensive multiplier
 of 1.5. It has a vulnerability rating of 60, a speed of 23, a visibility of
 15, spy value of 2, reaction radius maximum of 1, no firing range, no accuracy,
-no firing damage, uses no ammo, has no aa fire, doesn't carry of use fuel,
+no firing damage, uses no ammo, has no aa fire,
 and can't carry extra-light planes.
 .s1
 When attacking, a unit's value is expressed in 'mil-equivalents', i.e. 1
@@ -206,11 +206,6 @@ information on supply and how it affects combat)
 .s1
 Aaf is the unit's anti-aircraft fire value. The higher the value, the more
 damage the unit will do to planes flying overhead.
-.s1
-Fc/fu are the unit's fuel capacity and use. If the fc/fu are 0, the unit
-does not need fuel to gain mobility. If the fc/fu are non-zero, the unit
-will need fuel to gain mobility, and will use fu fuel whenever it fights.
-(See info \*Qfuel\*U for information on fuel)
 .s1
 Xpl is the number of extra-light planes the unit can carry. Planes can
 operate from a land unit in the same fashion as if it were a carrier.

info/Options.t
@@ -17,7 +17,6 @@ ALL_BLEED:	you get tech bleed from all countries, not just allies
 SLOW_WAR:	War declaring takes time. No land attacks unless at war.
 NOMOBCOST:	ships pay 0 mob to fire. Subs pay 1/2 a sect's movement cost
 TRADESHIPS:	you can build/nav/scuttle trade ships to make money
-FUEL:		ships need fuel to gain mobility
 RES_POP:	Research affects max sector population.
 NOFOOD:		No food is required.
 BLITZ:		Players get infinite BTU's.

commit c15df51e2720534e2ada7ae36de7a70c7fb4dbd5

Fix "show land s" to show columns xpl and lnd again
Broken in commit 3b4de2fe, v4.3.15.

src/lib/subs/show.c
@@ -400,6 +400,8 @@ show_land_stats(int tlev)
 	pr("%2d %2d %2d %2d %2d ",
 	   l_frg(lcp, tlev), l_acc(lcp, tlev), l_dam(lcp, tlev),
 	   lcp->l_ammo, lcp->l_aaf);
+	pr("%2d %2d ",
+	   lcp->l_nxlight, lcp->l_nland);
 	pr("\n");
     }
 }