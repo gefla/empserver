gefla/empserver

commit 64a53c90f0522ba60bf70a7912161a082dd0b745

Cargo lists storing lists of cargo for each carrier
Persistent game state encodes "who carries what" by storing the
carrier uid in the cargo.  Cargo lists augment that: they store lists
of cargo for each carrier.  They are not persistent.

New unit_cargo_init() to compute the cargo lists from game state.
Call it in ef_init_srv() and at the end of update_main().

New unit_onresize() to resize the cargo list data structure.
Installed as units' struct empfile callback onresize to make them
resize automatically with the unit files.

New unit_carrier_change() to update cargo lists when carriers change
in game state.  Convenience wrappers pln_carrier_change(),
lnd_carrier_change() and nuk_carrier_change().  Call them from
prewrite callbacks to keep cargo lists in sync with game state.

To make that work, unused units must not point to a carrier.  Add new
pln_oninit(), lnd_oninit() and nuk_oninit() take care of newly created
units.  Change lnd_prewrite() and nuk_prewrite() to take dead land
units and nukes off their carrier.  pln_prewrite() did that already.

New unit_cargo_first(), unit_cargo_next() to traverse cargo lists.
Convenience wrappers lnd_first_on_ship(), lnd_first_on_land(),
lnd_next_on_unit(), pln_first_on_ship(), pln_first_on_land(),
pln_next_on_unit() and nuk_on_plane().  The latter is disabled for now
because it clashes with an existing function.

src/lib/subs/land.c
@@ -120,6 +120,7 @@ lnd_prewrite(int n, void *old, void *new)
 
     if (llp->lnd_own && llp->lnd_effic < LAND_MINEFF) {
 	own = 0;
+	llp->lnd_ship = llp->lnd_land = -1;
 
 	for (i = 0; NULL != (lp = getlandp(i)); i++) {
 	    if (lp->lnd_own && lp->lnd_land == n) {
@@ -149,12 +150,27 @@ lnd_prewrite(int n, void *old, void *new)
 	item_prewrite(llp->lnd_item);
     }
 
+    if (CANT_HAPPEN(llp->lnd_ship >= 0 && llp->lnd_land >= 0))
+	llp->lnd_land = -1;
+    if (oldlp->lnd_ship != llp->lnd_ship)
+	lnd_carrier_change(llp, EF_SHIP, oldlp->lnd_ship, llp->lnd_ship);
+    if (oldlp->lnd_land != llp->lnd_land)
+	lnd_carrier_change(llp, EF_LAND, oldlp->lnd_land, llp->lnd_land);
+
     /* We've avoided assigning to llp->lnd_own, in case oldsp == sp */
     if (oldlp->lnd_own != own)
 	lost_and_found(EF_LAND, oldlp->lnd_own, own,
 		       llp->lnd_uid, llp->lnd_x, llp->lnd_y);
 
     llp->lnd_own = own;
+}
+
+void
+lnd_oninit(void *ptr)
+{
+    struct lndstr *lp = ptr;
+
+    lp->lnd_ship = lp->lnd_land = -1;
 }
 
 char *

src/lib/subs/nuke.c
@@ -44,8 +44,6 @@
 #include "plane.h"
 #include "player.h"
 #include "prototypes.h"
-#include "sect.h"
-#include "xy.h"
 
 void
 nuk_postread(int n, void *ptr)
@@ -80,8 +78,13 @@ nuk_prewrite(int n, void *old, void *new)
     struct nukstr *np = new;
     natid own = np->nuk_own;
 
-    if (np->nuk_effic == 0)
+    if (np->nuk_effic == 0) {
 	own = 0;
+	np->nuk_plane = -1;
+    }
+
+    if (oldnp->nuk_plane != np->nuk_plane)
+	nuk_carrier_change(np, EF_PLANE, oldnp->nuk_plane, np->nuk_plane);
 
     /* We've avoided assigning to np->nuk_own, in case oldsp == sp */
     if (oldnp->nuk_own != own)
@@ -104,6 +107,14 @@ nuk_on_plane(struct nukstr *np, int pluid)
 	    return np->nuk_uid;
     }
     return -1;
+}
+
+void
+nuk_oninit(void *ptr)
+{
+    struct nukstr *np = ptr;
+
+    np->nuk_plane = -1;
 }
 
 char *

src/lib/subs/plane.c
@@ -119,6 +119,7 @@ pln_prewrite(int n, void *old, void *new)
     if (pp->pln_effic < PLANE_MINEFF) {
 	own = 0;
 	pp->pln_effic = 0;
+	pp->pln_ship = pp->pln_land = -1;
 	for (i = 0; NULL != (np = getnukep(i)); i++) {
 	    if (np->nuk_own && np->nuk_plane == n) {
 		mpr(np->nuk_own, "%s lost!\n", prnuke(np));
@@ -129,12 +130,27 @@ pln_prewrite(int n, void *old, void *new)
 	}
     }
 
+    if (CANT_HAPPEN(pp->pln_ship >= 0 && pp->pln_land >= 0))
+	pp->pln_land = -1;
+    if (oldpp->pln_ship != pp->pln_ship)
+	pln_carrier_change(pp, EF_SHIP, oldpp->pln_ship, pp->pln_ship);
+    if (oldpp->pln_land != pp->pln_land)
+	pln_carrier_change(pp, EF_LAND, oldpp->pln_land, pp->pln_land);
+
     /* We've avoided assigning to pp->pln_own, in case oldsp == sp */
     if (oldpp->pln_own != own)
 	lost_and_found(EF_PLANE, oldpp->pln_own, own,
 		       pp->pln_uid, pp->pln_x, pp->pln_y);
 
     pp->pln_own = own;
+}
+
+void
+pln_oninit(void *ptr)
+{
+    struct plnstr *pp = ptr;
+
+    pp->pln_ship = pp->pln_land = -1;
 }
 
 char *

include/land.h
@@ -159,6 +159,12 @@ extern int lnd_acc(struct lndstr *);
 extern int lnd_dam(struct lndstr *);
 extern int lnd_aaf(struct lndstr *);
 
+/* src/lib/common/cargo.c */
+extern void lnd_carrier_change(struct lndstr *, int, int, int);
+extern int lnd_first_on_ship(struct shpstr *);
+extern int lnd_first_on_land(struct lndstr *);
+extern int lnd_next_on_unit(int);
+
 extern int lnd_fire(struct lndstr *);
 extern double lnd_fire_range(struct lndstr *);
 

include/nuke.h
@@ -90,4 +90,10 @@ struct nchrstr {
 
 extern struct nchrstr nchr[N_MAXNUKE + 1];
 
+/* src/lib/common/cargo.c */
+extern void nuk_carrier_change(struct nukstr *, int, int, int);
+#if 0 /* clashes with the one in src/lib/subs/nuke.c */
+extern int nuk_on_plane(struct plnstr *);
+#endif
+
 #endif

include/plane.h
@@ -156,6 +156,12 @@ extern int pln_acc(struct plnstr *);
 extern int pln_range_max(struct plnstr *);
 extern int pln_load(struct plnstr *);
 
+/* src/lib/common/cargo.c */
+extern void pln_carrier_change(struct plnstr *, int, int, int);
+extern int pln_first_on_ship(struct shpstr *);
+extern int pln_first_on_land(struct lndstr *);
+extern int pln_next_on_unit(int);
+
 /* src/lib/subs/aircombat.c */
 extern void ac_combat_headers(natid, natid);
 extern void ac_airtoair(struct emp_qelem *, struct emp_qelem *);

include/prototypes.h
@@ -450,6 +450,7 @@ extern void bitinit2(struct nstr_sect *, unsigned char *, int);
 extern int getele(char *, char *);
 /* land.c */
 extern char *prland(struct lndstr *);
+extern void lnd_oninit(void *);
 extern void lnd_postread(int, void *);
 extern void lnd_prewrite(int, void *, void *);
 /* landgun.c */
@@ -517,6 +518,7 @@ extern void init_nreport(void);
 extern void nreport(natid, int, natid, int);
 /* nuke.c */
 extern char *prnuke(struct nukstr *);
+extern void nuk_oninit(void *);
 extern void nuk_postread(int, void *);
 extern void nuk_prewrite(int, void *, void *);
 extern int nuk_on_plane(struct nukstr *, int);
@@ -528,6 +530,7 @@ extern int nxtsct(struct nstr_sect *, struct sctstr *);
 extern int onearg(char *, char *);
 /* plane.c */
 extern char *prplane(struct plnstr *);
+extern void pln_oninit(void *);
 extern void pln_postread(int, void *);
 extern void pln_prewrite(int, void *, void *);
 /* plnsub.c */

include/unit.h
@@ -44,6 +44,12 @@ struct ulist {
     int supplied;		/* LAND only */
 };
 
+extern void unit_cargo_init(void);
+extern void unit_carrier_change(struct empobj *, int, int, int);
+extern int unit_cargo_first(int, int, int);
+extern int unit_cargo_next(int, int);
+extern int unit_onresize(int);
+
 extern void unit_list(struct emp_qelem *);
 extern void unit_put(struct emp_qelem *list, natid actor);
 extern char *unit_path(int, struct empobj *, char *);

commit a8b7d1d017da35cc6b1b88c4191b718828e738e8

Fix prewrite callbacks' cargo list update for in-place update
When updating in-place (old==new), we must not write through new
before we're done reading the same memory through old.

Bug: we write the carrier uids too early.  Cargo lists aren't updated
when a carrier dies in an in-place update.  No such updates are known.
Broken in commit 64a53c90, v4.3.17.

src/lib/subs/land.c
@@ -64,21 +64,24 @@ lnd_prewrite(int n, void *old, void *new)
     struct lndstr *oldlp = old;
     struct lndstr *lp = new;
     natid own = lp->lnd_effic < LAND_MINEFF ? 0 : lp->lnd_own;
+    int ship = lp->lnd_ship;
+    int land = lp->lnd_land;
+
+    /* Be careful with writing to *lp, in case oldlp == lp */
 
     if (!own) {
 	lp->lnd_effic = 0;
-	lp->lnd_ship = lp->lnd_land = -1;
+	ship = land = -1;
     }
     item_prewrite(lp->lnd_item);
 
-    if (CANT_HAPPEN(lp->lnd_ship >= 0 && lp->lnd_land >= 0))
-	lp->lnd_land = -1;
-    if (oldlp->lnd_ship != lp->lnd_ship)
-	lnd_carrier_change(lp, EF_SHIP, oldlp->lnd_ship, lp->lnd_ship);
-    if (oldlp->lnd_land != lp->lnd_land)
-	lnd_carrier_change(lp, EF_LAND, oldlp->lnd_land, lp->lnd_land);
+    if (CANT_HAPPEN(ship >= 0 && land >= 0))
+	land = -1;
+    if (oldlp->lnd_ship != ship)
+	lnd_carrier_change(lp, EF_SHIP, oldlp->lnd_ship, ship);
+    if (oldlp->lnd_land != land)
+	lnd_carrier_change(lp, EF_LAND, oldlp->lnd_land, land);
 
-    /* We've avoided assigning to lp->lnd_own, in case oldlp == lp */
     if (oldlp->lnd_own != own) {
 	lost_and_found(EF_LAND, oldlp->lnd_own, own,
 		       lp->lnd_uid, lp->lnd_x, lp->lnd_y);
@@ -87,6 +90,8 @@ lnd_prewrite(int n, void *old, void *new)
     }
 
     lp->lnd_own = own;
+    lp->lnd_ship = ship;
+    lp->lnd_land = land;
     if (!own || lp->lnd_x != oldlp->lnd_x || lp->lnd_y != oldlp->lnd_y)
 	unit_update_cargo((struct empobj *)lp);
 }

src/lib/subs/nuke.c
@@ -29,7 +29,7 @@
  *  Known contributors to this file:
  *     Dave Pare, 1989
  *     Steve McClure, 1996
- *     Markus Armbruster, 2006-2008
+ *     Markus Armbruster, 2006-2011
  */
 
 #include <config.h>
@@ -60,21 +60,24 @@ nuk_prewrite(int n, void *old, void *new)
     struct nukstr *oldnp = old;
     struct nukstr *np = new;
     natid own = np->nuk_effic == 0 ? 0 : np->nuk_own;
+    int plane = np->nuk_plane;
+
+    /* Be careful with writing to *np, in case oldnp == np */
 
     if (!own) {
 	np->nuk_effic = 0;
-	np->nuk_plane = -1;
+	plane = -1;
     }
 
-    if (oldnp->nuk_plane != np->nuk_plane)
-	nuk_carrier_change(np, EF_PLANE, oldnp->nuk_plane, np->nuk_plane);
+    if (oldnp->nuk_plane != plane)
+	nuk_carrier_change(np, EF_PLANE, oldnp->nuk_plane, plane);
 
-    /* We've avoided assigning to np->nuk_own, in case oldnp == np */
     if (oldnp->nuk_own != own)
 	lost_and_found(EF_NUKE, oldnp->nuk_own, own,
 		       np->nuk_uid, np->nuk_x, np->nuk_y);
 
     np->nuk_own = own;
+    np->nuk_plane = plane;
 }
 
 char *

src/lib/subs/plane.c
@@ -64,20 +64,23 @@ pln_prewrite(int n, void *old, void *new)
     struct plnstr *oldpp = old;
     struct plnstr *pp = new;
     natid own = pp->pln_effic < PLANE_MINEFF ? 0 : pp->pln_own;
+    int ship = pp->pln_ship;
+    int land = pp->pln_land;
+
+    /* Be careful with writing to *pp, in case oldpp == pp */
 
     if (!own) {
 	pp->pln_effic = 0;
-	pp->pln_ship = pp->pln_land = -1;
+	ship = land = -1;
     }
 
-    if (CANT_HAPPEN(pp->pln_ship >= 0 && pp->pln_land >= 0))
-	pp->pln_land = -1;
-    if (oldpp->pln_ship != pp->pln_ship)
-	pln_carrier_change(pp, EF_SHIP, oldpp->pln_ship, pp->pln_ship);
-    if (oldpp->pln_land != pp->pln_land)
-	pln_carrier_change(pp, EF_LAND, oldpp->pln_land, pp->pln_land);
+    if (CANT_HAPPEN(ship >= 0 && land >= 0))
+	land = -1;
+    if (oldpp->pln_ship != ship)
+	pln_carrier_change(pp, EF_SHIP, oldpp->pln_ship, ship);
+    if (oldpp->pln_land != land)
+	pln_carrier_change(pp, EF_LAND, oldpp->pln_land, land);
 
-    /* We've avoided assigning to pp->pln_own, in case oldpp == pp */
     if (oldpp->pln_own != own) {
 	lost_and_found(EF_PLANE, oldpp->pln_own, own,
 		       pp->pln_uid, pp->pln_x, pp->pln_y);
@@ -86,6 +89,8 @@ pln_prewrite(int n, void *old, void *new)
     }
 
     pp->pln_own = own;
+    pp->pln_ship = ship;
+    pp->pln_land = land;
     if (!own || pp->pln_x != oldpp->pln_x || pp->pln_y != oldpp->pln_y)
 	unit_update_cargo((struct empobj *)pp);
 }