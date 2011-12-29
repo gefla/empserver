gefla/empserver

commit 73260a8a6dbe8e6673480765f3db6127781aa292

Factor ac_dog_report() out of ac_dog()

src/lib/subs/aircombat.c
@@ -431,11 +431,24 @@ ac_airtoair(struct emp_qelem *att_list, struct emp_qelem *int_list)
 }
 
 static void
+ac_dog_report(natid to, int intensity, double odds,
+	      struct plist *p1, int val1, int dam1, char *dam_mesg1,
+	      struct plist *p2, int val2, int dam2, char *dam_mesg2)
+{
+    mpr(to, " %3.3s #%-4d  %3.3s #%-4d   %3d/%-3d %3d  %3.2f  %3d/%-3d"
+	"%-13.13s %-13.13s\n",
+	p1->pcp->pl_name, p1->plane.pln_uid,
+	p2->pcp->pl_name, p2->plane.pln_uid,
+	val1, val2, intensity, odds, dam1, dam2,
+	dam_mesg1, dam_mesg2);
+}
+
+static void
 ac_dog(struct plist *ap, struct plist *dp)
 {
     int att, def;
     double odds;
-    int intensity;
+    int intensity, i;
     natid att_own, def_own;
     int adam, ddam;
     char adam_mesg[14], ddam_mesg[14];
@@ -443,13 +456,6 @@ ac_dog(struct plist *ap, struct plist *dp)
     att_own = ap->plane.pln_own;
     def_own = dp->plane.pln_own;
 
-    PR(att_own, " %3.3s #%-4d  %3.3s #%-4d",
-       ap->pcp->pl_name,
-       ap->plane.pln_uid, dp->pcp->pl_name, dp->plane.pln_uid);
-    if (def_own)
-	PR(def_own, " %3.3s #%-4d  %3.3s #%-4d",
-	   dp->pcp->pl_name,
-	   dp->plane.pln_uid, ap->pcp->pl_name, ap->plane.pln_uid);
     att = pln_att(&ap->plane);
     if (att == 0)
 	att = pln_def(&ap->plane);
@@ -478,33 +484,31 @@ ac_dog(struct plist *ap, struct plist *dp)
 	odds = 0.05;
     intensity = roll(20) + roll(20) + roll(20) + roll(20) + 1;
 
-    PR(att_own, "   %3d/%-3d %3d  %3.2f  ", att, def, intensity, odds);
-    PR(def_own, "   %3d/%-3d %3d  %3.2f  ", def, att, intensity, odds);
-
     adam = 0;
     ddam = 0;
-    while ((intensity--) > 0) {
-
+    for (i = 0; i < intensity; i++) {
 	if (chance(odds)) {
-	    ddam += 1;
-	    if ((dp->plane.pln_effic - ddam) < PLANE_MINEFF)
-		intensity = 0;
+	    ddam++;
+	    if (dp->plane.pln_effic - ddam < PLANE_MINEFF)
+		break;
 	} else {
-	    adam += 1;
-	    if ((ap->plane.pln_effic - adam) < PLANE_MINEFF)
-		intensity = 0;
+	    adam++;
+	    if (ap->plane.pln_effic - adam < PLANE_MINEFF)
+		break;
 	}
     }
 
     if (dp->pcp->pl_flags & P_M)
 	ddam = 100;
 
-    PR(att_own, "%3d/%-3d", adam, ddam);
-    PR(def_own, "%3d/%-3d", ddam, adam);
     ac_planedamage(ap, def_own, adam, 0, adam_mesg);
     ac_planedamage(dp, att_own, ddam, 0, ddam_mesg);
-    PR(att_own, "%-13.13s %-13.13s\n", adam_mesg, ddam_mesg);
-    PR(def_own, "%-13.13s %-13.13s\n", ddam_mesg, adam_mesg);
+    ac_dog_report(att_own, intensity, odds,
+		  ap, att, adam, adam_mesg,
+		  dp, def, ddam, ddam_mesg);
+    ac_dog_report(def_own, intensity, odds,
+		  dp, def, ddam, ddam_mesg,
+		  ap, att, adam, adam_mesg);
 
     if (opt_HIDDEN) {
 	setcont(att_own, def_own, FOUND_FLY);

commit 14af9e5b96af3c259e5ad1d27af6fc358cc565de

Fix use-after-free when plane is downed or aborted in dogfight
ac_dog() passed ap and dp to ac_dog_report() after ac_planedamage()
freed it.  Broken in commit 73260a8a, v4.3.27.  Diagnosed with
valgrind.

src/lib/subs/aircombat.c
@@ -30,7 +30,7 @@
  *     Dave Pare, 1986
  *     Thomas Ruschak, 1992
  *     Steve McClure, 1996
- *     Markus Armbruster, 2006-2010
+ *     Markus Armbruster, 2006-2011
  */
 
 #include <config.h>
@@ -61,7 +61,6 @@ static void ac_intercept(struct emp_qelem *, struct emp_qelem *,
 static void ac_combat_headers(natid, natid);
 static void ac_airtoair(struct emp_qelem *, struct emp_qelem *);
 static void ac_dog(struct plist *, struct plist *);
-static void ac_planedamage(struct plist *, natid, int, int, char *);
 static void ac_putplane(struct plist *, int);
 static void ac_doflak(struct emp_qelem *, struct sctstr *);
 static void ac_landflak(struct emp_qelem *, coord, coord);
@@ -450,7 +449,7 @@ ac_dog(struct plist *ap, struct plist *dp)
     double odds;
     int intensity, i;
     natid att_own, def_own;
-    int adam, ddam;
+    int adam, ddam, adisp, ddisp;
     char adam_mesg[14], ddam_mesg[14];
 
     att_own = ap->plane.pln_own;
@@ -501,38 +500,21 @@ ac_dog(struct plist *ap, struct plist *dp)
     if (dp->pcp->pl_flags & P_M)
 	ddam = 100;
 
-    ac_planedamage(ap, def_own, adam, 0, adam_mesg);
-    ac_planedamage(dp, att_own, ddam, 0, ddam_mesg);
+    adisp = ac_damage_plane(&ap->plane, def_own, adam, 0, adam_mesg);
+    ddisp = ac_damage_plane(&dp->plane, att_own, ddam, 0, ddam_mesg);
     ac_dog_report(att_own, intensity, odds,
 		  ap, att, adam, adam_mesg,
 		  dp, def, ddam, ddam_mesg);
     ac_dog_report(def_own, intensity, odds,
 		  dp, def, ddam, ddam_mesg,
 		  ap, att, adam, adam_mesg);
+    ac_putplane(ap, adisp);
+    ac_putplane(dp, ddisp);
 
     if (opt_HIDDEN) {
 	setcont(att_own, def_own, FOUND_FLY);
 	setcont(def_own, att_own, FOUND_FLY);
     }
-}
-
-/*
- * zap plane associated with plp.
- * Damaging country is "from", damage is "dam".
- *
- * NOTE: This routine removes the appropriate plane element from the
- * queue if it gets destroyed.  That means that the caller must assume
- * that the current queue pointer is invalid on return from the ac_planedamage
- * call.  (this has caused bugs in the past)
- */
-static void
-ac_planedamage(struct plist *plp, natid from, int dam, int flak,
-	       char *mesg)
-{
-    int disp;
-
-    disp = ac_damage_plane(&plp->plane, from, dam, flak, mesg);
-    ac_putplane(plp, disp);
 }
 
 int
@@ -570,6 +552,12 @@ ac_damage_plane(struct plnstr *pp, natid from, int dam, int flak,
     return disp;
 }
 
+/*
+ * NOTE: This routine may remove the appropriate plane element from the
+ * queue if it gets destroyed.  That means that the caller must assume
+ * that the current queue pointer is invalid on return from the
+ * call.  (this has caused bugs in the past)
+ */
 static void
 ac_putplane(struct plist *plp, int disp)
 {