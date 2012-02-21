gefla/empserver

commit 94a3108b23d23afa8e65aa8c138845cc905c3c28

Fix marking of unused trade slots
Trade code can't quite decide whether negative trd_unitid or zero
trd_owner marks unused slots.  The former is a bad idea, because blank
slots have a zero trd_unitid.

Make sure to zero trd_owner when setting trd_unitid to negative value
in trad() and check_trade().  This fixes recognition of unused slots
in set (broken in commit e16e38df, v4.2.18) and xdump (never worked).

src/lib/commands/trad.c
@@ -133,6 +133,7 @@ trad(void)
     }
     if (!trade_getitem(&trade, &tg)) {
 	pr("Can't find trade #%d!\n", trade.trd_unitid);
+	trade.trd_owner = 0;
 	trade.trd_unitid = -1;
 	if (!puttrade(lotno, &trade)) {
 	    logerror("trad: can't write trade");
@@ -289,12 +290,14 @@ check_trade(void)
 	if (!trade_getitem(&trade, &tg))
 	    continue;
 	if (tg.gen.own == 0) {
+	    trade.trd_owner = 0;
 	    trade.trd_unitid = -1;
 	    puttrade(n, &trade);
 	    continue;
 	}
 	if (tg.gen.own != trade.trd_owner) {
 	    logerror("Something weird, tg.gen.own != trade.trd_owner!\n");
+	    trade.trd_owner = 0;
 	    trade.trd_unitid = -1;
 	    puttrade(n, &trade);
 	    continue;
@@ -312,6 +315,7 @@ check_trade(void)
 	    continue;
 
 	saveid = trade.trd_unitid;
+	trade.trd_owner = 0;
 	trade.trd_unitid = -1;
 	if (!puttrade(n, &trade)) {
 	    logerror("Couldn't save trade after purchase; get help!\n");

commit b4f076fc3651c4baecd82a3a2069873ec1722431

Fix market not to expropriate sellers of units
When a ship, plane, land unit or nuke is sold, the seller is replaced
by POGO: POGO gets the money, the telegrams and makes the news.
Likewise when a sale fails because the buyer can't pay.

Broken in commit 94a3108b, v4.3.17.  Reported by Scott C. Zielinski.

src/lib/commands/trad.c
@@ -283,6 +283,7 @@ check_trade(void)
     double tleft;
     float price;
     int saveid;
+    natid seller;
 
     for (n = 0; gettrade(n, &trade); n++) {
 	if (trade.trd_unitid < 0)
@@ -315,6 +316,7 @@ check_trade(void)
 	    continue;
 
 	saveid = trade.trd_unitid;
+	seller = trade.trd_owner;
 	trade.trd_owner = 0;
 	trade.trd_unitid = -1;
 	if (!puttrade(n, &trade)) {
@@ -325,17 +327,17 @@ check_trade(void)
 	price = trade.trd_price;
 	natp = getnatp(trade.trd_maxbidder);
 	if (natp->nat_money < price) {
-	    nreport(trade.trd_maxbidder, N_WELCH_DEAL, trade.trd_owner, 1);
-	    wu(0, trade.trd_owner,
+	    nreport(trade.trd_maxbidder, N_WELCH_DEAL, seller, 1);
+	    wu(0, seller,
 	       "%s tried to buy a %s #%d from you for $%.2f\n",
 	       cname(trade.trd_maxbidder), trade_nameof(&trade, &tg),
 	       saveid, price * tradetax);
-	    wu(0, trade.trd_owner, "   but couldn't afford it.\n");
-	    wu(0, trade.trd_owner,
+	    wu(0, seller, "   but couldn't afford it.\n");
+	    wu(0, seller,
 	       "   Your item was taken off the market.\n");
 	    wu(0, trade.trd_maxbidder,
 	       "You tried to buy %s #%d from %s for $%.2f\n",
-	       trade_nameof(&trade, &tg), saveid, cname(trade.trd_owner),
+	       trade_nameof(&trade, &tg), saveid, cname(seller),
 	       price);
 	    wu(0, trade.trd_maxbidder, "but couldn't afford it.\n");
 	    continue;
@@ -346,7 +348,7 @@ check_trade(void)
 	natp->nat_money -= price;
 	putnat(natp);
 
-	natp = getnatp(trade.trd_owner);
+	natp = getnatp(seller);
 	natp->nat_money += roundavg(price * tradetax);
 	putnat(natp);
 
@@ -394,13 +396,13 @@ check_trade(void)
 	unit_give_away(&tg.gen, trade.trd_maxbidder, 0);
 	put_empobj(trade.trd_type, saveid, &tg.gen);
 
-	nreport(trade.trd_owner, N_MAKE_SALE, trade.trd_maxbidder, 1);
-	wu(0, trade.trd_owner, "%s bought a %s #%d from you for $%.2f\n",
+	nreport(seller, N_MAKE_SALE, trade.trd_maxbidder, 1);
+	wu(0, seller, "%s bought a %s #%d from you for $%.2f\n",
 	   cname(trade.trd_maxbidder), trade_nameof(&trade, &tg),
 	   saveid, price * tradetax);
 	wu(0, trade.trd_maxbidder,
 	   "The bidding is over & you bought %s #%d from %s for $%.2f\n",
-	   trade_nameof(&trade, &tg), saveid, cname(trade.trd_owner),
+	   trade_nameof(&trade, &tg), saveid, cname(seller),
 	   price);
     }
     return RET_OK;