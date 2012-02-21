gefla/empserver

commit 17223e8fe2956f0db878480caa9e4add844216d9

Fix read not to split production report when update is slow
Adjacent telegrams are squashed together if type and sender are the
same, and the timestamp is "close enough".  This is done in two
places: rea() and typed_wu().  They're inconsistent: typed_wu()
ignores the timestamp for production reports since Empire 2, but rea()
doesn't.

Record typed_wu()'s decision in new telstr member tel_cont.  Use it in
rea().

src/lib/commands/rea.c
@@ -31,7 +31,7 @@
  *     Doug Hay, 1998
  *     Steve McClure, 1998-2000
  *     Ron Koenderink, 2005-2007
- *     Markus Armbruster, 2009
+ *     Markus Armbruster, 2009-2011
  */
 
 #include <config.h>
@@ -62,10 +62,6 @@ rea(void)
     int teles;
     int size;
     char buf[1024];
-    int lasttype;
-    int lastcnum;
-    time_t lastdate;
-    int header;
     int filelen;
     char *kind;
     int n, res;
@@ -74,7 +70,6 @@ rea(void)
     time_t now;
     time_t then;
     time_t delta;
-    int first = 1;
     int may_delete = 1; /* may messages be deleted? */
 
     now = time(NULL);
@@ -108,9 +103,6 @@ rea(void)
     teles = 0;
     size = fsize(fileno(telfp));
   more:
-    lastdate = 0;
-    lastcnum = -1;
-    lasttype = -1;
     while ((res = tel_read_header(telfp, mbox, &tgm)) > 0) {
 	if (*kind == 'a') {
 	    if ((!player->god && (getrejects(tgm.tel_from, np) & REJ_ANNO))
@@ -121,26 +113,16 @@ rea(void)
 		continue;
 	    }
 	}
-	if (first && *kind == 'a') {
+	if (!teles && *kind == 'a')
 	    pr("\nAnnouncements since %s", ctime(&then));
-	    first = 0;
-	}
-	header = 0;
-	if (tgm.tel_type != lasttype || tgm.tel_from != lastcnum)
-	    header++;
-	if (abs((int)(tgm.tel_date - (long)lastdate)) > TEL_SECONDS)
-	    header++;
-	if (header) {
+	if (!teles || !tgm.tel_cont) {
 	    pr("\n> ");
-	    lastcnum = tgm.tel_from;
-	    lasttype = tgm.tel_type;
 	    pr("%s ", telnames[tgm.tel_type]);
 	    if ((tgm.tel_type == TEL_NORM) ||
 		(tgm.tel_type == TEL_ANNOUNCE) ||
 		(tgm.tel_type == TEL_BULLETIN))
 		pr("from %s, (#%d)", cname(tgm.tel_from), tgm.tel_from);
 	    pr("  dated %s", ctime(&tgm.tel_date));
-	    lastdate = tgm.tel_date;
 	}
 	teles++;
 	res = tel_read_body(telfp, mbox, &tgm, print_sink, NULL);

include/tel.h
@@ -27,7 +27,7 @@
  *  tel.h: Definitions for things having to do with telegrams
  *
  *  Known contributors to this file:
- *     Markus Armbruster, 2009-2010
+ *     Markus Armbruster, 2009-2011
  */
 
 #ifndef TEL_H
@@ -50,6 +50,7 @@
 struct telstr {
     natid tel_from;		/* sender */
     unsigned char tel_type;
+    signed char tel_cont;	/* squash into prev. telegram? */
     unsigned tel_length;	/* how long */
     time_t tel_date;		/* when sent */
 };

src/lib/subs/wu.c
@@ -28,6 +28,7 @@
  *
  *  Known contributors to this file:
  *     Steve McClure, 2000
+ *     Markus Armbruster, 2005-2011
  */
 
 #include <config.h>
@@ -145,6 +146,7 @@ typed_wu(natid from, natid to, char *message, int type)
     len = strlen(message);
     tel.tel_length = len;
     tel.tel_type = type;
+    tel.tel_cont = !telegram_is_new(to, &tel);
     iov[0].iov_base = &tel;
     iov[0].iov_len = sizeof(tel);
     iov[1].iov_base = message;
@@ -169,7 +171,7 @@ typed_wu(natid from, natid to, char *message, int type)
 	    putnat(np);
 	}
     } else {
-	if (!np->nat_tgms || telegram_is_new(to, &tel)) {
+	if (!np->nat_tgms || !tel.tel_cont) {
 	    np->nat_tgms++;
 	    putnat(np);
 	    if (np->nat_flags & NF_INFORM) {

commit c72e2cbf25668ec32d86496e20714eb3629d2c0b

read can print telegram without header after deletion prompt
rea() loops if more telegrams arrive while we wait for the player to
confirm deletion.  If the first new one is a continuation of the last
old one, its header is suppressed.  Don't do that.

Broken in commit 17223e8f, v4.3.29.

src/lib/commands/rea.c
@@ -59,7 +59,7 @@ rea(void)
     char mbox_buf[256];		/* Maximum path length */
     struct telstr tgm;
     FILE *telfp;
-    int teles;
+    int teles, need_hdr;
     char buf[1024];
     char *kind;
     int n, res;
@@ -99,6 +99,7 @@ rea(void)
 	return RET_FAIL;
     }
     teles = 0;
+    need_hdr = 1;
     for (;;) {
 	res = tel_read_header(telfp, mbox, &tgm);
     more:
@@ -115,7 +116,7 @@ rea(void)
 	}
 	if (!teles && *kind == 'a')
 	    pr("\nAnnouncements since %s", ctime(&then));
-	if (!teles || !tgm.tel_cont) {
+	if (need_hdr || !tgm.tel_cont) {
 	    pr("\n> ");
 	    pr("%s ", telnames[tgm.tel_type]);
 	    if ((tgm.tel_type == TEL_NORM) ||
@@ -125,6 +126,7 @@ rea(void)
 	    pr("  dated %s", ctime(&tgm.tel_date));
 	}
 	teles++;
+	need_hdr = 0;
 	res = tel_read_body(telfp, mbox, &tgm, print_sink, NULL);
 	if (res < 0)
 	    break;
@@ -169,6 +171,7 @@ rea(void)
 		res = tel_read_header(telfp, mbox, &tgm);
 		if (res != 0) {
 		    pr("Wait a sec!  A new %s has arrived...\n", kind);
+		    need_hdr = 1;
 		    goto more;
 		}
 		/* Here, we just re-open the file for "w" only,