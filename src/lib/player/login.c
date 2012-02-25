gefla/empserver

commit 8549efbc1904e377c05e385844fe1adada61d573

Clean up how quit and server shutdown trigger connection close
Simply set the player connection's EOF indicator.  Cleaner than
setting player->state to PS_SHUTDOWN from random places.

Move the assignment of PS_PLAYING from player_main() to its caller
play_cmd(), so that player->state is exclusively controlled in
login.c.

src/lib/player/login.c
@@ -86,7 +86,7 @@ player_login(void *ud)
 
     pr_id(player, C_INIT, "Empire server ready\n");
 
-    while (player->state != PS_SHUTDOWN) {
+    for (;;) {
 	io_output(player->iop, 1);
 	if (io_gets(player->iop, buf, sizeof(buf)) < 0) {
 	    res = io_input(player->iop, 1);
@@ -355,9 +355,11 @@ play_cmd(void)
     empth_set_name(empth_self(), buf);
     logerror("%s logged in as country #%d", praddr(player), player->cnum);
     pr_id(player, C_INIT, "%d\n", CLIENTPROTO);
+    player->state = PS_PLAYING;
     player_main(player);
     logerror("%s logged out, country #%d", praddr(player), player->cnum);
-    player->state = PS_SHUTDOWN;
+    if (CANT_HAPPEN(!io_eof(player->iop)))
+	io_set_eof(player->iop);
     return RET_OK;
 }
 

src/lib/player/player.c
@@ -59,7 +59,6 @@ player_main(struct player *p)
     struct natstr *natp;
     char buf[128];
 
-    p->state = PS_PLAYING;
     player = p;
     time(&player->curup);
     update_timeused_login(player->curup);
@@ -174,7 +173,6 @@ status(void)
     time(&player->curup);
     update_timeused(player->curup);
     if (io_error(player->iop) || io_eof(player->iop)
-	|| player->state == PS_SHUTDOWN
 	|| !may_play_now(natp, player->curup))
 	return 0;
 
@@ -284,7 +282,7 @@ show_motd(void)
 int
 quit(void)
 {
-    player->state = PS_SHUTDOWN;
+    io_set_eof(player->iop);
     return RET_OK;
 }
 

src/server/main.c
@@ -419,9 +419,9 @@ shutdwn(int sig)
 	if (p->state != PS_PLAYING)
 	    continue;
 	pr_flash(p, "Server shutting down...\n");
-	p->state = PS_SHUTDOWN;
+	io_set_eof(p->iop);
+	p->aborted = 1;
 	p->may_sleep = PLAYER_SLEEP_NEVER;
-	p->aborted++;
 	if (p->command) {
 	    pr_flash(p, "Shutdown aborting command\n");
 	}

commit 9b92ac633d8efd8e8b52c9287193066bcefd303d

Fix how play_cmd() ensures connection close
play_cmd() needs to return in a state that makes player_login() break
the login command loop.

play_cmd() assumes player_main() always returns in such a state:
connection's EOF indicator set.  Unfortunately, the assumption is
wrong.  Fortunately, play_cmd() checks it, oopses and recovers.

player_main() can return with neiter error nor EOF indicator set,
e.g. when the game is down.

player_main() can return with just the error indicator set.  For
instance, when the client dies, io_input() detects ECONNRESET, sets
the error indicator and fails.

Broken in commit 8549efbc.  Fix by setting the EOF indicator silently
(without oopsing) when necessary.

src/lib/player/login.c
@@ -29,7 +29,7 @@
  *  Known contributors to this file:
  *     Dave Pare, 1994
  *     Steve McClure, 2000
- *     Markus Armbruster, 2004-2011
+ *     Markus Armbruster, 2004-2012
  *     Ron Koenderink, 2005-2009
  */
 
@@ -357,7 +357,7 @@ play_cmd(void)
     player->state = PS_PLAYING;
     player_main(player);
     logerror("%s logged out, country #%d", praddr(player), player->cnum);
-    if (CANT_HAPPEN(!io_eof(player->iop)))
+    if (!io_eof(player->iop) && !io_error(player->iop))
 	io_set_eof(player->iop);
     return RET_OK;
 }