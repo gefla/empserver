d381351c6521552eb901160a982c53c40c63c769
Markus Armbruster <armbru@pond.sub.org>
2012-03-11 11:43:38 +0100

    Flush all output before reading a login command, not just some
    Before, a client could theoretically make the output queue grow
    without bounds.

--- src/lib/player/login.c
+++ src/lib/player/login.c
@@ -87,7 +87,11 @@ player_login(void *ud)
     pr_id(player, C_INIT, "Empire server ready\n");
 
     for (;;) {
-	io_output(player->iop, (time_t)-1);
+	if (io_outputwaiting(player->iop)) {
+	    if (io_output(player->iop, (time_t)-1) <= 0)
+		break;
+	    continue;
+	}
 	if (io_gets(player->iop, buf, sizeof(buf)) < 0) {
 	    res = io_input(player->iop, player->curup + minutes(max_idle));
 	    if (res <= 0) {
