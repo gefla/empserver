gefla/empserver

commit 0a4d77e919028c54f5e99fdc883ec04bae92d882

Simplify checks whether player thread may sleep
A player thread may sleep on input or output, except:

(1) While it is executing a C_MOD command, it may only sleep on input.

(2) While it is being aborted by the update or shutdown, it may not
    sleep at all.

To find out whether a player thread may sleep on input, code has to
check condition (2).  It needs do to that in recvclient().

To find out whether it may sleep on output, it has to check both
conditions.  It needs to do that in pr_player() and upr_player().

The code tracked condition (1) in global variable play_lock_wanted.
It checked condition (2) by examining struct player member command.

Replace all that by new struct player member may_sleep.  Initialize it
in player_new(), update it in dispatch(), shutdwn() and update_run().
This makes the tests in recvclient(), pr_player() and upr_player()
obvious.  play_wrlock_wanted() is now unused, remove it.

src/lib/player/dispatch.c
@@ -85,6 +85,8 @@ dispatch(char *buf, char *redir)
 	pr("Command not implemented\n");
 	return 0;
     }
+    player->may_sleep = command->c_flags & C_MOD
+	? PLAYER_SLEEP_ON_INPUT : PLAYER_SLEEP_FREELY;
     player->command = command;
     empth_rwlock_rdlock(play_lock);
     if (redir) {
@@ -109,5 +111,6 @@ dispatch(char *buf, char *redir)
     }
     empth_rwlock_unlock(play_lock);
     player->command = NULL;
+    player->may_sleep = PLAYER_SLEEP_FREELY;
     return 0;
 }

include/player.h
@@ -51,6 +51,10 @@
 #define	CAP		bit(6)
 #define	MONEY		bit(7)
 
+enum player_sleep {
+    PLAYER_SLEEP_NEVER, PLAYER_SLEEP_ON_INPUT, PLAYER_SLEEP_FREELY
+};
+
 struct player {
     struct emp_qelem queue;
     empth_t *proc;
@@ -76,6 +80,7 @@ struct player {
     int simulation;		/* e.g. budget command */
     double dolcost;
     time_t curup;		/* when last input was received */
+    enum player_sleep may_sleep; /* when may thread sleep? */
     int aborted;		/* interrupt cookie or EOF received? */
     int eof;			/* EOF (cookie or real) received? */
     int recvfail;		/* #recvclient() failures */

include/server.h
@@ -40,7 +40,6 @@
 
 extern int shutdown_pending;
 extern empth_rwlock_t *play_lock;
-extern int play_wrlock_wanted;
 extern int update_running;
 extern time_t update_time[UPDATE_TIME_LEN];
 

src/lib/player/accept.c
@@ -89,6 +89,7 @@ player_new(int s)
 	}
 	emp_insque(&lp->queue, &Players);
 	lp->cnum = NATID_BAD;
+	lp->may_sleep = PLAYER_SLEEP_FREELY;
 	lp->curid = -1;
 	time(&lp->curup);
     }

src/lib/player/recvclient.c
@@ -39,7 +39,6 @@
 #include "journal.h"
 #include "player.h"
 #include "prototypes.h"
-#include "server.h"
 
 /*
  * Receive a line of input from the current player.
@@ -80,7 +79,9 @@ recvclient(char *cmd, int size)
 	 * Flush all queued output before potentially sleeping in
 	 * io_input(), to make sure player sees the prompt.
 	 */
-	while (io_output(player->iop, !play_wrlock_wanted) > 0) ;
+	while (io_output(player->iop,
+			 player->may_sleep >= PLAYER_SLEEP_ON_INPUT) > 0)
+	    ;
 
 	/*
 	 * If io_output_all() blocked and got unblocked by command

src/lib/subs/pr.c
@@ -244,9 +244,7 @@ pr_player(struct player *pl, int id, char *buf)
 
     if (player == pl) {
 	while (io_output_if_queue_long(pl->iop,
-			!play_wrlock_wanted
-			&& !(pl->command && (pl->command->c_flags & C_MOD)))
-	       > 0)
+			pl->may_sleep == PLAYER_SLEEP_FREELY) > 0)
 	    ;
     }
 }
@@ -302,9 +300,7 @@ upr_player(struct player *pl, int id, char *buf)
 
     if (player == pl) {
 	while (io_output_if_queue_long(pl->iop,
-			!play_wrlock_wanted
-			&& !(pl->command && (pl->command->c_flags & C_MOD)))
-	       > 0)
+			pl->may_sleep == PLAYER_SLEEP_FREELY) > 0)
 	    ;
     }
 }

src/server/main.c
@@ -86,13 +86,6 @@ static void loc_NTTerm(void);
  */
 empth_rwlock_t *play_lock;
 
-/*
- * Is a thread attempting to take an exclusive play_lock?
- * Threads holding a shared play_lock must not sleep while this is
- * true.
- */
-int play_wrlock_wanted;
-
 static char pidfname[] = "server.pid";
 
 /* Run as daemon?  If yes, detach from controlling terminal etc. */
@@ -421,12 +414,12 @@ shutdwn(int sig)
 
     logerror("Shutdown commencing (cleaning up threads.)");
 
-    play_wrlock_wanted = 1;
     for (p = player_next(NULL); p; p = player_next(p)) {
 	if (p->state != PS_PLAYING)
 	    continue;
 	pr_flash(p, "Server shutting down...\n");
 	p->state = PS_SHUTDOWN;
+	p->may_sleep = PLAYER_SLEEP_NEVER;
 	p->aborted++;
 	if (p->command) {
 	    pr_flash(p, "Shutdown aborting command\n");

src/server/update.c
@@ -193,12 +193,12 @@ update_run(void)
 {
     struct player *p;
 
-    play_wrlock_wanted = 1;
     for (p = player_next(NULL); p; p = player_next(p)) {
 	if (p->state != PS_PLAYING)
 	    continue;
 	if (p->command) {
 	    pr_flash(p, "Update aborting command\n");
+	    p->may_sleep = PLAYER_SLEEP_NEVER;
 	    p->aborted = 1;
 	    empth_wakeup(p->proc);
 	}
@@ -206,14 +206,13 @@ update_run(void)
     empth_rwlock_wrlock(play_lock);
     if (*pre_update_hook) {
 	if (run_hook(pre_update_hook, "pre-update")) {
-	    play_wrlock_wanted = 0;
 	    empth_rwlock_unlock(play_lock);
 	    return;
 	}
     }
     update_running = 1;
     update_main();
-    play_wrlock_wanted = update_running = 0;
+    update_running = 0;
     empth_rwlock_unlock(play_lock);
 }
 

commit fb9595fe6a4cfc1b113668cbfc5e90484c4bddc2

Fix unwanted player thread blocking on output during shutdown
shutdwn() disables blocking on I/O for all player threads in state
PS_PLAYING, by setting struct player member may_sleep to
PLAYER_SLEEP_NEVER.  This ensures the player threads complete logout
quickly and reliably.  A thread may still block on I/O in io_close()
called from player_delete(), since commit 904822e3, but that's okay,
because it happens after all game state updates.

Bug: if shutdwn() aborts a command, the player thread returns through
dispatch(), which resets may_sleep back to PLAYER_SLEEP_FREELY.  Input
can't block regardless, because the EOF indicator is set, but output
can.  When it happens, the player thread may not complete logout
before shutdwn() terminates the process.

This can make us lose a treasury update (similar to the bug fixed by
commit bdc1c40f; the relevant bug description is in commit note
6f8ca87f), play time update, and log entries.

How?  There are two paths from dispatch() to player_delete().  Here's
the first one:

  1. command()
     Doesn't print since dispatch() returns 0 when it resets may_sleep
  2. player_main()
     Loop and call status()
  3. status()
     If the command set dolcost to a non-trivial amount, print it
     Charge dolcost
     If player went broke or became solvent, notify him
     Charge time used
     Return 0, because shutdwn() set the EOF indicator
  4. player_main()
     Break the loop
     Charge time used
     print Bye-bye
     journal.log the logout
  5. play_cmd()
     server.log the logout
  6. player_login()
     Loop
     Try to flush output
     get EOF, break loop
     print so long
     call player_delete()

Ways the bug can bite:

A. When we block in 4. print Bye-bye, we can fail to log.

B. When we block in 3. print broke/solvent notification, we can
   additionally fail to charge time used.

C. When we block in 3. print dolcost, we can additionally fail to
   charge dolcost.

Note: B. and C. couldn't happen before commit bdc1c40f.  Instead,
something just like C happened always, whether player thread blocked
or not.

The second path:

  1. execute()
     Loop and call status()
  2. status()
     As above
  3. execute()
     break the loop
  4. dispatch()
     Continue with the first path

No additional ways to bite.

Fix by avoiding the may_sleep reset when the player thread is on its
way to terminate: may not sleep and has its EOF indicator set.

Broken in commit 0a4d77e9

src/lib/player/dispatch.c
@@ -29,7 +29,7 @@
  *  Known contributors to this file:
  *     Dave Pare, 1994
  *     Steve McClure, 1998
- *     Markus Armbruster, 2007-2011
+ *     Markus Armbruster, 2007-2012
  */
 
 #include <config.h>
@@ -112,6 +112,8 @@ dispatch(char *buf, char *redir)
     }
     empth_rwlock_unlock(play_lock);
     player->command = NULL;
-    player->may_sleep = PLAYER_SLEEP_FREELY;
+    if (player->may_sleep != PLAYER_SLEEP_NEVER || !io_eof(player->iop))
+	player->may_sleep = PLAYER_SLEEP_FREELY;
+    /* else we're being kicked out */
     return 0;
 }