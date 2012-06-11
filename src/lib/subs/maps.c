gefla/empserver

commit a00f9e200e610920bd59e5fa9537902149143424

Clean up bmap commands' flags argument
parse_map_flags() silently truncates map flags after the first 't' or
'r'.  This makes it accept arguments "true" and "revert".  However, it
also breaks the perfectly sensible argument "ts", which should show
ships just like "st", but doesn't.

info bmap & friends document arguments "true" and "revert", and also
suggest flags 't' and 'r'.  What a mess.

Make argument "revert" a special case.  Deprecate flag 'r', and clean
up truncation there.

Don't truncate after flag 't'.  If any bad flags follow, ignore
everything after 't', but deprecate that usage.

src/lib/subs/maps.c
@@ -41,6 +41,7 @@
 #include "file.h"
 #include "land.h"
 #include "map.h"
+#include "match.h"
 #include "misc.h"
 #include "nat.h"
 #include "nsc.h"
@@ -104,11 +105,21 @@ do_map(int bmap, int unit_type, char *arg, char *map_flags_arg)
     return draw_map(origin, map_flags, &ns);
 }
 
+static void
+warn_deprecated_arg(char *what, char *arg, char *use)
+{
+    pr("%s '%s' is deprecated and will go away in a future release.\n"
+       "Use %s instead.\n",
+       what, arg, use);
+}
+
 static int
 parse_map_flags(int bmap, char *str)
 {
     int map_flags;
     char *p;
+    int tflags = 0;
+    char *tp = NULL;
 
     switch (bmap) {
     default: CANT_REACH();
@@ -120,6 +131,10 @@ parse_map_flags(int bmap, char *str)
 
     if (!str)
 	return map_flags;
+
+    /* special case "revert" */
+    if (bmap == 'b' && mineq(str, "revert") != ME_MISMATCH)
+	return MAP_BMAP_REVERT;
 
     for (p = str; *p; p++) {
 	switch (*p) {
@@ -149,13 +164,29 @@ parse_map_flags(int bmap, char *str)
 	case 't':
 	    if (bmap != 'b')
 		goto bad_flag;
-	    return map_flags | MAP_ALT;
+	    map_flags |= MAP_ALT;
+	    /*
+	     * Flags following 't' used to be ignored.  That breaks
+	     * perfectly sensible "ts".  Try to continue, but save
+	     * state for when a bad flag is found.
+	     */
+	    if (!tflags) {
+		tflags = map_flags;
+		tp = p;
+	    }
+	    break;
 	case 'r':
-	    if (bmap != 'b')
+	    if (bmap != 'b' || tflags)
 		goto bad_flag;
+	    warn_deprecated_arg("Map flag", "r", "argument 'revert'");
 	    return MAP_BMAP_REVERT;
 	default:
 	bad_flag:
+	    if (tflags) {
+		/* ignore bad flags following 't' */
+		warn_deprecated_arg("Argument", tp, "map flag 't'");
+		return tflags;
+	    }
 	    pr("Bad flag %c!\n", *p);
 	    return -1;
 	}

info/bmap.t
@@ -77,12 +77,15 @@ A '*' will put all on the map.
 When multiple unit types are selected they are displayed
 in the following priority order: nukes, land units, ships, planes.
 To highlight sectors you own, specify the 'h' flag.
+With the 't' flag, the command shows your true bmap rather than your
+working bmap.
+.s1
+.L "Working and true bmap"
 .s1
 You actually have two bmaps.  One which is created by the server; this
 one is called your 'true' bmap.  You can view your true bmap at any
-time using the
-.SY "bmap <SECTS|SHIP> true"
-command.  However, usually when you view your bmap, you are looking at
+time using the 't' bmap flag.
+However, usually when you view your bmap, you are looking at
 your 'working' bmap.  The only difference between your working bmap
 and your true bmap is that your working bmap also contains
 designations put on there by you using the 'bdes' command, and by your

info/lbmap.t
@@ -79,12 +79,15 @@ A '*' will put all on the map.
 When multiple unit types are selected they are displayed
 in the following priority order: nukes, land units, ships, planes.
 To highlight sectors you own, specify the 'h' flag.
+With the 't' flag, the command shows your true bmap rather than your
+working bmap.
+.s1
+.L "Working and true bmap"
 .s1
 You actually have two bmaps.  One which is created by the server; this
 one is called your 'true' bmap.  You can view your true bmap at any
-time using the
-.SY "lbmap <SECTS|LAND UNIT> true"
-command.  However, usually when you view your bmap, you are looking at
+time using the 't' bmap flag.
+However, usually when you view your bmap, you are looking at
 your 'working' bmap.  The only difference between your working bmap
 and your true bmap is that your working bmap also contains
 designations put on there by you using the 'bdes' command, and by your

info/nbmap.t
@@ -77,12 +77,15 @@ A '*' will put all on the map.
 When multiple unit types are selected they are displayed
 in the following priority order: nukes, land units, ships, planes.
 To highlight sectors you own, specify the 'h' flag.
+With the 't' flag, the command shows your true bmap rather than your
+working bmap.
+.s1
+.L "Working and true bmap"
 .s1
 You actually have two bmaps.  One which is created by the server; this
 one is called your 'true' bmap.  You can view your true bmap at any
-time using the
-.SY "nbmap <SECTS|NUKE> true"
-command.  However, usually when you view your bmap, you are looking at
+time using the 't' bmap flag.
+However, usually when you view your bmap, you are looking at
 your 'working' bmap.  The only difference between your working bmap
 and your true bmap is that your working bmap also contains
 designations put on there by you using the 'bdes' command, and by your

info/pbmap.t
@@ -77,12 +77,15 @@ A '*' will put all on the map.
 When multiple unit types are selected they are displayed
 in the following priority order: nukes, land units, ships, planes.
 To highlight sectors you own, specify the 'h' flag.
+With the 't' flag, the command shows your true bmap rather than your
+working bmap.
+.s1
+.L "Working and true bmap"
 .s1
 You actually have two bmaps.  One which is created by the server; this
 one is called your 'true' bmap.  You can view your true bmap at any
-time using the
-.SY "pbmap <SECTS|PLANE> true"
-command.  However, usually when you view your bmap, you are looking at
+time using the 't' bmap flag.
+However, usually when you view your bmap, you are looking at
 your 'working' bmap.  The only difference between your working bmap
 and your true bmap is that your working bmap also contains
 designations put on there by you using the 'bdes' command, and by your

info/sbmap.t
@@ -79,12 +79,15 @@ A '*' will put all on the map.
 When multiple unit types are selected they are displayed
 in the following priority order: nukes, land units, ships, planes.
 To highlight sectors you own, specify the 'h' flag.
+With the 't' flag, the command shows your true bmap rather than your
+working bmap.
+.s1
+.L "Working and true bmap"
 .s1
 You actually have two bmaps.  One which is created by the server; this
 one is called your 'true' bmap.  You can view your true bmap at any
-time using the
-.SY "sbmap <SECTS|SHIP> true"
-command.  However, usually when you view your bmap, you are looking at
+time using the 't' bmap flag.
+However, usually when you view your bmap, you are looking at
 your 'working' bmap.  The only difference between your working bmap
 and your true bmap is that your working bmap also contains
 designations put on there by you using the 'bdes' command, and by your

info/sharebmap.t
@@ -29,7 +29,7 @@ not lose any information in this exchange.
 .s1
 Note that the sharebmap command only changes your 'working' bmap and
 so you can delete sharebmap information from your bmap by typing:
-.EX "bmap revert"
+.EX "bmap <SECTS> revert"
 See 'info bmap' for more details.
 .s1
 .SA "bdes, bmap, Communication, Maps"

commit 150b2c516528a284ad6b526b1d42e11248d9eb3f

Fix bmap commands not to parse empty flags argument as "revert"
Broken in commit a00f9e20, v4.3.27.

src/lib/subs/maps.c
@@ -144,7 +144,7 @@ parse_map_flags(int bmap, char *str)
     case 0:   map_flags = 0;
     }
 
-    if (!str)
+    if (!str || !*str)
 	return map_flags;
 
     /* special case "revert" */