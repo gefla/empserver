gefla/empserver

commit 3e370da58ca9b3543b55eb240ccfbe0f4f5131a8

Get rid of ship and land unit load counters
Load counters are redundant; they can be computed from the carrier
uids.  Keeping them up-to-date as the carriers change is a pain, and
we never got that quite complete.

Computing load counters straight from the carrier uids every time we
need them would be rather inefficient, but computing them from cargo
lists is not.  So do that.

Remove the load counters: struct shpstr members shp_nplane,
shp_nchoppers, shp_nxlight, shp_nland, and struct lndstr members
lnd_nxlight and lnd_nland.

Don't compute/update load counters in build_ship(), build_land(),
land(), ldump(), load_plane_ship(), load_land_ship(),
load_plane_land(), load_land_land(), lstat(), sdump(), shi(), sstat(),
tend_land(), check_trade(), put_combat(), pln_oneway_to_carrier_ok),
pln_newlanding(), fit_plane_on_ship(), fit_plane_on_land(),
unit_list().

Nothing left in fit_plane_off_ship(), fit_plane_off_land(), so remove
them.

load_land_ship(), load_land_land(), check_trade(), pln_newlanding(),
put_plane_on_ship(), take_plane_off_ship(), put_plane_on_land(),
take_plane_off_land() no longer change the carrier, so don't put it.

Remove functions to recompute the load counters from carrier uids:
count_units(), lnd_count_units(), count_planes(), count_land_planes(),
pln_fixup() and lnd_fixup(), along with the latter two's private
copies of fit_plane_on_ship() and fit_plane_on_land().

New cargo list functions to compute load counts: unit_cargo_count()
and unit_nplane(), with convenience wrappers shp_nplane(),
shp_nland(), lnd_nxlight(), lnd_nland().

Use them to make ship selectors nplane, nchoppers, nxlight, nland
virtual.  They now reflect what is loaded, not how the load uses the
available slots.  This makes a difference when x-light planes or
choppers use plane slots.

Use them to make land unit selectors nxlight and nland virtual.

Use them to get load counts in land(), ldump(), load_plane_ship(),

src/lib/subs/plnsub.c
@@ -202,8 +202,6 @@ pln_oneway_to_carrier_ok(struct emp_qelem *bomb_list,
     if (cno < 0 || !getship(cno, &ship))
 	return 0;
 
-    count_planes(&ship);
-
     /* for both lists */
     for (list = bomb_list;
 	 list;
@@ -232,8 +230,7 @@ pln_newlanding(struct emp_qelem *list, coord tx, coord ty, int cno)
     for (qp = list->q_forw; qp != list; qp = qp->q_forw) {
 	plp = (struct plist *)qp;
 	if (cno >= 0) {
-	    count_planes(&ship);
-	    if (!could_be_on_ship(&plp->plane, &ship))
+	    if (!could_be_on_ship(&plp->plane, &ship, 0, 0, 0, 0))
 		pr("\t%s cannot land on ship #%d! %s aborts!\n",
 		   prplane(&plp->plane), cno, prplane(&plp->plane));
 	    else if (!put_plane_on_ship(&plp->plane, &ship))
@@ -259,8 +256,6 @@ pln_newlanding(struct emp_qelem *list, coord tx, coord ty, int cno)
 	    plp->plane.pln_ship = cno;
 	}
     }
-    if (cno >= 0)
-	putship(ship.shp_uid, &ship);
 }
 
 void
@@ -813,80 +808,49 @@ pln_put1(struct plist *plp)
 }
 
 /*
+ * Can PP be loaded on a ship of SP's type?
+ * Assume that it already carries N planes, of which NCH are choppers,
+ * NXL xlight, NMSL light missiles, and the rest are light fixed-wing
+ * planes.
+ */
+int
+could_be_on_ship(struct plnstr *pp, struct shpstr *sp, 
+		 int n, int nch, int nxl, int nmsl)
+{
+    struct plchrstr *pcp = &plchr[pp->pln_type];
+    struct mchrstr *mcp = &mchr[sp->shp_type];
+
+    if (pcp->pl_flags & P_K)
+	nch++;
+    else if (pcp->pl_flags & P_E)
+	nxl++;
+    else if (!(pcp->pl_flags & P_L))
+	return 0;
+    else if (pcp->pl_flags & P_M)
+	nmsl++;
+    n++;
+
+    n -= MIN(nch, mcp->m_nchoppers);
+    n -= MIN(nxl, mcp->m_nxlight);
+    if (nmsl && !(mcp->m_flags & (M_MSL | M_FLY)))
+	return 0;		/* missile slots wanted */
+    if (nmsl < n && !(mcp->m_flags & M_FLY))
+	return 0;		/* fixed-wing slots wanted */
+    return n <= mcp->m_nplanes;
+}
+
+/*
  * Fit a plane of PP's type on ship SP.
- * Adjust SP's plane counters.
  * Updating the plane accordingly is the caller's job.
  * Return whether it fits.
  */
 static int
 fit_plane_on_ship(struct plnstr *pp, struct shpstr *sp)
 {
-    struct plchrstr *pcp = plchr + pp->pln_type;
-    struct mchrstr *mcp = mchr + sp->shp_type;
-    int wanted;
+    int n, nch, nxl, nmsl;
 
-    if (pcp->pl_flags & P_K) {
-	/* chopper, try chopper slot first */
-	if (sp->shp_nchoppers < mcp->m_nchoppers)
-	    return ++sp->shp_nchoppers;
-	/* else try plane slot */
-	wanted = M_FLY;
-    } else if (pcp->pl_flags & P_E) {
-	/* x-light, try x-light slot first */
-	if (sp->shp_nxlight < mcp->m_nxlight)
-	    return ++sp->shp_nxlight;
-	/* else try plane slot */
-	wanted = M_MSL | M_FLY;
-    } else if (!(pcp->pl_flags & P_L)) {
-	/* not light, no go */
-	wanted = 0;
-    } else if (pcp->pl_flags & P_M) {
-	/* missile, use plane slot */
-	wanted = M_MSL | M_FLY;
-    } else {
-	/* fixed-wing plane, use plane slot */
-	wanted = M_FLY;
-    }
-
-    if ((mcp->m_flags & wanted) == 0)
-	return 0;		/* ship not capable */
-
-    if (sp->shp_nplane < mcp->m_nplanes)
-	return ++sp->shp_nplane;
-
-    return 0;
-}
-
-/*
- * Fit a plane of PP's type off ship SP.
- * Adjust SP's plane counters, badly.  You need to run count_planes()
- * before the next fit_plane_on_ship().
- * Updating the plane accordingly is the caller's job.
- */
-static void
-fit_plane_off_ship(struct plnstr *pp, struct shpstr *sp)
-{
-    /*
-     * Frees chopper and nxlight slots first, which is why we need to
-     * run count_planes() before fit_plane_on_ship().
-     */
-    struct plchrstr *pcp = plchr + pp->pln_type;
-
-    if (pcp->pl_flags & P_K) {
-	if (sp->shp_nchoppers) {
-	    sp->shp_nchoppers--;
-	    return;
-	}
-    } else if (pcp->pl_flags & P_E) {
-	if (sp->shp_nxlight) {
-	    sp->shp_nxlight--;
-	    return;
-	}
-    }
-
-    if (CANT_HAPPEN(sp->shp_nplane == 0))
-	sp->shp_nplane = 1;
-    sp->shp_nplane--;
+    n = shp_nplane(sp, &nch, &nxl, &nmsl);
+    return could_be_on_ship(pp, sp, n, nch, nxl, nmsl);
 }
 
 int
@@ -902,7 +866,6 @@ put_plane_on_ship(struct plnstr *plane, struct shpstr *ship)
     plane->pln_y = ship->shp_y;
     plane->pln_ship = ship->shp_uid;
     putplane(plane->pln_uid, plane);
-    putship(ship->shp_uid, ship);
     return 1;
 }
 
@@ -912,15 +875,12 @@ take_plane_off_ship(struct plnstr *plane, struct shpstr *ship)
     if (CANT_HAPPEN(plane->pln_ship != ship->shp_uid))
 	return;
 
-    fit_plane_off_ship(plane, ship);
     plane->pln_ship = -1;
-    putship(ship->shp_uid, ship);
     putplane(plane->pln_uid, plane);
 }
 
 /*
  * Fit a plane of PP's type on land unit LP.
- * Adjust LP's plane counters.
  * Updating the plane accordingly is the caller's job.
  * Return whether it fits.
  */
@@ -930,23 +890,7 @@ fit_plane_on_land(struct plnstr *pp, struct lndstr *lp)
     struct plchrstr *pcp = plchr + pp->pln_type;
     struct lchrstr *lcp = lchr + lp->lnd_type;
 
-    if ((pcp->pl_flags & P_E) && lp->lnd_nxlight < lcp->l_nxlight)
-	return ++lp->lnd_nxlight;
-
-    return 0;
-}
-
-/*
- * Fit a plane of PP's type off land unit LP.
- * Adjust LP's plane counters.
- * Updating the plane accordingly is the caller's job.
- */
-static void
-fit_plane_off_land(struct plnstr *pp, struct lndstr *lp)
-{
-    if (CANT_HAPPEN(lp->lnd_nxlight == 0))
-	lp->lnd_nxlight = 1;
-    lp->lnd_nxlight--;
+    return (pcp->pl_flags & P_E) && lnd_nxlight(lp) < lcp->l_nxlight;
 }
 
 int
@@ -962,7 +906,6 @@ put_plane_on_land(struct plnstr *plane, struct lndstr *land)
     plane->pln_y = land->lnd_y;
     plane->pln_land = land->lnd_uid;
     putplane(plane->pln_uid, plane);
-    putland(land->lnd_uid, land);
     return 1;
 }
 
@@ -972,23 +915,8 @@ take_plane_off_land(struct plnstr *plane, struct lndstr *land)
     if (CANT_HAPPEN(plane->pln_land != land->lnd_uid))
 	return;
 
-    fit_plane_off_land(plane, land);
     plane->pln_land = -1;
-    putland(land->lnd_uid, land);
     putplane(plane->pln_uid, plane);
-}
-
-/*
- * Could a plane of PP's type be on on ship SP?
- */
-int
-could_be_on_ship(struct plnstr *pp, struct shpstr *sp)
-{
-    struct shpstr ship;
-
-    ship = *sp;
-    ship.shp_nplane = ship.shp_nchoppers = ship.shp_nxlight = 0;
-    return fit_plane_on_ship(pp, &ship);
 }
 
 void
@@ -1033,65 +961,6 @@ plane_sweep(struct emp_qelem *plane_list, coord x, coord y)
 	writemap(player->cnum);
     sect.sct_mines = mines_there;
     putsect(&sect);
-}
-
-void
-count_planes(struct shpstr *sp)
-{
-    struct nstr_item ni;
-    struct plnstr plane;
-    int nplane, nchoppers, nxlight;
-
-    if (sp->shp_effic < SHIP_MINEFF)
-	return;
-
-    nplane = sp->shp_nplane;
-    sp->shp_nplane = 0;
-    nchoppers = sp->shp_nchoppers;
-    sp->shp_nchoppers = 0;
-    nxlight = sp->shp_nxlight;
-    sp->shp_nxlight = 0;
-
-    snxtitem_xy(&ni, EF_PLANE, sp->shp_x, sp->shp_y);
-    while (nxtitem(&ni, &plane)) {
-	if (plane.pln_own == 0)
-	    continue;
-	if (plane.pln_ship == sp->shp_uid) {
-	    if (!fit_plane_on_ship(&plane, sp))
-		CANT_REACH();
-	}
-    }
-
-    if (nplane != sp->shp_nplane ||
-	nxlight != sp->shp_nxlight || nchoppers != sp->shp_nchoppers) {
-	putship(sp->shp_uid, sp);
-    }
-}
-
-void
-count_land_planes(struct lndstr *lp)
-{
-    struct nstr_item ni;
-    struct plnstr plane;
-    int nplane;
-
-    if (lp->lnd_effic < LAND_MINEFF)
-	return;
-
-    nplane = lp->lnd_nxlight;
-    lp->lnd_nxlight = 0;
-
-    snxtitem_all(&ni, EF_PLANE);
-    while (nxtitem(&ni, &plane)) {
-	if (plane.pln_own == 0)
-	    continue;
-	if (plane.pln_land == lp->lnd_uid)
-	    if (!fit_plane_on_land(&plane, lp))
-		CANT_REACH();
-    }
-
-    if (lp->lnd_nxlight != nplane)
-	putland(lp->lnd_uid, lp);
 }
 
 int

include/land.h
@@ -70,7 +70,6 @@ struct lndstr {
     short lnd_ship;		/* pointer to transporting ship */
     signed char lnd_harden;	/* fortification */
     short lnd_retreat;		/* retreat percentage */
-    unsigned char lnd_nxlight;	/* How many xlight planes on board? */
     int lnd_rflags;		/* When do I retreat? */
     char lnd_rpath[RET_LEN];	/* retreat path */
     unsigned char lnd_rad_max;	/* max radius for this unit */
@@ -79,7 +78,6 @@ struct lndstr {
     short lnd_pstage;		/* plague stage */
     short lnd_ptime;		/* how many etus remain in this stage */
     short lnd_land;		/* pointer to transporting unit */
-    unsigned char lnd_nland;
     short lnd_access;		/* Last tick mob was updated (MOB_ACCESS) */
 };
 
@@ -164,6 +162,8 @@ extern void lnd_carrier_change(struct lndstr *, int, int, int);
 extern int lnd_first_on_ship(struct shpstr *);
 extern int lnd_first_on_land(struct lndstr *);
 extern int lnd_next_on_unit(int);
+extern int lnd_nxlight(struct lndstr *);
+extern int lnd_nland(struct lndstr *);
 
 extern int lnd_fire(struct lndstr *);
 extern double lnd_fire_range(struct lndstr *);
@@ -186,8 +186,6 @@ extern void lnd_submil(struct lndstr *, int);
 extern void lnd_takemob(struct emp_qelem *, double);
 extern int lnd_spyval(struct lndstr *);
 extern void intelligence_report(int, struct lndstr *, int, char *);
-extern void count_units(struct shpstr *);
-extern void lnd_count_units(struct lndstr *);
 extern void lnd_mar(struct emp_qelem *, double *, double *, int *, natid);
 extern int lnd_hardtarget(struct lndstr *);
 extern int lnd_mar_one_sector(struct emp_qelem *, int, natid, int);

include/prototypes.h
@@ -533,7 +533,6 @@ extern void pln_oninit(void *);
 extern void pln_postread(int, void *);
 extern void pln_prewrite(int, void *, void *);
 /* plnsub.c */
-extern void count_planes(struct shpstr *);
 extern int get_planes(struct nstr_item *, struct nstr_item *,
 		      char *, char *);
 extern struct sctstr *get_assembly_point(char *, struct sctstr *, char *);
@@ -541,7 +540,8 @@ extern int pln_onewaymission(struct sctstr *, int *, int *);
 extern int pln_oneway_to_carrier_ok(struct emp_qelem *,
 				    struct emp_qelem *, int);
 extern void pln_newlanding(struct emp_qelem *, coord, coord, int);
-extern int could_be_on_ship(struct plnstr *, struct shpstr *);
+extern int could_be_on_ship(struct plnstr *, struct shpstr *,
+			    int, int, int, int);
 extern int put_plane_on_ship(struct plnstr *, struct shpstr *);
 extern void pln_dropoff(struct emp_qelem *, struct ichrstr *,
 			coord, coord, int);
@@ -559,7 +559,6 @@ extern void pln_put1(struct plist *);
 extern void take_plane_off_ship(struct plnstr *, struct shpstr *);
 extern void take_plane_off_land(struct plnstr *, struct lndstr *);
 extern void plane_sweep(struct emp_qelem *, coord, coord);
-extern void count_land_planes(struct lndstr *);
 extern int put_plane_on_land(struct plnstr *, struct lndstr *);
 extern int pln_hitchance(struct plnstr *, int, int);
 extern int pln_damage(struct plnstr *, coord, coord, char, int *, int);

include/ship.h
@@ -80,8 +80,6 @@ struct shpstr {
     short shp_mission;		/* mission code */
     short shp_radius;		/* mission radius */
     /* end of part matching struct empobj */
-    unsigned char shp_nplane;	/* number of planes on board */
-    unsigned char shp_nland;	/* number of land units on board */
     coord shp_destx[2];		/* location for ship destination */
     coord shp_desty[2];
     i_type shp_tstart[TMAX];	/* what goods to pick up at start point */
@@ -97,8 +95,6 @@ struct shpstr {
     char shp_path[MAXSHPPATH];
     short shp_follow;
     char shp_name[MAXSHPNAMLEN]; /* name set with the "name" command */
-    unsigned char shp_nchoppers; /* How many choppers on board? */
-    unsigned char shp_nxlight;	/* How many xlight planes on board? */
     coord shp_orig_x;
     coord shp_orig_y;		/* Where we were built */
     natid shp_orig_own;		/* Who built us */
@@ -181,6 +177,9 @@ extern int shp_speed(struct shpstr *);
 extern int shp_visib(struct shpstr *);
 extern int shp_frnge(struct shpstr *);
 extern int shp_glim(struct shpstr *);
+
+extern int shp_nplane(struct shpstr *, int *, int *, int *);
+extern int shp_nland(struct shpstr *);
 
 extern int shp_dchrg(struct shpstr *);
 extern int shp_fire(struct shpstr *);

include/unit.h
@@ -48,6 +48,8 @@ extern void unit_cargo_init(void);
 extern void unit_carrier_change(struct empobj *, int, int, int);
 extern int unit_cargo_first(int, int, int);
 extern int unit_cargo_next(int, int);
+extern int unit_cargo_count(int, int, int);
+extern int unit_nplane(int, int, int *, int *, int *);
 extern int unit_onresize(int);
 
 extern void unit_list(struct emp_qelem *);

src/lib/commands/buil.c
@@ -359,10 +359,6 @@ build_ship(struct sctstr *sp, struct mchrstr *mp, short *vec, int tlev)
     } else {
 	ship.shp_mobil = 0;
     }
-    ship.shp_nplane = 0;
-    ship.shp_nland = 0;
-    ship.shp_nxlight = 0;
-    ship.shp_nchoppers = 0;
     ship.shp_fleet = 0;
     memset(ship.shp_item, 0, sizeof(ship.shp_item));
     ship.shp_pstage = PLG_HEALTHY;
@@ -484,10 +480,8 @@ build_land(struct sctstr *sp, struct lchrstr *lp, short *vec, int tlev)
     land.lnd_flags = 0;
     land.lnd_ship = -1;
     land.lnd_land = -1;
-    land.lnd_nland = 0;
     land.lnd_harden = 0;
     land.lnd_retreat = morale_base;
-    land.lnd_nxlight = 0;
     land.lnd_rflags = 0;
     memset(land.lnd_rpath, 0, sizeof(land.lnd_rpath));
     land.lnd_rad_max = 0;

src/lib/commands/land.c
@@ -53,9 +53,6 @@ land(void)
 	    continue;
 	if (!player->owner && !player->god)
 	    continue;
-	count_land_planes(&land);
-	lnd_count_units(&land);
-
 	if (nunits++ == 0) {
 	    if (player->god)
 		pr("own ");
@@ -78,8 +75,8 @@ land(void)
 	pr("%4d ", land.lnd_tech);
 	pr("%3d%%", land.lnd_retreat);
 	pr("%3d", land.lnd_rad_max);
-	pr("%3d", land.lnd_nxlight);
-	pr("%3d", land.lnd_nland);
+	pr("%3d", lnd_nxlight(&land));
+	pr("%3d", lnd_nland(&land));
 	if (land.lnd_ship >= 0)
 	    pr(" %4dS", land.lnd_ship);
 	else if (land.lnd_land >= 0)

src/lib/commands/ldump.c
@@ -299,9 +299,6 @@ ldump(void)
 	    continue;
 	if (!player->owner && !player->god)
 	    continue;
-	count_land_planes(&land);
-	lnd_count_units(&land);
-
 	nunits++;
 	if (player->god)
 	    pr("%d ", land.lnd_own);
@@ -349,10 +346,10 @@ ldump(void)
 		pr(" %d", land.lnd_rad_max);
 		break;
 	    case 14:
-		pr(" %d", land.lnd_nxlight);
+		pr(" %d", lnd_nxlight(&land));
 		break;
 	    case 15:
-		pr(" %d", land.lnd_nland);
+		pr(" %d", lnd_nland(&land));
 		break;
 	    case 16:
 		pr(" %d", land.lnd_land);

commit b60e5be22c8b5559f82ad6dcb90f3fd747c9bc49

Don't let planes fly to a carrier without sufficient space
We test whether the the carrier has space for each plane individually
instead of whether it has space for all of them.  The planes that fit
land, the others abort and get teleported home.  Abusable.

pln_oneway_to_carrier_ok() was created in commit 1127762c (v4.2.17) to
fix almost the same bug.  It worked fine then, because
fit_plane_on_ship() worked with load counters, and incremented them.

Broken in commit 3e370da5 (v4.3.17), which made fit_plane_on_ship()
count the loaded planes, to permit the removal of load counters.  But
unlike load counters, loaded planes don't change during
pln_oneway_to_carrier_ok().  Thus, each plane is checked individually.

Fix by tallying all the planes before checking for space.

src/lib/subs/plnsub.c
@@ -51,7 +51,8 @@
 #include "ship.h"
 #include "xy.h"
 
-static int fit_plane_on_ship(struct plnstr *, struct shpstr *);
+static int ship_can_carry(struct shpstr *, int, int, int, int);
+static int inc_shp_nplane(struct plnstr *, int *, int *, int *);
 
 /*
  * Get planes and escorts argument.
@@ -188,12 +189,14 @@ int
 pln_oneway_to_carrier_ok(struct emp_qelem *bomb_list,
 			 struct emp_qelem *esc_list, int cno)
 {
+    int n, nch, nxl, nmsl;
     struct emp_qelem *list, *qp;
     struct plist *plp;
     struct shpstr ship;
 
     if (cno < 0 || !getship(cno, &ship))
 	return 0;
+    n = shp_nplane(&ship, &nch, &nxl, &nmsl);
 
     /* for both lists */
     for (list = bomb_list;
@@ -203,11 +206,12 @@ pln_oneway_to_carrier_ok(struct emp_qelem *bomb_list,
 	    plp = (struct plist *)qp;
 	    if (plp->plane.pln_ship == ship.shp_uid)
 		continue;
-	    if (!fit_plane_on_ship(&plp->plane, &ship))
+	    n++;
+	    if (!inc_shp_nplane(&plp->plane, &nch, &nxl, &nmsl))
 		return 0;
 	}
     }
-    return 1;
+    return ship_can_carry(&ship, n, nch, nxl, nmsl);
 }
 
 void