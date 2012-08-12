gefla/empserver

commit 404a76f79e2af222849a1e4fd7fce71ac09b2065

Clean up path finding in getpath()
Don't claim the destination sector is unreachable when the best path
is longer than 99 characters or the complete path is longer than 1023
characters.  Instead, report that the path is too long when the total
path is longer than 1023 characters.

src/lib/subs/paths.c
@@ -24,10 +24,10 @@
  *
  *  ---
  *
- *  path.c: Routines associated with paths, directions, etc.
+ *  paths.c: Routines associated with paths, directions, etc.
  *
  *  Known contributors to this file:
- *     Markus Armbruster, 2005-2009
+ *     Markus Armbruster, 2005-2011
  */
 
 #include <config.h>
@@ -113,51 +113,20 @@ getpath(char *buf, char *arg, coord x, coord y, int onlyown, int showdes,
 more:
     while (*p) {
 	if (sarg_xy(p, &dx, &dy)) {
-	    bp = NULL;
-	    switch (mobtype) {
-	    default:
-		CANT_REACH();
-		pr("Destination sectors not allowed here!\n");
-		break;
-	    case MOB_FLY:
-		if (path_find(x, y, dx, dy, 0, MOB_FLY) < 0)
-		    bp = NULL;
-		else {
-		    len = path_find_route(buf2, 100, x, y, dx, dy);
-		    if (len >= 100)
-			bp = NULL;
-		    else {
-			if (len == 0)
-			    strcpy(buf2, "h");
-			bp = buf2;
-		    }
-		}
-		break;
-	    case MOB_SAIL:
-		if (path_find(x, y, dx, dy, player->cnum, MOB_SAIL) < 0)
-		    bp = NULL;
-		else {
-		    len = path_find_route(buf2, 100, x, y, dx, dy);
-		    if (len >= 100)
-			bp = NULL;
-		    else {
-			if (len == 0)
-			    strcpy(buf2, "h");
-			bp = buf2;
-		    }
-		}
-		break;
-	    }
-	    if (bp && p + strlen(bp) + 1 < buf + MAX_PATH_LEN) {
-		strcpy(p, bp);
-		pr("Using best path  '%s'\n", p);
-		pr("Using total path '%s'\n", buf);
-		return buf;
-	    } else {
+	    if (path_find(x, y, dx, dy, player->cnum, mobtype) < 0) {
 		pr("Can't get to %s from here!\n",
 		   xyas(dx, dy, player->cnum));
+		break;
 	    }
-	    break;
+	    len = path_find_route(p, buf + MAX_PATH_LEN - p, x, y, dx, dy);
+	    if (p + len >= buf + MAX_PATH_LEN) {
+		pr("Can't handle path to %s, it's too long, sorry.\n",
+		   xyas(dx, dy, player->cnum));
+		break;
+	    }
+	    pr("Using best path  '%s'\n", p);
+	    pr("Using total path '%s'\n", buf);
+	    return buf;
 	}
 	dir = chkdir(*p, DIR_STOP, DIR_LAST);
 	if (dir < 0) {

commit 0f1e14f0f33886f92cbfcb6c1a8b7d21a22393d9

Fix flying commands for destination equal to assembly point
bomb, drop, fly, paradrop, recon and sweep fail when given a
destination sector equal to the assembly point.  Broken in commit
404a76f7, v4.3.27.  Reported by Tom Johnson.

Before that commit, getpath() returned NULL on error, "" when input is
an empty path, "h" when it's coordinates of the assembly point, and a
non-empty path otherwise.

The commit accidentally changed it to return "" instead of "h".

Instead of changing it back, make it return NULL when input is an
empty path, and change bomb() & friends to accept empty flight paths.

This also affects sail: it now fails when you give it an empty path,
just like bomb & friends.  Path "h" still works.

src/lib/commands/bomb.c
@@ -108,8 +108,7 @@ bomb(void)
 	return RET_SYN;
     ax = ap_sect.sct_x;
     ay = ap_sect.sct_y;
-    if (!getpath(flightpath, player->argp[5], ax, ay, 0, 0, MOB_FLY)
-	|| *flightpath == 0)
+    if (!getpath(flightpath, player->argp[5], ax, ay, 0, 0, MOB_FLY))
 	return RET_SYN;
     tx = ax;
     ty = ay;

src/lib/commands/drop.c
@@ -61,8 +61,7 @@ drop(void)
 	return RET_SYN;
     ax = ap_sect.sct_x;
     ay = ap_sect.sct_y;
-    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY)
-	|| *flightpath == 0)
+    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY))
 	return RET_SYN;
     tx = ax;
     ty = ay;

src/lib/commands/fly.c
@@ -65,8 +65,7 @@ fly(void)
 	return RET_SYN;
     ax = ap_sect.sct_x;
     ay = ap_sect.sct_y;
-    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY)
-	|| *flightpath == 0)
+    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY))
 	return RET_SYN;
     tx = ax;
     ty = ay;

src/lib/commands/para.c
@@ -64,8 +64,7 @@ para(void)
 	return RET_SYN;
     ax = ap_sect.sct_x;
     ay = ap_sect.sct_y;
-    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY)
-	|| *flightpath == 0)
+    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY))
 	return RET_SYN;
     tx = ax;
     ty = ay;

src/lib/commands/reco.c
@@ -63,8 +63,7 @@ reco(void)
 	return RET_SYN;
     ax = ap_sect.sct_x;
     ay = ap_sect.sct_y;
-    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY)
-	|| *flightpath == 0)
+    if (!getpath(flightpath, player->argp[4], ax, ay, 0, 0, MOB_FLY))
 	return RET_SYN;
     tx = ax;
     ty = ay;

src/lib/subs/paths.c
@@ -143,7 +143,7 @@ more:
 	    break;
 	}
 	if (dir == DIR_STOP) {
-	    p[1] = 0;
+	    *p = 0;
 	    return buf;
 	}
 	++p;
@@ -170,7 +170,7 @@ more:
     strcpy(p, bp);
     if (*bp)
 	goto more;
-    return buf;
+    return buf[0] ? buf : NULL;
 }
 
 /*