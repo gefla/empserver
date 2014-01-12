cdfcc1a1654cc871c987841ef7586cb1c791389a
Markus Armbruster <armbru@pond.sub.org>
2014-01-12 15:57:22 +0100

    tests/build: New; exercises build command
    drnuke_const is not covered, yet.
    
    Random rounding causes some complications: sometimes build succeeds
    without materials.  Troublesome spots are clearly marked in the test
    input.
    
    Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

--- Make.mk
+++ Make.mk
@@ -261,10 +261,12 @@ _check: all
 ifeq ($(empthread),LWP)
 	$(srcdir)/tests/smoke-test $(srcdir)
 	$(srcdir)/tests/actofgod-test $(srcdir)
+	$(srcdir)/tests/build-test $(srcdir)
 	$(srcdir)/tests/fire-test $(srcdir)
 else
 	@echo "$(srcdir)/tests/smoke-test SKIPPED"
 	@echo "$(srcdir)/tests/actofgod-test SKIPPED"
+	@echo "$(srcdir)/tests/build-test SKIPPED"
 	@echo "$(srcdir)/tests/fire-test SKIPPED"
 endif
 
