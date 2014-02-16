gefla/empserver

commit 40eb78eb74474597b79d05c4a5934ff02fb9e127

Fix confused and buggy bridge splashing code
A bridge (span or tower) must be splashed when it gets damaged below
SCT_MINEFF.  Likewise when its last supporting sector (bridge head or
tower) gets damaged below SCT_MINEFF, unless EASY_BRIDGES is enabled.
We need to check this whenever a bridge head, span or tower gets
damaged.  This is done in three places, and all of them screw up:

* checksect() ignores damage to bridge heads.  It also leaves writing
  back the sector it checks to the caller, which never happens when
  it's called from sct_postread().

  Note that checksect() drowns all planes on bridges it splashes.
  Functions that need to exempt flying planes from such a fate have to
  splash bridges themselves.

* sect_damage() ignores damage to bridge towers, and damage to bridge
  spans unless EASY_BRIDGES is enabled.  It then runs checksect(),
  which compensates for these omissions, but happily drowns the planes
  sect_damage() attempts to protect.

* eff_bomb() ignores damage to bridge heads.  Collateral damage makes
  sect_damage() run, which compensates for the omission.

This causes the following bugs:

* Efficiency damage going through sect_damage() can drown planes it
  shouldn't.  This affects pinpoint bombing when collateral damage
  splashes a bridge, and strategic bombing.  The drowned planes then
  crash and burn when they attempt to land at their (just splashed)
  base.

* Efficiency damage to bridge heads not going through sect_damage()
  fails to collapse unsupported bridges.  This affects pin-bombing
  efficiency without collateral damage, and ground combat.  Also deity
  commands edit, setsector and add, but that could be regarded as a
  feature.

* If the sector file somehow ends up with an inefficient bridge span,
  it collapses on every read again and again, until it collapses on a
  write.  Related problems exist with other actions of checksect(),
  and they're not addressed here.

src/lib/subs/bridgefall.c
@@ -48,6 +48,31 @@
 #include "sect.h"
 #include "xy.h"
 
+static void knockdown(struct sctstr *, struct emp_qelem *);
+
+/*
+ * Check bridges at and around SP after damage to SP.
+ * If SP is an inefficent bridge, splash it.
+ * If SP can't support a bridge, splash unsupported adjacent bridges.
+ * Don't drown planes in LIST when splashing bridges.
+ * Write back splashed bridges, except for SP; writing that one is
+ * left to the caller.
+ */
+void
+bridge_damaged(struct sctstr *sp, struct emp_qelem *list)
+{
+    int des;
+
+    if (sp->sct_effic >= SCT_MINEFF)
+	return;
+
+    des = sp->sct_type;
+    if (des == SCT_BSPAN || des == SCT_BTOWER)
+	knockdown(sp, list);
+    if ((des == SCT_BHEAD || des == SCT_BTOWER) && !opt_EASY_BRIDGES)
+	bridgefall(sp, list);
+}
+
 void
 bridgefall(struct sctstr *sp, struct emp_qelem *list)
 {
@@ -94,7 +119,7 @@ bridgefall(struct sctstr *sp, struct emp_qelem *list)
 
 /* Knock down a bridge span.  Note that this does NOT write the
  * sector out to the database, it's up to the caller to do that. */
-void
+static void
 knockdown(struct sctstr *sp, struct emp_qelem *list)
 {
     struct lndstr land;

include/prototypes.h
@@ -402,8 +402,8 @@ extern void stop_service(void);
 extern int confirm(char *);
 extern int askyn(char *);
 /* bridgefall.c */
+extern void bridge_damaged(struct sctstr *, struct emp_qelem *);
 extern void bridgefall(struct sctstr *, struct emp_qelem *);
-extern void knockdown(struct sctstr *, struct emp_qelem *);
 /* bsanct.c */
 extern void bsanct(void);
 /* caploss.c */

src/lib/commands/bomb.c
@@ -369,14 +369,7 @@ eff_bomb(struct emp_qelem *list, struct sctstr *target)
 	   "%s bombing raid did %d%% damage in %s\n",
 	   cname(player->cnum), oldeff - target->sct_effic,
 	   xyas(target->sct_x, target->sct_y, target->sct_own));
-    if (target->sct_effic < SCT_MINEFF) {
-	if (target->sct_type == SCT_BSPAN)
-	    knockdown(target, list);
-	else if (target->sct_type == SCT_BTOWER) {
-	    knockdown(target, list);
-	    bridgefall(target, list);
-	}
-    }
+    bridge_damaged(target, list);
     putsect(&sect);
     collateral_damage(target->sct_x, target->sct_y, dam, list);
 }

src/lib/subs/sect.c
@@ -70,6 +70,7 @@ sct_prewrite(int id, void *ptr)
 
     time(&sp->sct_timestamp);
 
+    bridge_damaged(sp, NULL);
     checksect(sp);
     getsect(sp->sct_x, sp->sct_y, &sect);
     return 1;
@@ -106,14 +107,6 @@ checksect(struct sctstr *sp)
     else
 	loyalcivs = 0;
 
-    if (sp->sct_effic < SCT_MINEFF) {
-	if (sp->sct_type == SCT_BSPAN)
-	    knockdown(sp, 0);
-	else if (sp->sct_type == SCT_BTOWER) {
-	    knockdown(sp, 0);
-	    bridgefall(sp, 0);
-	}
-    }
     if (sp->sct_own != 0 && !civs) {
 	sp->sct_work = 100;
 	sp->sct_oldown = sp->sct_own;

src/lib/subs/sectdamage.c
@@ -69,13 +69,7 @@ sect_damage(struct sctstr *sp, int dam, struct emp_qelem *list)
     if (sp->sct_mobil > 0)
 	sp->sct_mobil = damage(sp->sct_mobil, dam);
     item_damage(dam, sp->sct_item);
-    if (opt_EASY_BRIDGES == 0) {
-	if (sp->sct_effic < SCT_MINEFF && sp->sct_type == SCT_BHEAD)
-	    bridgefall(sp, list);
-    } else {
-	if (sp->sct_effic < SCT_MINEFF && sp->sct_type == SCT_BSPAN)
-	    knockdown(sp, list);
-    }
+    bridge_damaged(sp, list);
     putsect(sp);
     return eff;
 }

commit 292bfac7974f118d0c82e5194c3488344a37eb9f

bridgefall: Fix support loss with EASY_BRIDGES and BRIDGETOWERS on
With EASY_BRIDGES on, bridge spans need to be next to land or a bridge
tower to remain standing.

Land can't go away, but a bridge tower can fall.  Bridge spans next to
it may lose support then.  Bug: they don't fall when they lose support
that way.  Fix that.

Broken in commit 40eb78e, v4.3.12.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/subs/bridgefall.c
@@ -66,7 +66,7 @@ bridge_damaged(struct sctstr *sp)
     des = sp->sct_type;
     if (des == SCT_BSPAN || des == SCT_BTOWER)
 	knockdown(sp);
-    if ((des == SCT_BHEAD || des == SCT_BTOWER) && !opt_EASY_BRIDGES)
+    if ((des == SCT_BHEAD && !opt_EASY_BRIDGES) || des == SCT_BTOWER)
 	bridgefall(sp);
 }
 

tests/bridgefall/easy-on/01-1
@@ -9,5 +9,4 @@ fire se 2,0 0,-2
 | tower stands, and supports span
 fire se 2,0 6,0
 | tower falls, span 3,-3 falls
-| BUG: doesn't fall
 fire se 2,0 1,-3

tests/bridgefall/final.xdump
@@ -71,7 +71,7 @@ owner xloc yloc des effic mobil off loyal terr0 terr1 terr2 terr3 dterr xdist yd
 0 -4 -4 0 0 0 0 0 0 0 0 0 0 -4 -4 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0
 0 -2 -4 0 0 0 0 0 0 0 0 0 0 -2 -4 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0
 0 1 -3 0 0 0 0 0 0 0 0 0 0 1 -3 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0
-2 3 -3 28 100 0 0 0 0 0 0 0 0 3 -3 0 0 0 1 28 0 0 0 0 0 2 100 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0
+0 3 -3 0 0 0 0 0 0 0 0 0 0 3 -3 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0
 0 5 -3 0 0 0 0 0 0 0 0 0 0 5 -3 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0
 0 7 -3 0 0 0 0 0 0 0 0 0 0 7 -3 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0
 0 9 -3 0 0 0 0 0 0 0 0 0 0 9 -3 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 0 0 0 0 0 0

tests/bridgefall/journal.log
@@ -80,7 +80,7 @@
     Play#0 output Play#0 1      09876543210123456789
     Play#0 output Play#0 1   -5  . . . . . . + . . . -5
     Play#0 output Play#0 1   -4 . . . . . . = . . .  -4
-    Play#0 output Play#0 1   -3  . . . . . . = . . . -3
+    Play#0 output Play#0 1   -3  . . . . . . . . . . -3
     Play#0 output Play#0 1   -2 . . . . . . = . . .  -2
     Play#0 output Play#0 1   -1  . . . . . # = . . . -1
     Play#0 output Play#0 1    0 . . . . . c f . @ =  0
@@ -194,7 +194,7 @@
     Play#0 output Play#0 1      09876543210123456789
     Play#0 output Play#0 1   -5  . . . . . . + . . . -5
     Play#0 output Play#0 1   -4 . . . . . . = . . .  -4
-    Play#0 output Play#0 1   -3  . . . . . . = . . . -3
+    Play#0 output Play#0 1   -3  . . . . . . . . . . -3
     Play#0 output Play#0 1   -2 . . . . . . = . . .  -2
     Play#0 output Play#0 1   -1  . . . . . # = . . . -1
     Play#0 output Play#0 1    0 . . . . . c f . @ =  0
@@ -212,7 +212,7 @@
     Play#0 output Play#0 1 own   sect        eff prd mob uf uf old  civ  mil   uw food work avail fall coa
     Play#0 output Play#0 1   2    2,-4   =  100%       0 .. ..      100    0    0    0   0%    0    0   1
     Play#0 output Play#0 1   0    1,-3   .    0%       0 .. ..        0    0    0    0   0%    0    0   1
-    Play#0 output Play#0 1   2    3,-3   =  100%       0 .. ..      100    0    0    0   0%    0    0   1
+    Play#0 output Play#0 1   0    3,-3   .    0%       0 .. ..        0    0    0    0   0%    0    0   1
     Play#0 output Play#0 1   0    0,-2   .    0%       0 .. ..        0    0    0    0   0%    0    0   1
     Play#0 output Play#0 1   2    2,-2   =  100%       0 .. ..      100    0    0    0   0%    0    0   1
     Play#0 output Play#0 1   0   -1,-1   .    0%       0 .. ..        0    0    0    0   0%    0    0   1
@@ -249,6 +249,7 @@
     Play#0 output Play#0 1 Country #1 shelled sector 6,0 for 46 damage.
     Play#0 output Play#0 1 Country #1 shelled sector 1,-3 for 23 damage.
     Play#0 output Play#0 1 Crumble... SCREEEECH!  Splash! Bridge tower falls at 1,-3!
+    Play#0 output Play#0 1 Crumble... SCREEEECH!  Splash! Bridge falls at 3,-3!
     Play#0 output Play#0 1
     Play#0 output Play#0 1 > BULLETIN from POGO, (#0)  dated Thu Jan  1 00:00:00 1970
     Play#0 output Play#0 1 Country #1 shelled sector 8,2 for 28 damage.