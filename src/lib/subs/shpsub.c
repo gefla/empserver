gefla/empserver

commit 74e4e2810a4ffb4a3b397665697390adbaffd674

(fuel, load, shp_check_nav, retreat_ship1, shp_nav_one_sector) (shp_check_nav, sail_nav_fleet, bigcity_dchr[], sector_navigation[]) (d_navigation): Add a NEW d_navigation enum NAV_CANAL to indicate that a sector has canal capability. Add canal determination logic to shp_check_nav(). Update sector_navigation[] with new canal navigation enum.  Use shp_check_nav() to determine the sector can be navigated.

src/lib/subs/shpsub.c
@@ -156,7 +156,7 @@ shp_nav(struct emp_qelem *list, double *minmobp, double *maxmobp,
 	    shp_mess("was sucked into the sky by a strange looking spaceship", mlp);	/* heh -KHS */
 	    continue;
 	}
-	switch (shp_check_nav(&sect)) {
+	switch (shp_check_nav(&sect, &ship)) {
 	case CN_CONSTRUCTION:
 	    shp_mess("is caught in a construction zone", mlp);
 	    continue;
@@ -368,12 +368,18 @@ shp_mess(s_char *str, struct mlist *mlp)
 }
 
 int
-shp_check_nav(struct sctstr *sect)
+shp_check_nav(struct sctstr *sect, struct shpstr *shp)
 {
     switch (dchr[sect->sct_type].d_nav) {
     case NAVOK:
 	break;
-
+    case NAV_CANAL:
+	if (mchr[(int)shp->shp_type].m_flags & M_CANAL == M_CANAL) {
+	    if (sect->sct_effic < 2)
+		return CN_CONSTRUCTION;
+	} else
+	    return CN_LANDLOCKED;
+	break;
     case NAV_02:
 	if (sect->sct_effic < 2)
 	    return CN_CONSTRUCTION;
@@ -821,6 +827,7 @@ shp_nav_one_sector(struct emp_qelem *list, int dir, natid actor,
     double tech;		/* for mapping */
     double tf;			/* for mapping */
     s_char dp[80];
+    int navigate;
 
     if (dir <= DIR_STOP || dir >= DIR_VIEW) {
 	shp_put(list, actor);
@@ -834,27 +841,27 @@ shp_nav_one_sector(struct emp_qelem *list, int dir, natid actor,
 	newx = xnorm(mlp->ship.shp_x + dx);
 	newy = ynorm(mlp->ship.shp_y + dy);
 	getsect(newx, newy, &sect);
-	if (shp_check_nav(&sect) != CN_NAVIGABLE ||
+	navigate = shp_check_nav(&sect, &mlp->ship);
+	if (navigate != CN_NAVIGABLE ||
 	    (sect.sct_own && actor != sect.sct_own &&
 	     getrel(getnatp(sect.sct_own), actor) < FRIENDLY)) {
-	    if (together) {
-		mpr(actor, "can't go to %s\n", xyas(newx, newy, actor));
-		return 2;
-	    } else {
-		sprintf(dp, "can't go to %s", xyas(newx, newy, actor));
-		shp_mess(dp, mlp);
-		continue;
-	    }
-	}
-	if (IS_BIG_CITY(sect.sct_type)) {
-	    if (!(mlp->mcp->m_flags & M_CANAL)) {
+	    if (dchr[sect.sct_type].d_nav == NAV_CANAL &&
+		!(mlp->mcp->m_flags & M_CANAL) &&
+		navigate == CN_LANDLOCKED)
 		sprintf(dp,
 			"is too large to fit into the canal system at %s",
 			xyas(newx, newy, actor));
+	    else
+		sprintf(dp, "can't go to %s", xyas(newx, newy, actor));
+	    if (together) {
+		mpr(actor, "%s\n", dp);
+		return 2;
+	    } else {
 		shp_mess(dp, mlp);
 		continue;
 	    }
 	}
+
 	if (mlp->mobil <= 0.0) {
 	    shp_mess("is out of mobility", mlp);
 	    continue;

include/prototypes.h
@@ -684,7 +684,7 @@ extern int shp_sweep(struct emp_qelem *, int, natid);
 extern s_char *shp_path(int, struct shpstr *, s_char *);
 extern void shp_put(struct emp_qelem *, natid);
 extern void shp_list(struct emp_qelem *);
-extern int shp_check_nav(struct sctstr *);
+extern int shp_check_nav(struct sctstr *, struct shpstr *);
 extern int sect_has_dock(struct sctstr *);
 extern int shp_hardtarget(struct shpstr *);
 extern void shp_view(struct emp_qelem *);

include/sect.h
@@ -93,6 +93,7 @@ typedef enum {
     NAV_NONE,	/* ships can't navigate */
     NAVOK,	/* ships can always navigate */
     NAV_02,	/* requires 2% effic to navigate */
+    NAV_CANAL,	/* requires 2% effic to navigate and M_CANAL capability */
     NAV_60	/* requires 60% effic to navigate */
 } d_navigation;
 

src/lib/commands/fuel.c
@@ -121,13 +121,9 @@ fuel(void)
 	    if (!item.ship.shp_own)
 		continue;
 
-	    if ((sect.sct_type != SCT_HARBR)
-		&& (sect.sct_type != SCT_WATER)
-		&& (sect.sct_type != SCT_BSPAN)
-		&& (!IS_BIG_CITY(sect.sct_type))) {
-		pr("Sector %s is not a harbor, bridge span, or sea.\n",
-		   xyas(item.ship.shp_x, item.ship.shp_y,
-			item.ship.shp_own));
+	    if (shp_check_nav(&sect, &item.ship) == CN_LANDLOCKED) {
+		pr("%s is landlocked and cannot be fueled.\n",
+		   prship(&item.ship));
 		continue;
 	    }
 

src/lib/commands/load.c
@@ -153,8 +153,8 @@ load(void)
 	    if (noisy)
 		pr("Sector %s is not a harbor%s%s.\n",
 		   xyas(ship.shp_x, ship.shp_y, player->cnum),
-		   dchr[SCT_CAPIT].d_nav == NAV_02 ? " or a " : "",
-		   dchr[SCT_CAPIT].d_nav == NAV_02 ?
+		   dchr[SCT_CAPIT].d_nav == NAV_CANAL ? " or a " : "",
+		   dchr[SCT_CAPIT].d_nav == NAV_CANAL ?
 		    dchr[SCT_CAPIT].d_name : "");
 	    continue;
 	}

src/lib/global/nsc.c
@@ -787,6 +787,7 @@ struct symbol sector_navigation[] = { /* for d_nav */
     {NAV_NONE, "land"},
     {NAVOK, "sea"},
     {NAV_02, "harbor"},
+    {NAV_CANAL, "canal"},
     {NAV_60, "bridge"},
     {0, NULL}
 };

src/lib/global/sect.c
@@ -44,7 +44,7 @@
 #include "product.h"
 
 struct dchrstr bigcity_dchr = {
-    SCT_CAPIT,   'c',       0, 2,  NAV_02,   UPKG, 1.0, 2.0,   30, 0, 10, 1, 2, 999, "city"
+    SCT_CAPIT,   'c',       0, 2, NAV_CANAL, UPKG, 1.0, 2.0,   30, 0, 10, 1, 2, 999, "city"
 };
 
 struct dchrstr dchr[] = {

src/lib/subs/retreat.c
@@ -188,7 +188,7 @@ retreat_ship1(struct shpstr *sp, s_char code, int orig)
     }
 
     getsect(sp->shp_x, sp->shp_y, &sect);
-    switch (shp_check_nav(&sect)) {
+    switch (shp_check_nav(&sect, sp)) {
     case CN_CONSTRUCTION:
 	wu(0, sp->shp_own,
 	   "%s %s,\nbut was caught in a construction zone, and couldn't retreat!\n",
@@ -262,7 +262,7 @@ retreat_ship1(struct shpstr *sp, s_char code, int orig)
 	mobcost = 480.0 / (mobcost + techfact(sp->shp_tech, mobcost));
 
 	getsect(newx, newy, &sect);
-	if (shp_check_nav(&sect) != CN_NAVIGABLE ||
+	if (shp_check_nav(&sect, sp) != CN_NAVIGABLE ||
 	    (sect.sct_own && !player->owner &&
 	     getrel(getnatp(sect.sct_own), sp->shp_own) < FRIENDLY)) {
 	    wu(0, sp->shp_own, "%s %s,\nbut could not retreat to %s!\n",

commit d89825116ef130b971c17d674ee963c3a1e1a03a

navigate: Don't scatter ships on canal entry
When attempting to enter a sector with a ship that can't go there
while the navigating ships are all in the same sector, navigate stops
and prompts without removing the incapable ship from the group.  If
another ship has already entered the sector, the group becomes
scattered.

This can happen only when navigating a mixed group of ships with and
without canal capability into a canal.  Broken in commit 74e4e281,
v4.3.0.

Remove the incapable ship from the group when another ship can enter
the sector.  This avoids scattering ships.

Don't remove incapable ships when no ship can enter the sector.
Without this, navigate would remove everyone and end then.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/subs/shpsub.c
@@ -770,6 +770,7 @@ shp_nav_one_sector(struct emp_qelem *list, int dir, natid actor,
     coord dy;
     coord newx;
     coord newy;
+    int move;
     int stopping = 0;
     double mobcost;
     char dp[80];
@@ -781,6 +782,21 @@ shp_nav_one_sector(struct emp_qelem *list, int dir, natid actor,
     }
     dx = diroff[dir][0];
     dy = diroff[dir][1];
+
+    move = 0;
+    for (qp = list->q_back; qp != list; qp = next) {
+	next = qp->q_back;
+	mlp = (struct ulist *)qp;
+	newx = xnorm(mlp->unit.ship.shp_x + dx);
+	newy = ynorm(mlp->unit.ship.shp_y + dy);
+	getsect(newx, newy, &sect);
+	navigate = shp_check_nav(&mlp->unit.ship, &sect);
+	if (navigate == NAVOK &&
+	    (!sect.sct_own
+	     || relations_with(sect.sct_own, actor) >= FRIENDLY))
+	    move = 1;
+    }
+
     for (qp = list->q_back; qp != list; qp = next) {
 	next = qp->q_back;
 	mlp = (struct ulist *)qp;
@@ -797,7 +813,7 @@ shp_nav_one_sector(struct emp_qelem *list, int dir, natid actor,
 			xyas(newx, newy, actor));
 	    else
 		sprintf(dp, "can't go to %s", xyas(newx, newy, actor));
-	    if (together) {
+	    if (together && !move) {
 		mpr(actor, "%s\n", dp);
 		return 1;
 	    } else {

tests/navi-march/01-navigate-1
@@ -44,7 +44,6 @@ navi 54/55 n
 iyyh
 navi 57/56 n
 iyyh
-| BUG: 54/55 moves pt n, pt/cs yy, 57/56 moves nothing n, cs/pt yy
 navi 58/59 nX
 h
 | enter canal 4,2 1%

tests/navi-march/final.xdump
@@ -157,9 +157,9 @@ uid owner xloc yloc type effic mobil off tech opx opy mission radius fleet civil
 52 1 2 0 9 100 117 0 40 4 0 none 0 "" 0 2 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
 53 1 4 0 9 100 127 0 40 4 0 none 0 "" 0 2 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
 54 1 0 0 9 100 99 0 40 1 1 none 0 "" 0 2 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
-55 1 -1 -1 2 100 97 0 20 0 0 none 0 "" 0 5 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
-56 1 -1 -1 9 100 108 0 40 1 1 none 0 "" 0 2 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
-57 1 -1 -1 2 100 97 0 20 0 0 none 0 "" 0 5 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
+55 1 1 1 2 100 127 0 20 0 0 none 0 "" 0 5 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
+56 1 0 0 9 100 99 0 40 1 1 none 0 "" 0 2 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
+57 1 1 1 2 100 127 0 20 0 0 none 0 "" 0 5 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
 58 1 1 1 2 100 127 0 20 0 0 none 0 "" 0 5 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
 59 1 1 1 2 100 127 0 20 0 0 none 0 "" 0 5 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""
 60 1 5 1 9 100 127 0 40 5 1 none 0 "" 0 2 0 0 0 0 0 0 0 0 0 0 0 0 healthy 0 0 "" 1 1 1 () ""

tests/navi-march/journal.log
@@ -200,32 +200,29 @@
     Play#1 input navi 54/55 n
     Play#1 command navigate
     Play#1 output Play#1 1 Flagship is pt   patrol boat (#54)
-    Play#1 output Play#1 1 is too large to fit into the canal system at 2,2
+    Play#1 output Play#1 1 cs   cargo ship (#55) is too large to fit into the canal system at 2,2 & stays in 1,1
     Play#1 output Play#1 1  h h
     Play#1 output Play#1 1 c c c
     Play#1 output Play#1 1  + +
-    Play#1 output Play#1 4 <127.0:117.8: 2,2>
+    Play#1 output Play#1 4 <117.8:117.8: 2,2>
     Play#1 input iyyh
     Play#1 output Play#1 1 shp#     ship type       x,y   fl  eff mil  sh gun pn he xl ln mob tech
     Play#1 output Play#1 1   54 pt   patrol boat    2,2      100%   2   0   0  0  0  0  0 117  40
-    Play#1 output Play#1 1   55 cs   cargo ship     1,1      100%   5   0   0  0  0  0  0 127  20
     Play#1 output Play#1 1 pt   patrol boat (#54) stopped at 0,0
-    Play#1 output Play#1 1 cs   cargo ship (#55) stopped at -1,-1
     Play#1 output Play#1 6 0 623
     Play#1 input navi 57/56 n
     Play#1 command navigate
     Play#1 output Play#1 1 Flagship is cs   cargo ship (#57)
-    Play#1 output Play#1 1 is too large to fit into the canal system at 2,2
-    Play#1 output Play#1 1  . =
-    Play#1 output Play#1 1 h h h
-    Play#1 output Play#1 1  c c
-    Play#1 output Play#1 4 <127.0:127.0: 1,1>
+    Play#1 output Play#1 1 cs   cargo ship (#57) is too large to fit into the canal system at 2,2 & stays in 1,1
+    Play#1 output Play#1 1 Changing flagship to pt   patrol boat (#56)
+    Play#1 output Play#1 1  h h
+    Play#1 output Play#1 1 c c c
+    Play#1 output Play#1 1  + +
+    Play#1 output Play#1 4 <117.8:117.8: 2,2>
     Play#1 input iyyh
     Play#1 output Play#1 1 shp#     ship type       x,y   fl  eff mil  sh gun pn he xl ln mob tech
-    Play#1 output Play#1 1   57 cs   cargo ship     1,1      100%   5   0   0  0  0  0  0 127  20
-    Play#1 output Play#1 1   56 pt   patrol boat    1,1      100%   2   0   0  0  0  0  0 127  40
-    Play#1 output Play#1 1 cs   cargo ship (#57) stopped at -1,-1
-    Play#1 output Play#1 1 pt   patrol boat (#56) stopped at -1,-1
+    Play#1 output Play#1 1   56 pt   patrol boat    2,2      100%   2   0   0  0  0  0  0 117  40
+    Play#1 output Play#1 1 pt   patrol boat (#56) stopped at 0,0
     Play#1 output Play#1 6 0 622
     Play#1 input navi 58/59 nX
     Play#1 command navigate
@@ -1409,9 +1406,9 @@
     Play#0 output Play#0 1   1   52 pt   patrol boat    2,0       100%   0   2   0   0  0  0  0  0 117   40
     Play#0 output Play#0 1   1   53 pt   patrol boat    4,0       100%   0   2   0   0  0  0  0  0 127   40
     Play#0 output Play#0 1   1   54 pt   patrol boat    0,0       100%   0   2   0   0  0  0  0  0  99   40
-    Play#0 output Play#0 1   1   55 cs   cargo ship    -1,-1      100%   0   5   0   0  0  0  0  0  97   20
-    Play#0 output Play#0 1   1   56 pt   patrol boat   -1,-1      100%   0   2   0   0  0  0  0  0 108   40
-    Play#0 output Play#0 1   1   57 cs   cargo ship    -1,-1      100%   0   5   0   0  0  0  0  0  97   20
+    Play#0 output Play#0 1   1   55 cs   cargo ship     1,1       100%   0   5   0   0  0  0  0  0 127   20
+    Play#0 output Play#0 1   1   56 pt   patrol boat    0,0       100%   0   2   0   0  0  0  0  0  99   40
+    Play#0 output Play#0 1   1   57 cs   cargo ship     1,1       100%   0   5   0   0  0  0  0  0 127   20
     Play#0 output Play#0 1   1   58 cs   cargo ship     1,1       100%   0   5   0   0  0  0  0  0 127   20
     Play#0 output Play#0 1   1   59 cs   cargo ship     1,1       100%   0   5   0   0  0  0  0  0 127   20
     Play#0 output Play#0 1   1   60 pt   patrol boat    5,1       100%   0   2   0   0  0  0  0  0 127   40