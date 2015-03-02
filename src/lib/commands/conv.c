gefla/empserver

commit 82c916654cf7b344d24349696a7eb3bb5ecc1b53

Don't permit convert to spend more money than available
Complicated by the fact that conv() ran the conversion code twice,
first for adding up the cost for chkmoney(), then for actually
converting.  chkmoney() asks the player to confirm when he's about to
spend more than half his cash.  Get rid of that, not worth the
complexity.  This merges do_conv() back into conv().

src/lib/commands/conv.c
@@ -40,41 +40,25 @@
 #include "commands.h"
 #include "land.h"
 
-static long do_conv(struct nstr_sect nstr, int uwtoconvert, int for_real);
-
 int
 conv(void)
 {
     struct natstr *natp;
-    long cash;
-    long cost;
+    struct sctstr sect;
     struct nstr_sect nstr;
-    int uwtoconvert;
+    int uwtoconvert, newuw, totaluw, uw;
+    int maxpop, civ, mil, adj_mob, mob;
+    double security_extra = 1.0;
+    struct lndstr land;
+    struct nstr_item ni;
+
 
     natp = getnatp(player->cnum);
-    cash = natp->nat_money;
     if (!snxtsct(&nstr, player->argp[1]))
 	return RET_SYN;
     uwtoconvert = onearg(player->argp[2], "Number to convert: ");
     if (uwtoconvert < 0)
 	return RET_SYN;
-    cost = do_conv(nstr, uwtoconvert, 0);
-    if (chkmoney(cost, cash, player->argp[3]))
-	return RET_SYN;
-    return (int)do_conv(nstr, uwtoconvert, 1);
-}
-
-static long
-do_conv(struct nstr_sect nstr, int uwtoconvert, int for_real)
-{
-    struct natstr *natp;
-    struct sctstr sect;
-    int newuw, totaluw, uw;
-    int maxpop, civ, mil, adj_mob, mob;
-    double security_extra = 1.0;
-    struct lndstr land;
-    struct nstr_item ni;
-    long cost = 0;
 
     totaluw = 0;
     while (nxtsct(&nstr, &sect)) {
@@ -106,8 +90,6 @@ do_conv(struct nstr_sect nstr, int uwtoconvert, int for_real)
 		 */
 		security_extra += .1;
 		land.lnd_mobil -= 10;
-		if (for_real)
-		    putland(land.lnd_uid, &land);
 		mil += land.lnd_item[I_MILIT];
 	    }
 	}
@@ -138,9 +120,10 @@ do_conv(struct nstr_sect nstr, int uwtoconvert, int for_real)
 	    newuw = adj_mob;
 	if (newuw <= 0)
 	    continue;
-	if (!for_real) {
-	    cost += newuw * 1.5;
-	    continue;
+	if (player->dolcost + newuw * 1.5 > natp->nat_money) {
+	    pr("You can't afford to convert %d civilians in %s!\n",
+	       newuw, xyas(sect.sct_x, sect.sct_y, player->cnum));
+	    break;
 	}
 	player->btused += (newuw - 1) / 100 + 1;
 	player->dolcost += newuw * 1.5;
@@ -163,8 +146,6 @@ do_conv(struct nstr_sect nstr, int uwtoconvert, int for_real)
 	putsect(&sect);
 	totaluw += newuw;
     }
-    if (!for_real)
-	return cost;
     pr("Total civilians converted: %d\n", totaluw);
     pr("Paperwork at conversion places ... %d\n", player->btused);
     return RET_OK;

commit 39884aff595c44a03df2e1d3c7df8cd8ec731f80

convert: Drop broken code to charge security unit mobility
Conversion is easier when land units with capability security are
present.  Each such land unit is charged 10 mobility.  The mobility
charge is undocumented.

Land unit mobility is charged even when conversion turns out to be
impossible, say because the sector has no mobility.  I call this a
bug.  Has been that way since security land units were added in
Chainsaw 3.

Except the mobility charge doesn't actually work anymore: the changed
land unit is never written back.  Broken in commit 82c9166, v4.3.16.
Fix this bug would be trivial, but would bring back the bug described
above, and fixing that one is harder, and doesn't feel worthwhile.

Remove the broken charging of land unit mobility instead.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/commands/conv.c
@@ -28,6 +28,7 @@
  *
  *  Known contributors to this file:
  *     Dave Pare, 1986
+ *     Markus Armbruster, 2004-2015
  */
 
 /*
@@ -80,14 +81,11 @@ conv(void)
 
 	    /* Anti-terrorist units count double */
 	    if (lchr[(int)land.lnd_type].l_flags & L_SECURITY) {
-
 		/*
 		 * They also increase the efficiency of
 		 * the conversion process by 10% each.
-		 * (but they use 10 mobility doing it)
 		 */
 		security_extra += .1;
-		land.lnd_mobil -= 10;
 		mil += land.lnd_item[I_MILIT];
 	    }
 	}