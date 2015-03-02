gefla/empserver

commit fe5b26658d92a53cee1ed0fe20f3f54c46d4e42c

Fix fire not to disclose retreat and wipe out target shell use
multifire() clobbered any changes to the target ship or sector made by
defend().  This let the target fire back for free.

multifire() retreated the target ship before reporting its location to
the player.  This disclosed its new location.

Fix by damaging and retreating the target after calling defend().

src/lib/commands/mfir.c
@@ -443,8 +443,6 @@ multifire(void)
 	case targ_sub:
 	    pr_beep();
 	    pr("Kawhomp!!!\n");
-	    if (vship.shp_rflags & RET_DCHRGED)
-		retreat_ship(&vship, 'd');
 	    break;
 	default:
 	    pr_beep();
@@ -470,18 +468,11 @@ multifire(void)
 		   player->cnum, xyas(x, y, vict), dam);
 	    pr("Shells hit sector %s for %d damage.\n",
 	       xyas(x, y, player->cnum), dam);
-	    if (target != targ_bogus)
-		sectdamage(&vsect, dam);
 	    break;
 	case targ_ship:
 	    nreport(player->cnum, N_SHP_SHELL, vict, 1);
 	    /* fall through */
 	default:
-	    if ((target != targ_sub) ||
-		((vship.shp_rflags & RET_DCHRGED) == 0))
-		check_retreat_and_do_shipdamage(&vship, dam);
-	    else
-		shipdamage(&vship, dam);
 	    if (vict) {
 		wu(0, vict,
 		   "Country #%d shelled %s in %s for %d damage.\n",
@@ -491,10 +482,6 @@ multifire(void)
 	    pr("Shells hit %s in %s for %d damage.\n",
 	       prsub(&vship),
 	       xyas(vship.shp_x, vship.shp_y, player->cnum), dam);
-
-	    if (vship.shp_effic < SHIP_MINEFF)
-		pr("%s sunk!\n", prsub(&vship));
-
 	    break;
 	}
 	/*  Ok, now, check if we had a bogus target.  If so,
@@ -509,9 +496,19 @@ multifire(void)
 	totaldefdam = defend(&fired, &defended, attgp, vict, &ndefending);
 	switch (target) {
 	case targ_land:
+	    getsect(x, y, &vsect);
+	    sectdamage(&vsect, dam);
 	    putsect(&vsect);
 	    break;
 	default:
+	    getship(vshipno, &vship);
+	    check_retreat_and_do_shipdamage(&vship, dam);
+	    if (vship.shp_effic < SHIP_MINEFF)
+		pr("%s sunk!\n", prsub(&vship));
+	    else if (target == targ_sub
+		&& (vship.shp_rflags & RET_DCHRGED)
+		&& !(vship.shp_rflags & RET_INJURED))
+		retreat_ship(&vship, 'd');
 	    putship(vship.shp_uid, &vship);
 	    break;
 	}

commit c88344dad43eb104c21a698baf16eabd75267b18

fire: Fix artillery splashing the bridge span under itself
multifire() writes back the firing sector after applying damage.  When
an artillery unit on a bridge span commits suicide by shelling down
the supporting bridge heads, this writeback puts the bridge span right
back (less the land units and planes on it), triggering a seqno oops.
On the next update of the bridge head, the bridge span falls again.
Broken in commit fe5b266, v4.3.14.

The problematic write back is superfluous.  Remove it along with a few
equally superfluous ones.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/commands/mfir.c
@@ -352,7 +352,6 @@ multifire(void)
 	    pr("Target out of range.\n");
 	    switch (type) {
 	    case EF_SECTOR:
-		putsect(&fsect);
 		break;
 	    case EF_LAND:
 		fland.lnd_mission = 0;
@@ -437,7 +436,6 @@ multifire(void)
 	}
 	switch (attgp->ef_type) {
 	case EF_SECTOR:
-	    putsect(&fsect);
 	    break;
 	case EF_SHIP:
 	    if ((target == targ_ship) || (target == targ_sub)) {
@@ -445,7 +443,6 @@ multifire(void)
 		    shp_missdef(&fship, vict);
 		}
 	    }
-	    putship(fship.shp_uid, &fship);
 	    break;
 	default:
 	    CANT_REACH();