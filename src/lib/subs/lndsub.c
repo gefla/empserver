gefla/empserver

commit 7c1b1661f5dae7bbadfbb83a6a7a2b552ee8d952

march: Check for sector abandonment before anyone marches
Unlike the move command, march checks sector abandonment before every
step.

If the player declines, the last land unit stays put and is removed
from the march.

Except when sectors or land units change while we're waiting for the
player's reply.  Then the last unit is not removed from the march.
This can scatter land units.  Screwed up when checking for abandoning
the sector was added in 4.2.2.

Change march to work like move, and to avoid scattering land units: if
the player declines to abandon the sector, the command simply fails.

Put the check into new lnd_abandon_askyn().

Extend would_abandon() and want_to_abandon() from a single land unit
to many.  Rename the latter to abandon_askyn() for consistency.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/subs/lndsub.c
@@ -949,13 +949,40 @@ lnd_mobcost(struct lndstr *lp, struct sctstr *sp)
     return lnd_pathcost(lp, sector_mcost(sp, lnd_mobtype(lp)));
 }
 
+/*
+ * Ask user to confirm sector abandonment, if any.
+ * All land units in LIST must be in the same sector.
+ * If removing the land units in LIST would abandon their sector, ask
+ * the user to confirm.
+ * Return zero when abandonment was declined, else non-zero.
+ */
+int lnd_abandon_askyn(struct emp_qelem *list)
+{
+    struct ulist *llp;
+    struct sctstr sect;
+    struct emp_qelem *qp;
+
+    if (QEMPTY(list))
+	return 1;
+    llp = (struct ulist *)list->q_back;
+    getsect(llp->unit.land.lnd_x, llp->unit.land.lnd_y, &sect);
+    if (!abandon_askyn(&sect, I_CIVIL, 0, llp))
+	return 0;
+    if (!check_sect_ok(&sect))
+	return 0;
+    for (qp = list->q_back; qp != list; qp = qp->q_back) {
+	if (!check_land_ok(&((struct ulist *)qp)->unit.land))
+	    return 0;
+    }
+    return 1;
+}
+
 int
 lnd_mar_one_sector(struct emp_qelem *list, int dir, natid actor,
 		   int together)
 {
     struct sctstr sect, osect;
     struct emp_qelem *qp;
-    struct emp_qelem *qp2;
     struct emp_qelem *next;
     struct ulist *llp;
     struct emp_qelem cur, done;
@@ -967,7 +994,6 @@ lnd_mar_one_sector(struct emp_qelem *list, int dir, natid actor,
     enum lnd_stuck stuck;
     int stopping = 0;
     int visible;
-    int stop;
     char dp[80];
     int rel;
     int oldown;
@@ -1031,27 +1057,6 @@ lnd_mar_one_sector(struct emp_qelem *list, int dir, natid actor,
 		    lnd_stays(actor, dp, llp);
 		    continue;
 		}
-	    }
-	}
-	/* Note we check would_abandon first because we don't want
-	   to always have to do these checks */
-	if (would_abandon(&osect, I_CIVIL, 0, &llp->unit.land)) {
-	    stop = 0;
-	    if (!want_to_abandon(&osect, I_CIVIL, 0, &llp->unit.land)) {
-		stop = 1;
-	    }
-	    /* now check stuff */
-	    if (!check_sect_ok(&sect))
-		return 1;
-	    if (!check_sect_ok(&osect))
-		return 1;
-	    for (qp2 = list->q_back; qp2 != list; qp2 = qp2->q_back) {
-		if (!check_land_ok(&((struct ulist *)qp2)->unit.land))
-		    return 1;
-	    }
-	    if (stop) {
-		lnd_stays(actor, "stops", llp);
-		continue;
 	    }
 	}
 	if (llp->mobil <= 0.0) {

include/land.h
@@ -196,6 +196,7 @@ extern void lnd_mar(struct emp_qelem *, double *, double *, int *, natid);
 extern void lnd_put(struct emp_qelem *);
 extern void lnd_put_one(struct ulist *);
 extern int lnd_hardtarget(struct lndstr *);
+extern int lnd_abandon_askyn(struct emp_qelem *);
 extern int lnd_mar_one_sector(struct emp_qelem *, int, natid, int);
 extern int lnd_support(natid, natid, coord, coord, int);
 extern int lnd_can_attack(struct lndstr *);

include/prototypes.h
@@ -404,8 +404,8 @@ extern int check_trade_ok(struct trdstr *);
 extern void set_coastal(struct sctstr *, int, int);
 /* control.c */
 extern int military_control(struct sctstr *);
-extern int want_to_abandon(struct sctstr *, i_type, int, struct lndstr *);
-extern int would_abandon(struct sctstr *, i_type, int, struct lndstr *);
+extern int abandon_askyn(struct sctstr *, i_type, int, struct ulist *);
+extern int would_abandon(struct sctstr *, i_type, int, struct ulist *);
 /* damage.c */
 extern void landdamage(struct lndstr *, int);
 extern void ship_damage(struct shpstr *, int);

src/lib/commands/expl.c
@@ -118,7 +118,7 @@ explore(void)
 	pr("Only exploring with %d.\n", amount);
     }
 
-    if (!want_to_abandon(&sect, vtype, amount, NULL)) {
+    if (!abandon_askyn(&sect, vtype, amount, NULL)) {
 	pr("Explore cancelled.\n");
 	return RET_FAIL;
     }

src/lib/commands/load.c
@@ -30,7 +30,7 @@
  *     David Sharnoff, 1987
  *     Ken Stevens, 1995 (rewritten)
  *     Steve McClure, 1998-2000
- *     Markus Armbruster, 2004-2013
+ *     Markus Armbruster, 2004-2014
  */
 
 #include <config.h>
@@ -692,7 +692,7 @@ load_comm_ship(struct sctstr *sectp, struct shpstr *sp,
 			   load_unload, atoi(p));
     if (!load_comm_ok(sectp, sp->shp_own, item, move_amt))
 	return RET_OK;
-    if (!want_to_abandon(sectp, item, move_amt, NULL))
+    if (!abandon_askyn(sectp, item, move_amt, NULL))
 	return RET_FAIL;
     if (!still_ok_ship(sectp, sp))
 	return RET_SYN;

src/lib/commands/move.c
@@ -140,7 +140,7 @@ move(void)
 	}
     }
 
-    if (!istest && !want_to_abandon(&sect, vtype, amount, NULL)) {
+    if (!istest && !abandon_askyn(&sect, vtype, amount, NULL)) {
 	pr("Move cancelled.\n");
 	return RET_FAIL;
     }

src/lib/commands/navi.c
@@ -74,6 +74,7 @@ do_unit_move(struct emp_qelem *ulist, int *together,
     int dir;
     int stopping = 0;
     int skip = 0;
+    int moved = 0;
     char buf[1024];
     char prompt[128];
     char bmap_flag;
@@ -153,10 +154,14 @@ do_unit_move(struct emp_qelem *ulist, int *together,
 	    if (type == EF_SHIP)
 		stopping |= shp_nav_one_sector(ulist, dir,
 					       player->cnum, *together);
-	    else
+	    else {
+		if (!moved && !lnd_abandon_askyn(ulist))
+		    return RET_FAIL;
 		stopping |=
 		    lnd_mar_one_sector(ulist, dir, player->cnum,
 				       *together);
+	    }
+	    moved = 1;
 	    cp++;
 	    continue;
 	}

src/lib/subs/control.c
@@ -28,6 +28,7 @@
  *
  *  Known contributors to this file:
  *     Dave Pare, 1986
+ *     Markus Armbruster, 2014
  */
 
 #include <config.h>
@@ -38,6 +39,7 @@
 #include "player.h"
 #include "prototypes.h"
 #include "sect.h"
+#include "unit.h"
 
 /*
  * Does the player->owner have military control of this sector?
@@ -62,8 +64,16 @@ military_control(struct sctstr *sp)
     return 1;
 }
 
+/*
+ * Ask user to confirm abandonment of sector SP, if any.
+ * If removing AMNT commodities of type VTYPE and the land units in
+ * LIST would abandon their sector, ask the user to confirm.
+ * All land units in LIST must be in this sector.  LIST may be null.
+ * Return zero when abandonment was declined, else non-zero.
+ */
 int
-want_to_abandon(struct sctstr *sp, i_type vtype, int amnt, struct lndstr *lp)
+abandon_askyn(struct sctstr *sp, i_type vtype, int amnt,
+	      struct ulist *land_list)
 {
     char prompt[80];
 
@@ -71,7 +81,7 @@ want_to_abandon(struct sctstr *sp, i_type vtype, int amnt, struct lndstr *lp)
      * First, would we be abandoning it?  If not, just return that
      * it's ok to move out.
      */
-    if (!would_abandon(sp, vtype, amnt, lp))
+    if (!would_abandon(sp, vtype, amnt, land_list))
 	return 1;
 
     sprintf(prompt, "Do you really want to abandon %s [yn]? ",
@@ -80,10 +90,17 @@ want_to_abandon(struct sctstr *sp, i_type vtype, int amnt, struct lndstr *lp)
     return askyn(prompt);
 }
 
+/*
+ * Would removing this stuff from SP abandon it?
+ * Consider removal of AMNT commodities of type VTYPE and the land
+ * units in LIST.
+ * All land units in LIST must be in this sector.  LIST may be null.
+ */
 int
-would_abandon(struct sctstr *sp, i_type vtype, int amnt, struct lndstr *lp)
+would_abandon(struct sctstr *sp, i_type vtype, int amnt,
+	      struct ulist *land_list)
 {
-    int mil, civs;
+    int mil, civs, nland;
 
     if (vtype != I_CIVIL && vtype != I_MILIT)
 	return 0;
@@ -96,6 +113,10 @@ would_abandon(struct sctstr *sp, i_type vtype, int amnt, struct lndstr *lp)
     if (vtype == I_CIVIL)
 	civs -= amnt;
 
-    return sp->sct_own != 0 && civs <= 0 && mil <= 0
-	&& !has_units(sp->sct_x, sp->sct_y, sp->sct_own, lp);
+    if (!sp->sct_own || civs > 0 || mil > 0)
+	return 0;
+    nland = unitsatxy(sp->sct_x, sp->sct_y, 0, 0, 1);
+    if (land_list)
+	nland -= emp_quelen(&land_list->queue);
+    return nland <= 0;
 }

commit 863fde5a2c90cafbdbf5b5f181dba42723c3a82d

march: Fix concurrent updates at sector abandon prompt
When the player declines to abandon a sector, we write back stale land
units, triggering a generation oops.  Any updates made by other
threads meanwhile are wiped out, triggering a seqno mismatch oops.

The culprit is lnd_abandon_askyn(): when the player declines, it
returns without calling check_sect_ok(), check_land_ok().  Broken in
commit 7c1b166, v4.3.33.  Fix it.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/subs/lndsub.c
@@ -963,21 +963,21 @@ int lnd_abandon_askyn(struct emp_qelem *list)
 {
     struct ulist *llp;
     struct sctstr sect;
+    int abandon;
     struct emp_qelem *qp;
 
     if (QEMPTY(list))
 	return 1;
     llp = (struct ulist *)list->q_back;
     getsect(llp->unit.land.lnd_x, llp->unit.land.lnd_y, &sect);
-    if (!abandon_askyn(&sect, I_CIVIL, 0, llp))
-	return 0;
+    abandon = abandon_askyn(&sect, I_CIVIL, 0, llp);
     if (!check_sect_ok(&sect))
 	return 0;
     for (qp = list->q_back; qp != list; qp = qp->q_back) {
 	if (!check_land_ok(&((struct ulist *)qp)->unit.land))
 	    return 0;
     }
-    return 1;
+    return abandon;
 }
 
 int

tests/navi-march/server.log
@@ -12,10 +12,6 @@ tester@127.0.0.1 logged out, country #1
 Connect from 127.0.0.1
 tester@127.0.0.1 using country #1
 tester@127.0.0.1 logged in as country #1
-Oops: elt->generation != (ef_generation & 0xfff) in ../src/lib/common/file.c:699
-Crash dump complete
-Oops: elt->generation != (ef_generation & 0xfff) in ../src/lib/common/file.c:699
-Crash dump complete
 tester@127.0.0.1 logged out, country #1
 Connect from 127.0.0.1
 tester@127.0.0.1 using country #0