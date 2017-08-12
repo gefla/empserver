gefla/empserver

commit bb0f3a656ef1bd9e1541e891e714f57234c003b7

Make: Tidy up distribution and cleaning of generated makefiles
Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

Make.mk
@@ -101,10 +101,8 @@ subst.in = sed \
 
 # Generated files
 # See `Cleanliness' below
-mk :=
-ifeq ($(revctrl),git)
-mk += $(srcdir)/sources.mk
-endif
+# Generated makefiles, distributed by dist-source from $(srcdir):
+mk := sources.mk
 # Generated by Autoconf, not distributed:
 ac := config.h config.log config.status info.html info.nr lib stamp-h
 ac += $(basename $(filter %.in, $(src)))
@@ -157,9 +155,12 @@ endif
 clean := $(obj) $(deps) $(libs) $(util) $(client) $(server) $(tsubj)	\
 info/toc info/TOP.t $(info.all) $(empth_obj) $(empth_lib) sandbox
 # Removed by distclean:
-distclean := $(ac) $(mk)
+distclean := $(ac)
+ifeq ($(revctrl),git)
+distclean += $(addprefix $(srcdir)/, $(mk))
+endif
 # Distributed by dist-source from $(srcdir):
-src_distgen := $(acdist)
+src_distgen := $(acdist) $(mk)
 # Distributed by dist-client from $(srcdir)/src/client; removed by distclean:
 cli_distgen := $(acdistcli)
 
@@ -366,10 +367,13 @@ info.ps: info/TROFF.MAC info/INFO.MAC info/TOP.t $(tsubj) $(tsrc)
 
 .PHONY: dist-source
 dist-source: $(src_distgen)
+	$(tarball) $(TARNAME)-$(VERSION) -C $(srcdir) $(src_distgen) $(src)
+
 ifeq ($(revctrl),git)
-	echo "src := $(src)" >$(srcdir)/sources.mk
+.PHONY: $(srcdir)/sources.mk
+$(srcdir)/sources.mk:
+	$(call quiet-command,echo "src := $(src)" >$@,GEN $@)
 endif
-	$(tarball) $(TARNAME)-$(VERSION) -C $(srcdir) $(src_distgen) $(src) sources.mk
 
 .PHONY: dist-client
 dist-client: $(cli_distgen)

commit 5f59699cfd62c29c8e4db108ea0839f573ec584d

Make: Fix "make dist" to remake stale files in $(srcdir)
Broken in commit bb0f3a6.  VPATH claims another victim.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

Make.mk
@@ -385,7 +385,7 @@ info.ps: info/TROFF.MAC info/INFO.MAC info/TOP.t $(tsubj) $(tsrc)
 # Distributing
 
 .PHONY: dist-source
-dist-source: $(src_distgen)
+dist-source: $(addprefix $(srcdir)/, $(src_distgen))
 	$(tarball) $(TARNAME) $(version) -C $(srcdir) $(src_distgen) $(src)
 
 ifeq ($(revctrl),git)
@@ -395,7 +395,7 @@ $(srcdir)/sources.mk:
 endif
 
 .PHONY: dist-client
-dist-client: $(cli_distgen)
+dist-client: $(addprefix $(srcdir)/, $(cli_distgen))
 	$(tarball) $(TARNAME)-client $(version)				\
 	-C $(srcdir)/src/client						\
 		$(notdir $(filter src/client/%, $(src))	$(cli_distgen))	\