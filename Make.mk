gefla/empserver

commit 1ec9b94f7467fdd51462c6c22c40ecb14403ed3f

configure Make: More detailed version information
Version information is in output of commands version, xdump version,
and in program output for option -v.  Looks like this:

    Wolfpack Empire 4.3.33

The version number is defined in configure.ac, and incremented
manually.  It identifies only the base release (here: 4.3.33).  Fine
when this is an unmodified released version.  Pretty much useless
during development.

Add a suffix to the version number that describes it further:

    V               Unmodified release V (same as before)
    V.N-H           Modified release built from a clean git tree
                    N is the number of additional commits, and
                    H is the abbreviated commit hash
    V.N-H-dirty     Same, but the working tree is dirty
    V-dirty         Modified release built from a tarball

A git tree is clean when the contents of its files are unchanged.
Changing only the their timestamps doesn't count.  It does count when
building from a tarball, because tracking contents isn't implemented
there.

Also use this suffixed version for tarball names.

The version reported by configure is fixed at configure generation
time, i.e. it is usually out of date during development.  Ensuring a
release tarball contains one with a current version is manual for now.
Running autoconf -f should do the trick.

Elsewhere, the version is determined at build time, so it is always
current.

Dirty tracking isn't implemented in the standalone client build.  If
you start with a clean tarball, the version will not change from V to
V-dirty when you build with modifications.

Steal build-aux/git-version-gen from autoconf 2.69 to help with
computing the version string.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

Make.mk
@@ -47,8 +47,13 @@ all:
 # Source files
 ifeq ($(revctrl),git)
 src := $(shell cd $(srcdir) && git ls-files | uniq)
+version := $(shell cd $(srcdir) && build-aux/git-version-gen /dev/null)
 else
 include $(srcdir)/sources.mk
+version := $(shell cat $(srcdir)/.tarball-version || echo "UNKNOWN")
+endif
+ifeq ($(version),UNKNOWN)
+$(error cannot figure out version)
 endif
 dirs := $(sort $(dir $(src)))
 csrc := $(filter %.c, $(src))
@@ -333,6 +338,19 @@ $(libs) $(empth_lib):
 	$(call quiet-command,$(AR) rc $@ $?,AR $@)
 	$(RANLIB) $@
 
+src/lib/global/version.o: CPPFLAGS += -DVERSION='"$(version)"'
+src/lib/global/version.o: $(src)
+
+ifneq ($(revctrl),git)
+$(srcdir)/.tarball-version: $(src)
+	v=`sed -e 's/-dirty$$//' <$@`; echo "$$v-dirty" >$@
+# Force Make to start over after updating .tarball-version, so that
+# $(version) gets the new value
+$(srcdir)/.dirty-stamp: .tarball-version
+	>$@
+include $(srcdir)/.dirty-stamp
+endif
+
 # Info formatting
 
 # mksubj.pl reads $(tsrc) and writes $(tsubj).  A naive rule
@@ -367,7 +385,7 @@ info.ps: info/TROFF.MAC info/INFO.MAC info/TOP.t $(tsubj) $(tsrc)
 
 .PHONY: dist-source
 dist-source: $(src_distgen)
-	$(tarball) $(TARNAME)-$(VERSION) -C $(srcdir) $(src_distgen) $(src)
+	$(tarball) $(TARNAME) $(version) -C $(srcdir) $(src_distgen) $(src)
 
 ifeq ($(revctrl),git)
 .PHONY: $(srcdir)/sources.mk
@@ -377,7 +395,7 @@ endif
 
 .PHONY: dist-client
 dist-client: $(cli_distgen)
-	$(tarball) $(TARNAME)-client-$(VERSION)				\
+	$(tarball) $(TARNAME)-client $(version)				\
 	-C $(srcdir)/src/client						\
 		$(notdir $(filter src/client/%, $(src))	$(cli_distgen))	\
 	-C $(srcdir)/include proto.h version.h				\
@@ -389,8 +407,8 @@ dist-client: $(cli_distgen)
 
 .PHONY: dist-info
 dist-info: info html
-	$(tarball) $(TARNAME)-info-text-$(VERSION) -C info.nr $(info)
-	$(tarball) $(TARNAME)-info-html-$(VERSION) -C info.html $(addsuffix .html, $(info))
+	$(tarball) $(TARNAME)-info-text $(version) -C info.nr $(info)
+	$(tarball) $(TARNAME)-info-html $(version) -C info.html $(addsuffix .html, $(info))
 
 # Dependencies
 

build-aux/git-version-gen
@@ -1,0 +1,220 @@
+#!/bin/sh
+# Print a version string.
+scriptversion=2012-03-18.17; # UTC
+
+# Copyright (C) 2007-2012 Free Software Foundation, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This script is derived from GIT-VERSION-GEN from GIT: http://git.or.cz/.
+# It may be run two ways:
+# - from a git repository in which the "git describe" command below
+#   produces useful output (thus requiring at least one signed tag)
+# - from a non-git-repo directory containing a .tarball-version file, which
+#   presumes this script is invoked like "./git-version-gen .tarball-version".
+
+# In order to use intra-version strings in your project, you will need two
+# separate generated version string files:
+#
+# .tarball-version - present only in a distribution tarball, and not in
+#   a checked-out repository.  Created with contents that were learned at
+#   the last time autoconf was run, and used by git-version-gen.  Must not
+#   be present in either $(srcdir) or $(builddir) for git-version-gen to
+#   give accurate answers during normal development with a checked out tree,
+#   but must be present in a tarball when there is no version control system.
+#   Therefore, it cannot be used in any dependencies.  GNUmakefile has
+#   hooks to force a reconfigure at distribution time to get the value
+#   correct, without penalizing normal development with extra reconfigures.
+#
+# .version - present in a checked-out repository and in a distribution
+#   tarball.  Usable in dependencies, particularly for files that don't
+#   want to depend on config.h but do want to track version changes.
+#   Delete this file prior to any autoconf run where you want to rebuild
+#   files to pick up a version string change; and leave it stale to
+#   minimize rebuild time after unrelated changes to configure sources.
+#
+# As with any generated file in a VC'd directory, you should add
+# /.version to .gitignore, so that you don't accidentally commit it.
+# .tarball-version is never generated in a VC'd directory, so needn't
+# be listed there.
+#
+# Use the following line in your configure.ac, so that $(VERSION) will
+# automatically be up-to-date each time configure is run (and note that
+# since configure.ac no longer includes a version string, Makefile rules
+# should not depend on configure.ac for version updates).
+#
+# AC_INIT([GNU project],
+#         m4_esyscmd([build-aux/git-version-gen .tarball-version]),
+#         [bug-project@example])
+#
+# Then use the following lines in your Makefile.am, so that .version
+# will be present for dependencies, and so that .version and
+# .tarball-version will exist in distribution tarballs.
+#
+# EXTRA_DIST = $(top_srcdir)/.version
+# BUILT_SOURCES = $(top_srcdir)/.version
+# $(top_srcdir)/.version:
+#	echo $(VERSION) > $@-t && mv $@-t $@
+# dist-hook:
+#	echo $(VERSION) > $(distdir)/.tarball-version
+
+
+me=$0
+
+version="git-version-gen $scriptversion
+
+Copyright 2011 Free Software Foundation, Inc.
+There is NO warranty.  You may redistribute this software
+under the terms of the GNU General Public License.
+For more information about these matters, see the files named COPYING."
+
+usage="\
+Usage: $me [OPTION]... \$srcdir/.tarball-version [TAG-NORMALIZATION-SED-SCRIPT]
+Print a version string.
+
+Options:
+
+   --prefix           prefix of git tags (default 'v')
+
+   --help             display this help and exit
+   --version          output version information and exit
+
+Running without arguments will suffice in most cases."
+
+prefix=v
+
+while test $# -gt 0; do
+  case $1 in
+    --help) echo "$usage"; exit 0;;
+    --version) echo "$version"; exit 0;;
+    --prefix) shift; prefix="$1";;
+    -*)
+      echo "$0: Unknown option '$1'." >&2
+      echo "$0: Try '--help' for more information." >&2
+      exit 1;;
+    *)
+      if test -z "$tarball_version_file"; then
+        tarball_version_file="$1"
+      elif test -z "$tag_sed_script"; then
+        tag_sed_script="$1"
+      else
+        echo "$0: extra non-option argument '$1'." >&2
+        exit 1
+      fi;;
+  esac
+  shift
+done
+
+if test -z "$tarball_version_file"; then
+    echo "$usage"
+    exit 1
+fi
+
+tag_sed_script="${tag_sed_script:-s/x/x/}"
+
+nl='
+'
+
+# Avoid meddling by environment variable of the same name.
+v=
+v_from_git=
+
+# First see if there is a tarball-only version file.
+# then try "git describe", then default.
+if test -f $tarball_version_file
+then
+    v=`cat $tarball_version_file` || v=
+    case $v in
+        *$nl*) v= ;; # reject multi-line output
+        [0-9]*) ;;
+        *) v= ;;
+    esac
+    test -z "$v" \
+        && echo "$0: WARNING: $tarball_version_file is missing or damaged" 1>&2
+fi
+
+if test -n "$v"
+then
+    : # use $v
+# Otherwise, if there is at least one git commit involving the working
+# directory, and "git describe" output looks sensible, use that to
+# derive a version string.
+elif test "`git log -1 --pretty=format:x . 2>&1`" = x \
+    && v=`git describe --abbrev=4 --match="$prefix*" HEAD 2>/dev/null \
+          || git describe --abbrev=4 HEAD 2>/dev/null` \
+    && v=`printf '%s\n' "$v" | sed "$tag_sed_script"` \
+    && case $v in
+         $prefix[0-9]*) ;;
+         *) (exit 1) ;;
+       esac
+then
+    # Is this a new git that lists number of commits since the last
+    # tag or the previous older version that did not?
+    #   Newer: v6.10-77-g0f8faeb
+    #   Older: v6.10-g0f8faeb
+    case $v in
+        *-*-*) : git describe is okay three part flavor ;;
+        *-*)
+            : git describe is older two part flavor
+            # Recreate the number of commits and rewrite such that the
+            # result is the same as if we were using the newer version
+            # of git describe.
+            vtag=`echo "$v" | sed 's/-.*//'`
+            commit_list=`git rev-list "$vtag"..HEAD 2>/dev/null` \
+                || { commit_list=failed;
+                     echo "$0: WARNING: git rev-list failed" 1>&2; }
+            numcommits=`echo "$commit_list" | wc -l`
+            v=`echo "$v" | sed "s/\(.*\)-\(.*\)/\1-$numcommits-\2/"`;
+            test "$commit_list" = failed && v=UNKNOWN
+            ;;
+    esac
+
+    # Change the first '-' to a '.', so version-comparing tools work properly.
+    # Remove the "g" in git describe's output string, to save a byte.
+    v=`echo "$v" | sed 's/-/./;s/\(.*\)-g/\1-/'`;
+    v_from_git=1
+else
+    v=UNKNOWN
+fi
+
+v=`echo "$v" |sed "s/^$prefix//"`
+
+# Test whether to append the "-dirty" suffix only if the version
+# string we're using came from git.  I.e., skip the test if it's "UNKNOWN"
+# or if it came from .tarball-version.
+if test -n "$v_from_git"; then
+  # Don't declare a version "dirty" merely because a time stamp has changed.
+  git update-index --refresh > /dev/null 2>&1
+
+  dirty=`exec 2>/dev/null;git diff-index --name-only HEAD` || dirty=
+  case "$dirty" in
+      '') ;;
+      *) # Append the suffix only if there isn't one already.
+          case $v in
+            *-dirty) ;;
+            *) v="$v-dirty" ;;
+          esac ;;
+  esac
+fi
+
+# Omit the trailing newline, so that m4_esyscmd can use the result directly.
+echo "$v" | tr -d "$nl"
+
+# Local variables:
+# eval: (add-hook 'write-file-hooks 'time-stamp)
+# time-stamp-start: "scriptversion="
+# time-stamp-format: "%:y-%02m-%02d.%02H"
+# time-stamp-time-zone: "UTC"
+# time-stamp-end: "; # UTC"
+# End:

configure.ac
@@ -27,7 +27,7 @@
 #   configure.ac: Autoconf input file
 #
 #   Known contributors to this file:
-#      Markus Armbruster, 2005-2015
+#      Markus Armbruster, 2005-2016
 #
 # Process this file with autoconf to produce a configure script.
 
@@ -37,7 +37,9 @@
 # whether it's worth fixing.
 
 AC_PREREQ(2.69)
-AC_INIT([Wolfpack Empire], [4.3.34], [wolfpack@wolfpackempire.com], [empire],
+AC_INIT([Wolfpack Empire],
+	m4_esyscmd([build-aux/git-version-gen .tarball-version]),
+	[wolfpack@wolfpackempire.com], [empire],
 	[http://www.wolfpackempire.com/])
 AC_CONFIG_SRCDIR([include/combat.h])
 AC_CONFIG_AUX_DIR([build-aux])
@@ -58,8 +60,12 @@ AC_ARG_VAR(NROFF, [nroff command])
 AC_CHECK_PROG(NROFF, groff, [GROFF_NO_SGR= groff -Tascii -U], nroff)
 
 # not really a check for a program, but close enough
-if test -d $srcdir/.git; then revctrl=git
+if test -d $srcdir/.git
+then revctrl=git
 else revctrl=
+fi
+if test "$revctrl" && test -r .tarball-version
+then AC_MSG_ERROR([.tarball-version must not exist])
 fi
 AC_SUBST(revctrl,$revctrl)
 

src/client/configure.ac
@@ -27,7 +27,7 @@
 #   configure.ac: Autoconf input file
 #
 #   Known contributors to this file:
-#      Markus Armbruster, 2005-2015
+#      Markus Armbruster, 2005-2016
 #
 # Process this file with autoconf to produce a configure script.
 
@@ -37,7 +37,9 @@
 # whether it's worth fixing.
 
 AC_PREREQ(2.69)
-AC_INIT([Empire Client], [4.3.34], [wolfpack@wolfpackempire.com],,
+AC_INIT([Wolfpack Empire Client],
+	m4_esyscmd([cat .tarball-version]),
+	[wolfpack@wolfpackempire.com],,
 	[http://www.wolfpackempire.com/])
 AC_CONFIG_SRCDIR([empire.6])
 AC_CONFIG_HEADERS([config.h])
@@ -87,6 +89,8 @@ test "$EMPIREPORT" || EMPIREPORT=6789
 MY_WITH_TERMINFO
 
 ### Output
+
+AC_DEFINE_UNQUOTED(VERSION, "`cat .tarball-version`", [Tarball version])
 
 AC_CONFIG_FILES([Makefile ipglob.c])
 AC_OUTPUT

src/lib/global/version.c
@@ -27,14 +27,14 @@
  *  version.c:
  *
  *  Known contributors to this file:
- *     Markus Armbruster, 2006
+ *     Markus Armbruster, 2006-2016
  */
 
 #include <config.h>
 
 #include "version.h"
 
-char version[] = PACKAGE_STRING;
+char version[] = PACKAGE_NAME " " VERSION;
 char legal[] =
 "Copyright (C) 1986-2016, Dave Pare, Jeff Bailey, Thomas Ruschak,\n"
 "              Ken Stevens, Steve McClure, Markus Armbruster\n"

src/scripts/tarball
@@ -5,9 +5,13 @@ if [ $# = 0 ]; then
     exit 1
 fi
 
-name=$1; shift
+name="$1-$2"
+version="$2"
+shift; shift
 
 mkdir $name
+printf "%s" $version >$name/.tarball-version
+>$name/.dirty-stamp
 
 # arrange cleanup
 trap 'rm -rf "$name"' 0

tests/normalize.pl
@@ -87,6 +87,8 @@ while (<>) {
 	    and (/\: No (sector|ship|plane|unit|nuke)\(s\)|\: Nothing lost/
 		 or /^[0-9]+ (sector|ship|plane|unit|nuke|lost item)/));
 
+    ### Version
+    s/(Wolfpack( |\\\\040)Empire( |\\\\040))[0-9][^ "]*/${1}4.3.34/;
     ### Formatted time
     # nat_timeused in prompt
     s/^\[[0-9]+(:[0-9]+\] Command \:)/[0$1/;

commit 10ddaca6b12cda2766269e9fc59f7612ad0da01a

Make: Fix configure generated for dist-client
Commit 1ec9b94 broke regeneration of configure for dist-client: it
uses .tarball-version, which exists only in the tarball, not in the
source tree.

Generating the client's autoconf stuff into the source tree is a
questionable idea anyway, as it won't run there.  Generate it while
building the tarball instead.

Bonus: doesn't distribute a useless .dirty-stamp.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

.gitignore
@@ -55,10 +55,6 @@
 /configure
 /stamp-h.in
 /sources.mk
-# $(cli_distgen)
-/src/client/aclocal.m4
-/src/client/config.h.in
-/src/client/configure
 
 # Generic junk
 *.orig

Make.mk
@@ -114,8 +114,6 @@ ac += $(basename $(filter %.in, $(src)))
 ac += $(srcdir)/autom4te.cache $(srcdir)/src/client/autom4te.cache
 # distributed by dist-source from $(srcdir):
 acdist := aclocal.m4 config.h.in configure stamp-h.in
-# distributed by dist-client from $(srcdir):
-acdistcli := $(addprefix src/client/, aclocal.m4 config.h.in configure)
 # Object files:
 obj := $(csrc:.c=.o) $(filter %.o, $(ac:.c=.o))
 # Dependencies:
@@ -166,9 +164,6 @@ distclean += $(addprefix $(srcdir)/, $(mk))
 endif
 # Distributed by dist-source from $(srcdir):
 src_distgen := $(acdist) $(mk)
-# Distributed by dist-client from $(srcdir)/src/client; removed by distclean:
-cli_distgen := $(acdistcli)
-distclean += $(addprefix $(srcdir)/, $(cli_distgen))
 
 # Compiler flags
 CPPFLAGS += -I$(srcdir)/include -I.
@@ -395,18 +390,20 @@ $(srcdir)/sources.mk:
 endif
 
 .PHONY: dist-client
-dist-client: $(addprefix $(srcdir)/, $(cli_distgen))
-	$(tarball)  -x $(srcdir)/src/scripts/gen-tarball-version	\
+dist-client:
+	$(tarball) -x $(srcdir)/src/scripts/gen-client-configure	\
 	$(TARNAME)-client $(version)					\
 	-C $(srcdir)/src/client						\
-		$(notdir $(filter src/client/%, $(src))	$(cli_distgen))	\
+		$(notdir $(filter src/client/%, $(src)))		\
 	-C $(srcdir)/include fnameat.h proto.h version.h		\
 	-C $(srcdir)/src/lib/global version.c				\
 	-C $(srcdir)/src/lib/gen fnameat.c				\
 	-C $(srcdir)/src/lib $(addprefix w32/, $(client/w32))		\
 	-C $(srcdir)/man empire.6					\
 	-C $(srcdir)/build-aux install-sh				\
-	-C $(srcdir) COPYING INSTALL
+	-C $(srcdir) COPYING INSTALL					\
+		m4/ax_lib_socket_nsl.m4 m4/my_lib_readline.m4		\
+		m4/my_terminfo.m4 m4/my_windows_api.m4
 
 .PHONY: dist-info
 dist-info: info html
@@ -449,16 +446,3 @@ config.status: configure
 
 src/lib/global/path.c src/client/ipglob.c: %: %.in GNUmakefile Make.mk
 	$(call quiet-command,$(subst.in) <$< >$@,GEN $@)
-
-
-# Make files for standalone client distribution
-
-$(srcdir)/src/client/configure: src/client/configure.ac src/client/aclocal.m4
-	cd $(dir $@) && autoconf
-
-$(srcdir)/src/client/config.h.in: src/client/configure.ac src/client/aclocal.m4
-	cd $(dir $@) && autoheader
-	touch $@
-
-$(srcdir)/src/client/aclocal.m4: m4/ax_lib_socket_nsl.m4 m4/my_lib_readline.m4 m4/my_terminfo.m4 m4/my_windows_api.m4
-	cat $^ >$@

src/scripts/gen-client-configure
@@ -1,0 +1,13 @@
+#!/bin/sh -e
+
+if [ $# = 0 ]; then
+    echo "Usage: $0 NAME VERSION (FILE | -C DIR)..."
+    exit 1
+fi
+
+cd $1-$2
+printf "%s" $2 >.tarball-version
+cat m4/*.m4 >aclocal.m4
+autoconf
+autoheader
+rm -rf m4 autom4te.cache