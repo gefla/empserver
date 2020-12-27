e551b6198d36605120f1479baccbcaf1cb400313
Markus Armbruster <armbru@pond.sub.org>
2020-12-27 10:06:08 +0100

    client: Move get_password() from login.c to getpass.c
    Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

---
+++ src/client/getpass.c
@@ -0,0 +1,77 @@
+/*
+ *  Empire - A multi-player, client/server Internet based war game.
+ *  Copyright (C) 1986-2020, Dave Pare, Jeff Bailey, Thomas Ruschak,
+ *                Ken Stevens, Steve McClure, Markus Armbruster
+ *
+ *  Empire is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  ---
+ *
+ *  See files README, COPYING and CREDITS in the root of the source
+ *  tree for related information and legal notices.  It is expected
+ *  that future projects/authors will amend these files as needed.
+ *
+ *  ---
+ *
+ *  getpass.c: Get a password
+ *
+ *  Known contributors to this file:
+ *     Markus Armbruster, 2009-2020
+ */
+
+#include <config.h>
+
+#ifdef HAVE_GETPASS
+#include <unistd.h>
+#else
+#include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#endif
+#include "misc.h"
+
+char *
+get_password(const char *prompt)
+{
+#ifdef HAVE_GETPASS
+    return getpass(prompt);
+#else
+    static char buf[128];
+    char *p;
+    size_t len;
+#ifdef _WIN32
+    DWORD mode;
+    HANDLE input_handle = GetStdHandle(STD_INPUT_HANDLE);
+
+    if (GetConsoleMode(input_handle, &mode))
+	SetConsoleMode(input_handle, mode & ~ENABLE_ECHO_INPUT);
+    else
+#endif
+	printf("Note: your input is echoed to the screen\n");
+    printf("%s", prompt);
+    fflush(stdout);
+    p = fgets(buf, sizeof(buf), stdin);
+#ifdef _WIN32
+    if (GetConsoleMode(input_handle, &mode))
+	SetConsoleMode(input_handle, mode | ENABLE_ECHO_INPUT);
+#endif
+    if (!p)
+	return NULL;
+    len = strlen(p);
+    if (p[len - 1] == '\n')
+	p[len - 1] = 0;
+    return p;
+#endif	/* !HAVE_GETPASS */
+}
