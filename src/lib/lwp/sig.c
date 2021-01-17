gefla/empserver

commit fe2de3d743e07cc5ac3b932eb106fe473bd1a838

Simplify.  Should also be more efficient. (LwpSigCheck): New. (lwpCatchAwaitedSig): Set it. (lwpSigWait): Clear it. (lwpSigWakeup): Test it. (lwpSigWakeup): Unblock LwpSigWaiter without testing LwpSigCatched. (lwpSigWait): Test LwpSigCatched after wakeup. (LwpSigAwaited, LwpSigPtr): Remove.

src/lib/lwp/sig.c
@@ -28,7 +28,7 @@
  *  sig.c: Wait for signals
  * 
  *  Known contributors to this file:
- *     Markus Armbruster, 2006
+ *     Markus Armbruster, 2006-2007
  */
 
 #include <config.h>
@@ -38,20 +38,19 @@
 #include "lwp.h"
 #include "lwpint.h"
 
-/* Signals awaited by lwpSigWait() */
-static sigset_t *LwpSigAwaited;
-
 /*
- * Signals from LwpSigAwaited catched so far
+ * Signals catched so far.
  * Access only with signals blocked!
  */
 static sigset_t LwpSigCatched;
 
+/*
+ * LwpSigCatched changed since last 
+ */
+static sig_atomic_t LwpSigCheck;
+
 /* The thread waiting for signals in lwpSigWait() */
 static struct lwpProc *LwpSigWaiter;
-
-/* Where to return the signal number to the thread in lwpSigWait() */
-static int *LwpSigPtr;
 
 static void lwpCatchAwaitedSig(int);
 
@@ -80,6 +79,7 @@ static void
 lwpCatchAwaitedSig(int sig)
 {
     sigaddset(&LwpSigCatched, sig);
+    LwpSigCheck = 1;
 }
 
 /*
@@ -119,14 +119,14 @@ lwpSigWait(sigset_t *set, int *sig)
 
     if (CANT_HAPPEN(LwpSigWaiter))
 	return EBUSY;
-    res = lwpGetSig(set);
-    if (res <= 0) {
+    for (;;) {
+	LwpSigCheck = 0;
+	res = lwpGetSig(set);
+	if (res > 0)
+	    break;
 	lwpStatus(LwpCurrent, "Waiting for signals");
-	LwpSigAwaited = set;
-	LwpSigPtr = sig;
 	LwpSigWaiter = LwpCurrent;
 	lwpReschedule();
-	return 0;
     }
     *sig = res;
     return 0;
@@ -139,13 +139,7 @@ lwpSigWait(sigset_t *set, int *sig)
 void
 lwpSigWakeup(void)
 {
-    int res;
-
-    if (!LwpSigWaiter)
-	return;
-    res = lwpGetSig(LwpSigAwaited);
-    if (res > 0) {
-	*LwpSigPtr = res;
+    if (LwpSigWaiter && LwpSigCheck) {
 	lwpReady(LwpSigWaiter);
 	LwpSigWaiter = NULL;
     }

commit bfb558dee35875037fdea18a6c7ae2c4a97bec13

lwp: Fix signal wait screwup for multiple different signals
lwpSigWait() clears LwpSigCheck even when signals remain in
LwpSigCaught.  The next empth_wait_for_signal() will then wait until
another one gets caught.  Broken in commit fe2de3d74, v4.3.10.

Fix by clearing it only when LwpSigCaught is empty.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/lwp/sig.c
@@ -91,9 +91,10 @@ static int
 lwpGetSig(sigset_t *set)
 {
     sigset_t save;
-    int i;
+    int i, j;
 
     sigprocmask(SIG_BLOCK, set, &save);
+
     for (i = NSIG - 1; i > 0; i--) {
 	if (sigismember(set, i) > 0 && sigismember(&LwpSigCaught, i) > 0) {
 	    lwpStatus(LwpCurrent, "Got awaited signal %d", i);
@@ -101,6 +102,14 @@ lwpGetSig(sigset_t *set)
 	    break;
 	}
     }
+
+    for (j = i;
+	 sigismember(set, i) > 0 && sigismember(&LwpSigCaught, i) > 0;
+	 j--)
+	;
+    if (!j)
+	LwpSigCheck = 0;
+
     sigprocmask(SIG_SETMASK, &save, NULL);
     return i;
 }
@@ -119,7 +128,6 @@ lwpSigWait(sigset_t *set, int *sig)
     if (LwpSigWaiter)
 	return EBUSY;
     for (;;) {
-	LwpSigCheck = 0;
 	res = lwpGetSig(set);
 	if (res > 0)
 	    break;