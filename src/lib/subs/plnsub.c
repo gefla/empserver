gefla/empserver

commit 820d755e597bee13ec3f5d4b798d32de99953628

subs: Change pln_damage()'s parameter noisy to string prefix
No functional change for now.  The next commit will put it to use.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/subs/plnsub.c
@@ -30,7 +30,7 @@
  *     Dave Pare, 1986
  *     Ken Stevens, 1995
  *     Steve McClure, 1998-2000
- *     Markus Armbruster, 2004-2012
+ *     Markus Armbruster, 2004-2015
  */
 
 #include <config.h>
@@ -1004,7 +1004,7 @@ pln_hitchance(struct plnstr *pp, int hardtarget, int type)
 }
 
 int
-pln_damage(struct plnstr *pp, char type, int noisy)
+pln_damage(struct plnstr *pp, char type, char *noisy)
 {
     struct plchrstr *pcp = plchr + pp->pln_type;
     int load, i, hitroll, aim, len;
@@ -1039,8 +1039,14 @@ pln_damage(struct plnstr *pp, char type, int noisy)
 	aim = 100 - aim;
     }
 
-    len = 0;
+    len = snprintf(buf, sizeof(buf), "%s", noisy);
     while (i--) {
+	if (noisy) {
+	    if (len > 75) {
+		mpr(pp->pln_own, "%s\n", buf);
+		len = 0;
+	    }
+	}
 	dam += roll(6);
 	hitroll = roll(100);
 	if (hitroll >= 90) {
@@ -1057,10 +1063,6 @@ pln_damage(struct plnstr *pp, char type, int noisy)
 		len += sprintf(buf + len, "blam");
 	}
 	if (noisy) {
-	    if (len > 75) {
-		mpr(pp->pln_own, "%s\n", buf);
-		len = 0;
-	    }
 	    if (i)
 		len += sprintf(buf + len, "-");
 	}

include/prototypes.h
@@ -533,7 +533,7 @@ extern void pln_put1(struct plist *);
 extern void plane_sweep(struct emp_qelem *, coord, coord);
 extern int put_plane_on_land(struct plnstr *, struct lndstr *);
 extern int pln_hitchance(struct plnstr *, int, int);
-extern int pln_damage(struct plnstr *, char, int);
+extern int pln_damage(struct plnstr *, char, char *);
 extern int pln_identchance(struct plnstr *, int, int);
 extern int pln_is_in_orbit(struct plnstr *);
 extern void pln_set_tech(struct plnstr *, int);

src/lib/commands/bomb.c
@@ -275,7 +275,7 @@ eff_bomb(struct emp_qelem *list, struct sctstr *target)
 	plp = (struct plist *)qp;
 	if (changed_plane_aborts(plp))
 	    continue;
-	dam += pln_damage(&plp->plane, 'p', 1);
+	dam += pln_damage(&plp->plane, 'p', "");
     }
     getsect(target->sct_x, target->sct_y, &sect);
     target = &sect;
@@ -348,7 +348,7 @@ comm_bomb(struct emp_qelem *list, struct sctstr *target)
 	plp = (struct plist *)qp;
 	if (changed_plane_aborts(plp))
 	    continue;
-	dam += pln_damage(&plp->plane, 'p', 1);
+	dam += pln_damage(&plp->plane, 'p', "");
     }
     getsect(target->sct_x, target->sct_y, &sect);
     target = &sect;
@@ -457,7 +457,7 @@ ship_bomb(struct emp_qelem *list, struct sctstr *target)
 	}
 	if (pct_chance(hitchance)) {
 	    /* pinbombing is more accurate than normal bombing */
-	    dam = 2 * pln_damage(&plp->plane, 'p', 1);
+	    dam = 2 * pln_damage(&plp->plane, 'p', "");
 	} else {
 	    pr("splash\n");
 	    /* Bombs that miss have to land somewhere! */
@@ -556,7 +556,7 @@ plane_bomb(struct emp_qelem *list, struct sctstr *target)
 	}
 	if (pct_chance(hitchance)) {
 	    /* pinbombing is more accurate than normal bombing */
-	    dam = 2 * pln_damage(&plp->plane, 'p', 1);
+	    dam = 2 * pln_damage(&plp->plane, 'p', "");
 	} else {
 	    pr("thud\n");
 	    /* Bombs that miss have to land somewhere! */
@@ -664,7 +664,7 @@ land_bomb(struct emp_qelem *list, struct sctstr *target)
 	    pr("%d%% hitchance...", hitchance);
 	}
 	if (pct_chance(hitchance)) {
-	    dam = 2 * pln_damage(&plp->plane, 'p', 1);
+	    dam = 2 * pln_damage(&plp->plane, 'p', "");
 	} else {
 	    pr("thud\n");
 	    /* Bombs that miss have to land somewhere! */
@@ -703,7 +703,7 @@ strat_bomb(struct emp_qelem *list, struct sctstr *target)
 	    detonate(&nuke, target->sct_x, target->sct_y,
 		     plp->plane.pln_flags & PLN_AIRBURST);
 	else
-	    dam += pln_damage(&plp->plane, 's', 1);
+	    dam += pln_damage(&plp->plane, 's', "");
     }
     if (dam <= 0)
 	return;

src/lib/commands/laun.c
@@ -239,7 +239,7 @@ launch_missile(struct plnstr *pp)
 	if (getnuke(nuk_on_plane(pp), &nuke))
 	    detonate(&nuke, sx, sy, pp->pln_flags & PLN_AIRBURST);
 	else {
-	    dam = pln_damage(pp, 's', 1);
+	    dam = pln_damage(pp, 's', "");
 	    pr("did %d damage in %s\n", PERCENT_DAMAGE(dam),
 	       xyas(sx, sy, player->cnum));
 	    if (sect.sct_own != 0) {
@@ -268,11 +268,11 @@ launch_missile(struct plnstr *pp)
 		     N_SHP_MISS, N_SHP_SMISS, sublaunch,
 		     target_ship.shp_own)) {
 	    pr("splash\n");
-	    dam = pln_damage(pp, 'p', 0);
+	    dam = pln_damage(pp, 'p', NULL);
 	    collateral_damage(target_ship.shp_x, target_ship.shp_y, dam);
 	    return RET_OK;
 	}
-	dam = pln_damage(pp, 'p', 1);
+	dam = pln_damage(pp, 'p', "");
 	shipdamage(&target_ship, dam);
 	if (target_ship.shp_effic < SHIP_MINEFF)
 	    pr("%s sunk!\n", prship(&target_ship));

src/lib/subs/lndsub.c
@@ -818,10 +818,10 @@ lnd_missile_interdiction(struct emp_qelem *list, coord newx, coord newy,
 	    stopping = 1;
 	    if (msl_hit(&plp->plane, hardtarget, EF_LAND,
 			N_LND_MISS, N_LND_SMISS, sublaunch, victim)) {
-		newdam = pln_damage(&plp->plane, 'p', 1);
+		newdam = pln_damage(&plp->plane, 'p', "");
 		dam += newdam;
 	    } else {
-		newdam = pln_damage(&plp->plane, 'p', 0);
+		newdam = pln_damage(&plp->plane, 'p', NULL);
 		collateral_damage(newx, newy, newdam);
 	    }
 	use_up_msl:

src/lib/subs/mission.c
@@ -561,7 +561,7 @@ perform_mission_msl(int dam, struct emp_qelem *missiles, coord x, coord y,
 	    if (!msl_hit(&plp->plane, SECT_HARDTARGET, EF_SECTOR,
 			 N_SCT_MISS, N_SCT_SMISS, sublaunch, victim))
 		CANT_REACH();
-	    dam2 = pln_damage(&plp->plane, 'p', 1);
+	    dam2 = pln_damage(&plp->plane, 'p', "");
 	    air_dam += dam2;
 	use_up_msl:
 	    plp->plane.pln_effic = 0;
@@ -1066,7 +1066,7 @@ air_damage(struct emp_qelem *bombers, coord x, coord y, int mission,
 	else if (hardtarget != SECT_HARDTARGET)
 	    wu(0, pp->pln_own, "\t\t%d%% hitchance...", hitchance);
 	if (pct_chance(hitchance)) {
-	    newdam = pln_damage(&plp->plane, 'p', 1);
+	    newdam = pln_damage(&plp->plane, 'p', "");
 	    wu(0, pp->pln_own,
 	       "\t\thit %s %s for %d damage\n",
 	       cname(victim), s, newdam);

src/lib/subs/mslsub.c
@@ -114,7 +114,7 @@ msl_launch(struct plnstr *pp, int type, char *what, coord x, coord y,
 	    struct sctstr sect;
 	    int dam;
 
-	    dam = pln_damage(pp, 'p', 0) / 2;
+	    dam = pln_damage(pp, 'p', NULL) / 2;
 	    if (pp->pln_ship >= 0) {
 		shipdamage(&ship, dam);
 		putship(ship.shp_uid, &ship);

src/lib/subs/shpsub.c
@@ -569,13 +569,13 @@ shp_missile_interdiction(struct emp_qelem *list, coord newx, coord newy,
 	    if (msl_hit(&plp->plane,
 			shp_hardtarget(&mvs->unit.ship), EF_SHIP,
 			N_SHP_MISS, N_SHP_SMISS, sublaunch, victim)) {
-		dam = pln_damage(&plp->plane, 'p', 1);
+		dam = pln_damage(&plp->plane, 'p', "");
 		mpr(victim,
 		    "missile interdiction mission does %d damage to %s!\n",
 		    dam, prship(&mvs->unit.ship));
 		shp_damage_one(mvs, dam);
 	    } else {
-		dam = pln_damage(&plp->plane, 'p', 0);
+		dam = pln_damage(&plp->plane, 'p', NULL);
 		collateral_damage(newx, newy, dam);
 	    }
 	    mvs = most_valuable_ship(list, newx, newy);

commit e312e4f8d6eea49423722c87b7f368e42b1086be

bomb launch interdiction: Fix crash on bombs missing target
plane damage() prints @noisy with snprintf() even when it's null.  It
doesn't actually use the output then.  Some systems (GNU, Windows)
deal gracefully with printing null strings, others crash.  @noisy is
null when bombers or missiles miss and do collateral damage.  Affects
bomb, launch, and interdiction missions.  Broken in commit 820d755e5
"subs: Change pln_damage()'s parameter noisy to string prefix",
v4.3.33.

Fix by guarding the snprintf().

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

src/lib/subs/plnsub.c
@@ -30,7 +30,7 @@
  *     Dave Pare, 1986
  *     Ken Stevens, 1995
  *     Steve McClure, 1998-2000
- *     Markus Armbruster, 2004-2016
+ *     Markus Armbruster, 2004-2020
  */
 
 #include <config.h>
@@ -1052,7 +1052,7 @@ pln_damage(struct plnstr *pp, char type, char *noisy)
 	aim = 100 - aim;
     }
 
-    len = snprintf(buf, sizeof(buf), "%s", noisy);
+    len = noisy ? snprintf(buf, sizeof(buf), "%s", noisy) : 0;
     while (i--) {
 	if (noisy) {
 	    if (len > 75) {