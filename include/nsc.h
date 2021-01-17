gefla/empserver

commit 157ae8ec21ac52892897aacf344a732cee9deb21

Clean up superfluous include of nsc.h in prototypes.h

include/prototypes.h
@@ -39,7 +39,6 @@
 #include <string.h>
 #include <time.h>
 #include "item.h"
-#include "nsc.h"
 #include "misc.h"
 #include "types.h"
 

include/commands.h
@@ -35,8 +35,9 @@
 
 #include "file.h"
 #include "nat.h"
+#include "nsc.h"
 #include "player.h"
-#include "sect.h"
 #include "prototypes.h"
+#include "sect.h"
 
 #endif

src/lib/common/type.c
@@ -38,6 +38,7 @@
 #include "item.h"
 #include "land.h"
 #include "match.h"
+#include "nsc.h"
 #include "nuke.h"
 #include "plane.h"
 #include "product.h"

src/lib/subs/check.c
@@ -38,6 +38,7 @@
 #include "file.h"
 #include "player.h"
 #include "prototypes.h"
+#include "xy.h"
 
 /* Note that timestamps make things tricky.  And, we don't
  * really care about the timestamp, we just care about the rest

src/lib/subs/control.c
@@ -34,6 +34,7 @@
 
 #include "file.h"
 #include "land.h"
+#include "nsc.h"
 #include "prototypes.h"
 #include "sect.h"
 

src/lib/subs/distrea.c
@@ -34,6 +34,7 @@
 
 #include "file.h"
 #include "nat.h"
+#include "nsc.h"
 #include "player.h"
 #include "prototypes.h"
 #include "treaty.h"

src/lib/subs/move.c
@@ -36,6 +36,7 @@
 #include "damage.h"
 #include "file.h"
 #include "map.h"
+#include "nsc.h"
 #include "path.h"
 #include "player.h"
 #include "prototypes.h"

src/lib/subs/nstr.c
@@ -38,6 +38,7 @@
 #include <limits.h>
 #include "file.h"
 #include "match.h"
+#include "nsc.h"
 #include "player.h"
 #include "prototypes.h"
 

commit 259200777e6fddc24a0ae2914f4c78fecd301980

include: Move sarg.c stuff to nsc.h to avoid incomplete enum
Portability issue: prototypes.h uses enum ns_seltype, defined in
nsc.h.  GCC is fine with that; it supports incomplete enum types as an
extension.  Broken in commit 157ae8ec2 "Clean up superfluous include
of nsc.h in prototypes.h", v4.3.27.  Moving sarg.c stuff to nsc.h
fixes it.

Signed-off-by: Markus Armbruster <armbru@pond.sub.org>

include/nsc.h
@@ -28,7 +28,7 @@
  *
  *  Known contributors to this file:
  *     Dave Pare, 1989
- *     Markus Armbruster, 2004-2016
+ *     Markus Armbruster, 2004-2020
  */
 
 #ifndef NSC_H
@@ -319,6 +319,12 @@ extern void nsc_init(void);
 extern int nxtitem(struct nstr_item *, void *);
 /* src/lib/subs/nxtsct.c */
 extern int nxtsct(struct nstr_sect *, struct sctstr *);
+/* src/lib/subs/sarg.c */
+extern enum ns_seltype sarg_type(char *);
+extern int sarg_xy(char *, coord *, coord *);
+extern int sarg_area(char *, struct range *);
+extern int sarg_range(char *, coord *, coord *, int *);
+extern int sarg_list(char *, int *, int);
 /* src/lib/subs/snxtitem.c */
 extern int snxtitem(struct nstr_item *, int, char *, char *);
 extern void snxtitem_area(struct nstr_item *, int, struct range *);

include/prototypes.h
@@ -566,11 +566,7 @@ extern void rad_map_set(natid, int, int, int, double, int);
 /* retreat.c */
 /* in retreat.h */
 /* sarg.c */
-extern enum ns_seltype sarg_type(char *);
-extern int sarg_xy(char *, coord *, coord *);
-extern int sarg_area(char *, struct range *);
-extern int sarg_range(char *, coord *, coord *, int *);
-extern int sarg_list(char *, int *, int);
+/* in nsc.h */
 /* satmap.c */
 extern void satdisp_sect(struct sctstr *, int);
 extern void satdisp_units(coord, coord);